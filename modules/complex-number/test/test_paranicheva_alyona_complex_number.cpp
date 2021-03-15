a6c9d70b6f243397886b59aea3c0bcfb9187de38
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2021-03-15 11:13:10 +0300

    Update test_paranicheva_alyona_complex_number.cpp

--- modules/complex-number/test/test_paranicheva_alyona_complex_number.cpp
+++ modules/complex-number/test/test_paranicheva_alyona_complex_number.cpp
@@ -1,35 +1,35 @@
 // Copyright 2021 Paranicheva Alyona
 
 #include <gtest/gtest.h>
-#include <math.h>
+#include <cmath>
 
 #include "include/complex_number.h"
 
 TEST(Paranicheva_Alyona_ComplexNumberTest, Module) {
     ComplexNumber z(6.0, -8.0);
 
-    double resmodule = sqrt(pow(z.getRe(), 2) + pow(z.getIm(), 2));
-    EXPECT_EQ(10.0, resmodule);
+    double resmodule = hypot(z.getRe(), z.getIm());
+    EXPECT_DOUBLE_EQ(10.0, resmodule);
 }
 
 TEST(Paranicheva_Alyona_ComplexNumberTest, Module_Addition) {
     ComplexNumber z1(6.0, -8.0);
     ComplexNumber z2(3.0, -4.0);
 
-    double resmodule1 = sqrt(pow(z1.getRe(), 2) + pow(z1.getIm(), 2));
-    double resmodule2 = sqrt(pow(z2.getRe(), 2) + pow(z2.getIm(), 2));
+    double resmodule1 = hypot(z1.getRe(), z1.getIm());
+    double resmodule2 = hypot(z2.getRe(), z2.getIm());
     double sum = resmodule1 + resmodule2;
 
-    EXPECT_EQ(15.0, sum);
+    EXPECT_DOUBLE_EQ(15.0, sum);
 }
 
 TEST(Paranicheva_Alyona_ComplexNumberTest, Module_Subtraction) {
     ComplexNumber z1(6.0, -8.0);
     ComplexNumber z2(3.0, -4.0);
 
-    double resmodule1 = sqrt(pow(z1.getRe(), 2) + pow(z1.getIm(), 2));
-    double resmodule2 = sqrt(pow(z2.getRe(), 2) + pow(z2.getIm(), 2));
+    double resmodule1 = hypot(z1.getRe(), z1.getIm());
+    double resmodule2 = hypot(z2.getRe(), z2.getIm());
     double sub = resmodule1 - resmodule2;
 
-    EXPECT_EQ(5.0, sub);
+    EXPECT_DOUBLE_EQ(5.0, sub);
 }
