0527e667917e36bbd6e4396c41fa7b1df27f3863
dashachernyh <dasha.chernyh.@mail.ru>
2021-03-16 13:47:55 +0300

    add_nex_test

--- modules/complex-number/test/test_chernyh_daria_complex_number.cpp
+++ modules/complex-number/test/test_chernyh_daria_complex_number.cpp
@@ -2,34 +2,45 @@
 
 #include <gtest/gtest.h>
 
+#include <tuple>
 #include "include/complex_number.h"
 
-TEST(Chernyh_Daria_ComplexNumberTEST, Addition_of_conjugate) {
-  ComplexNumber number(21, 6);
-  ComplexNumber number_conj(21, -6);
+typedef testing::TestWithParam<std::tuple<double, double>>
+Chernyh_Daria_ComplexNumberTest_Parametrized;
+TEST_P(Chernyh_Daria_ComplexNumberTest_Parametrized,
+  Addition_of_conjugate) {
+  double numberRe = std::get<0>(GetParam());
+  double numberIm = std::get<1>(GetParam());
+  double numberIm_conj = -numberIm;
+  ComplexNumber number(numberRe, numberIm);
+  ComplexNumber number_conj(numberRe, numberIm_conj);
   ComplexNumber result = number + number_conj;
-  ComplexNumber check_res(number.getRe() + number.getRe(), 0);
 
-  ASSERT_EQ(check_res, result);
+  double check_Re = numberRe + numberRe;
+  double check_Im = numberIm + numberIm_conj;
+
+  ASSERT_DOUBLE_EQ(check_Re, result.getRe());
+  ASSERT_DOUBLE_EQ(check_Im, result.getIm());
 }
 
-TEST(Chernyh_Daria_ComplexNumberTEST, Multiply_of_conjugate) {
-  ComplexNumber number(21, 6);
-  ComplexNumber number_conj(21, -6);
-  ComplexNumber result = number * number_conj;
-  double resRe = number.getRe() * number.getRe()
-               + number.getIm() * number.getIm();
+typedef testing::TestWithParam<std::tuple<double, double>>
+Chernyh_Daria_ComplexNumberTest_Parametrized;
+TEST_P(Chernyh_Daria_ComplexNumberTest_Parametrized,
+  Division_of_conjugate) {
+  double numberRe = std::get<0>(GetParam());
+  double numberIm = std::get<1>(GetParam());
+  double numberIm_conj = -numberIm;
+  ComplexNumber number(numberRe, numberIm);
+  ComplexNumber number_conj(numberRe, numberIm_conj);
+  ComplexNumber result = number / number_conj;
 
-  ComplexNumber check_res(resRe, 0);
+  double check_Re = (numberRe * numberRe + numberIm * numberIm_conj)
+      / (numberRe * numberRe + numberIm_conj * numberIm_conj);
+  double check_Im = (numberRe * numberIm - numberRe * numberIm_conj)
+      / (numberRe * numberRe + numberIm_conj * numberIm_conj);
 
-  ASSERT_EQ(check_res, result);
-}
-
-TEST(Chernyh_Daria_ComplexNumberTEST, Division_by_zero) {
-  ComplexNumber number(21, 6);
-  ComplexNumber number_zero(0, 0);
-
-  ASSERT_ANY_THROW(number / number_zero);
+  ASSERT_DOUBLE_EQ(check_Re, result.getRe());
+  ASSERT_DOUBLE_EQ(check_Im, result.getIm());
 }
 
 TEST(Chernyh_Daria_ComplexNumberTEST, Not_equality_of_conjugate) {
@@ -38,3 +49,8 @@ TEST(Chernyh_Daria_ComplexNumberTEST, Not_equality_of_conjugate) {
 
   ASSERT_NE(number, number_conj);
 }
+INSTANTIATE_TEST_CASE_P(/**/, Chernyh_Daria_ComplexNumberTest_Parametrized,
+    testing::Combine(
+    testing::Values(-1.0, 3.0),
+    testing::Values(6.0, -2.0)
+));
