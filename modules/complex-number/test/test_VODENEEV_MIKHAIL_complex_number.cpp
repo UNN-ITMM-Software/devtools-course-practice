ae9fa846bc0d0e2652fb1485458ba7d38afc5a9d
Vodeneev <55285701+Vodeneev@users.noreply.github.com>
2021-03-16 09:19:50 +0300

    Added new tests for lab1

---
+++ modules/complex-number/test/test_VODENEEV_MIKHAIL_complex_number.cpp
@@ -0,0 +1,30 @@
+// Copyright 2021 Vodeneev Mikhail
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+
+TEST(Vodeneev_Mikhail_ComplexNumberTest, Comparison) {
+
+    double re1 = 10, re2 = 10;
+    double im1 = 15, im2 = 15;
+
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+
+    EXPECT_EQ(z1, z2);
+}
+
+TEST(Vodeneev_Mikhail_ComplexNumberTest, Add) {
+
+    double re1 = 1, re2 = 2;
+    double im1 = 6, im2 = 9;
+
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+    ComplexNumber res = z1 + z2;
+
+    EXPECT_EQ(res.getRe(), 3);
+    EXPECT_EQ(res.getIm(), 15);
+}
