504aa62a35995744c9dd68e2a1f1002146d477ae
Kolesin Andrei <kolesinandrei1910@gmail.com>
2021-03-29 12:41:47 +0300

    change int to float

--- modules/complex-number/test/test_kolesin_andrey_complex_number.cpp
+++ modules/complex-number/test/test_kolesin_andrey_complex_number.cpp
@@ -6,25 +6,25 @@
 
 TEST(Kolesin_Andrey_ComplexNumberTest, Rotation) {
     ComplexNumber i(0, 1);
-    ComplexNumber a(3, 5);
-    ComplexNumber b(-5, 3);
-    ComplexNumber c(-3, -5);
-    ComplexNumber d(5, -3);
+    ComplexNumber a(3.6, 5.7);
+    ComplexNumber b(-5.7, 3.6);
+    ComplexNumber c(-3.6, -5.7);
+    ComplexNumber d(5.7, -3.6);
     ASSERT_EQ(b, a * i);
     ASSERT_EQ(c, a * i * i);
     ASSERT_EQ(d, a * i * i * i);
     ASSERT_EQ(a, a * i * i * i * i);
 }
 TEST(Kolesin_Andrey_ComplexNumberTest, NegSqr) {
-    ComplexNumber a(-5, 0);
+    ComplexNumber a(-5.2, 0);
     ComplexNumber b = a * a;
     ASSERT_EQ(b.getIm(), 0);
     ASSERT_GT(b.getRe(), 0);
 }
 TEST(Kolesin_Andrey_ComplexNumberTest, Conjugate) {
-    ComplexNumber a(-5, 7);
+    ComplexNumber a(-5.3, 7.2);
     ComplexNumber ac(a.getRe(), -a.getIm());
-    ComplexNumber b(8, -10);
+    ComplexNumber b(8.9, -10.5);
     ComplexNumber bc(b.getRe(), -b.getIm());
     ComplexNumber apb = a + b;
     ComplexNumber apbc(apb.getRe(), -apb.getIm());
