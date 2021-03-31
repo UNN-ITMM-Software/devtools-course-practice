2552fcd5ce5c204084eb16918d8eaea94257f248
Denis Ostapovich <vofksden@gmail.com>
2021-03-31 15:45:17 +0300

    modules/complex-number/test/test_OSTAPOVICH_DENIS_complex_number.cpp

--- modules/complex-number/test/test_OSTAPOVICH_DENIS_complex_number.cpp
+++ modules/complex-number/test/test_OSTAPOVICH_DENIS_complex_number.cpp
@@ -6,12 +6,6 @@
 
 const double epsilon = std::numeric_limits<double>::epsilon();
 
-TEST(Ostapovich_Denis_ComplexNumberTest, Throw_On_Division_By_Zero) {
-    ComplexNumber a(1, 2);
-    ComplexNumber b(0, 0);
-    ASSERT_ANY_THROW(a / b);
-}
-
 TEST(Ostapovich_Denis_ComplexNumberTest, Equality_Operator_Not_Zero) {
     ComplexNumber a(2, 1);
     ComplexNumber b(2, 1);
@@ -19,7 +13,7 @@ TEST(Ostapovich_Denis_ComplexNumberTest, Equality_Operator_Not_Zero) {
 }
 
 TEST(Ostapovich_Denis_ComplexNumberTest, Equality_Operator_Approximately_Zero) {
-    ComplexNumber a(0, 0);
+    ComplexNumber a(0.0, 0.0);
     ComplexNumber b(-epsilon, epsilon);
     ASSERT_TRUE(a != b);
 }
@@ -31,19 +25,19 @@ TEST(Ostapovich_Denis_ComplexNumberTest, No_Throw_On_Addition) {
 }
 
 TEST(Ostapovich_Denis_ComplexNumberTest, No_Throw_On_Subtraction) {
-    ComplexNumber a(0, 1);
+    ComplexNumber a(0, 1.3);
     ComplexNumber b(1, 2);
     ASSERT_NO_THROW(a - b);
 }
 
 TEST(Ostapovich_Denis_ComplexNumberTest, No_Throw_On_Multiplication) {
     ComplexNumber a(8, 9);
-    ComplexNumber b(10, 11);
+    ComplexNumber b(10, 11.0);
     ASSERT_NO_THROW(a * b);
 }
 
 TEST(Ostapovich_Denis_ComplexNumberTest, No_Throw_On_Division_By_Not_Zero) {
-    ComplexNumber a(7, 6);
-    ComplexNumber b(5, 4);
+    ComplexNumber a(7.0, 6.1);
+    ComplexNumber b(5.6, 4.3);
     ASSERT_NO_THROW(a / b);
 }
