3546ad265bd8b8342d3e2d1d2d023f2cbb7c5608
Denis Ostapovich <vofksden@gmail.com>
2021-03-31 17:52:01 +0300

    tests parameterized

--- modules/complex-number/test/test_OSTAPOVICH_DENIS_complex_number.cpp
+++ modules/complex-number/test/test_OSTAPOVICH_DENIS_complex_number.cpp
@@ -8,9 +8,18 @@
 
 const double epsilon = std::numeric_limits<double>::epsilon();
 
-TEST(Ostapovich_Denis_ComplexNumberTest, Equality_Operator_Not_Zero) {
-    ComplexNumber a(2, 1);
-    ComplexNumber b(2, 1);
+typedef testing::TestWithParam<std::tuple<double, double>> 
+    Ostapovich_Denis_ComplexNumberTest_Parameterized_2;
+
+typedef testing::TestWithParam<std::tuple<double, double, double, double>>
+    Ostapovich_Denis_ComplexNumberTest_Parameterized_4;
+
+TEST_P(Ostapovich_Denis_ComplexNumberTest_Parameterized_2, Equality_Operator_Not_Zero) {
+    double re = std::get<0>(GetParam());
+    double im = std::get<1>(GetParam());
+
+    ComplexNumber a(re, im);
+    ComplexNumber b(re, im);
     ASSERT_EQ(a, b);
 }
 
@@ -20,26 +29,48 @@ TEST(Ostapovich_Denis_ComplexNumberTest, Equality_Operator_Approximately_Zero) {
     ASSERT_NE(a, b);
 }
 
-TEST(Ostapovich_Denis_ComplexNumberTest, No_Throw_On_Addition) {
-    ComplexNumber a(5, 6);
-    ComplexNumber b(1, 2);
+TEST_P(Ostapovich_Denis_ComplexNumberTest_Parameterized_4, No_Throw_On_Addition) {
+    double re1 = std::get<0>(GetParam());
+    double im1 = std::get<1>(GetParam());
+    double re2 = std::get<2>(GetParam());
+    double im2 = std::get<3>(GetParam());
+    ComplexNumber a(re1, im1);
+    ComplexNumber b(re1, im2);
     ASSERT_NO_THROW(a + b);
 }
 
-TEST(Ostapovich_Denis_ComplexNumberTest, No_Throw_On_Subtraction) {
+TEST_P(Ostapovich_Denis_ComplexNumberTest_Parameterized_4, No_Throw_On_Subtraction) {
     ComplexNumber a(0, 1.3);
     ComplexNumber b(1, 2);
     ASSERT_NO_THROW(a - b);
 }
 
-TEST(Ostapovich_Denis_ComplexNumberTest, No_Throw_On_Multiplication) {
+TEST_P(Ostapovich_Denis_ComplexNumberTest_Parameterized_4, No_Throw_On_Multiplication) {
     ComplexNumber a(8, 9);
     ComplexNumber b(10, 11.0);
     ASSERT_NO_THROW(a * b);
 }
 
-TEST(Ostapovich_Denis_ComplexNumberTest, No_Throw_On_Division_By_Not_Zero) {
+TEST_P(Ostapovich_Denis_ComplexNumberTest_Parameterized_4, No_Throw_On_Division_By_Not_Zero) {
     ComplexNumber a(7.0, 6.1);
     ComplexNumber b(5.6, 4.3);
     ASSERT_NO_THROW(a / b);
 }
+
+INSTANTIATE_TEST_CASE_P(/**/, Ostapovich_Denis_ComplexNumberTest_Parameterized_2,
+    testing::Values(
+  std::make_tuple(15.0, 1.0),
+  std::make_tuple(6.0, -3.0),
+  std::make_tuple(0, 0),
+  std::make_tuple(-12, 10),
+  std::make_tuple(-12, -21)
+));
+
+INSTANTIATE_TEST_CASE_P(/**/, Ostapovich_Denis_ComplexNumberTest_Parameterized_4,
+    testing::Values(
+  std::make_tuple(11.0, 12.0, 13.0, 14.0),
+  std::make_tuple(-11.0, 12.0, -13.0, 14.0),
+  std::make_tuple(11.0, -12.0, 13.0, -14.0),
+  std::make_tuple(-11.0, -12.0, -13.0, -14.0),
+  std::make_tuple(0, 0, 0, 0)
+));
