18a41b7aea82db1e2c33aa0da18c41f00b5d6c8d
Andrew <andrey.aleksandrychev2@mail.ru>
2021-03-31 21:23:37 +0300

    Fifth commit

--- modules/complex-number/test/test_aleksandrychev_andrey_complex_number.cpp
+++ modules/complex-number/test/test_aleksandrychev_andrey_complex_number.cpp
@@ -4,12 +4,13 @@
 
 #include "include/complex_number.h"
 
-double Re1 = 10.5, Im1 = 14.2;
-double Re2 = 5.0, Im2 = 2.1;
-double Re3 = 8.3, Im3 = 7.6;
 
 // Test arithmatical operators and Copy Constructor
 TEST(Aleksandrychev_Andrey_ComplexNumberTest, Arithmetic_Copy_Constr) {
+    double Re1 = -1.5, Im1 = 4.3;
+    double Re2 = 5.0, Im2 = 2.1;
+    double Re3 = 8.3, Im3 = 7.6;
+
     ComplexNumber num1(Re1, Im1);
     ComplexNumber num2(Re2, Im2);
     ComplexNumber num3(Re3, Im3);
@@ -21,13 +22,16 @@ TEST(Aleksandrychev_Andrey_ComplexNumberTest, Arithmetic_Copy_Constr) {
 
 // Test of multiplying
 TEST(Aleksandrychev_Andrey_ComplexNumberTest, Multiplication) {
+    double Re1 = 10.5, Im1 = 14.2;
+    double Re2 = 5.0, Im2 = 2.1;
     ComplexNumber z1(Re1, Im1);
     ComplexNumber z2(Re2, -Im2);
-    ASSERT_EQ(82.32, (z1*z2).getRe());
+    ASSERT_EQ(z2.getRe(), (z1*z2/z1).getRe());
 }
 
 // Test != operator
 TEST(Aleksandrychev_Andrey_ComplexNumberTest, Not_equally_operator) {
+    double Re1 = 4.0, Im1 = 2.2;
     ComplexNumber a(Re1, Im1);
     ComplexNumber b(0.0, 0.0);
     ASSERT_TRUE(a != b);
