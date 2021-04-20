bd3fc6c49f30140358ecb91cc68b86565c365102
dashachernyh <dasha.chernyh.@mail.ru>
2021-04-20 11:42:34 +0300

    lab2_f4

--- modules/prime_numbers/src/prime_numbers.cpp
+++ modules/prime_numbers/src/prime_numbers.cpp
@@ -7,8 +7,7 @@
 
 #include "include/prime_numbers.h"
 
-PrimeNumber::PrimeNumber(int size)
-{
+PrimeNumber::PrimeNumber(int size) {
   n = size;
   primes.resize(n, 0);
   std::mt19937 gen;
@@ -19,9 +18,7 @@ PrimeNumber::PrimeNumber(int size)
 PrimeNumber::PrimeNumber(const PrimeNumber& pn) : n(pn.n),
 primes(pn.primes) {}
 
-
-PrimeNumber::PrimeNumber(int a, int count, int h) 
-{
+PrimeNumber::PrimeNumber(int a, int count, int h) {
   n = count;
   primes.resize(n, 0);
   for(int i = 0; i < n; i++)
@@ -38,18 +35,14 @@ PrimeNumber& PrimeNumber::operator=(const PrimeNumber& pn) {
   return *this;
 }
 
-void PrimeNumber::FindPrimes()
-{
+void PrimeNumber::FindPrimes() {
   std::vector<int> primes_tmp;
-  for (int i = 0; i < n; i++)
-  {
+  for (int i = 0; i < n; i++) {
     int j = 2;
-    while(primes[i] % j != 0)
-    {
+    while(primes[i] % j != 0) {
       j++;
     }
-    if (primes[i] == j)
-    {
+    if (primes[i] == j) {
       primes_tmp.push_back(j);
     }
   }
@@ -57,17 +50,14 @@ void PrimeNumber::FindPrimes()
   n = static_cast<int>(primes_tmp.size());
 }
 
-void PrimeNumber::PrintPrimes()
-{
-  for (int i = 0; i < n; i++)
-  {
+void PrimeNumber::PrintPrimes() {
+  for (int i = 0; i < n; i++) {
     std::cout << primes[i] << " ";
   }
   std::cout << std::endl;
 }
 
-std::vector<int> PrimeNumber::Get_primes() const
-{
+std::vector<int> PrimeNumber::Get_primes() const {
   return primes;
 }
 
