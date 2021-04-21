d77158ad1406b3cca58fd485e4fe7c09a84a34d0
MADZEROPIE <ask_ii1@mail.ru>
2021-04-21 14:44:27 +0300

    Finally done.

--- modules/Ceasar_cipher/test/test_cipher.cpp
+++ modules/Ceasar_cipher/test/test_cipher.cpp
@@ -1,11 +1,15 @@
 // Copyright 2021 Kudriavtsev Alexandr
 #include <gtest/gtest.h>
+#include <string>
+#include <random>
+#include <ctime>
 
 #include "include/cipher.h"
 
 TEST(CEASAR_CIPHER, no_exception_with_empty_string) {
     CeasarCipher CC;
     const std::string test = "";
+
     ASSERT_NO_THROW(CC.Decode(test));
     ASSERT_NO_THROW(CC.Encode(test));
 }
@@ -13,6 +17,7 @@ TEST(CEASAR_CIPHER, no_exception_with_empty_string) {
 TEST(CEASAR_CIPHER, nothings_happens_with_empty_string_enc) {
     CeasarCipher CC;
     std::string test, enc;
+
     ASSERT_NO_THROW(enc = CC.Encode(test));
     ASSERT_EQ(test, enc);
 }
@@ -20,6 +25,7 @@ TEST(CEASAR_CIPHER, nothings_happens_with_empty_string_enc) {
 TEST(CEASAR_CIPHER, nothings_happens_with_empty_string_dec) {
     CeasarCipher CC;
     const std::string test;
+
     std::string dec;
     ASSERT_NO_THROW(dec = CC.Decode(test));
     ASSERT_EQ(test, dec);
@@ -28,6 +34,7 @@ TEST(CEASAR_CIPHER, nothings_happens_with_empty_string_dec) {
 TEST(CEASAR_CIPHER, decode_is_working_as_expected_1) {
     CeasarCipher CC;
     const std::string test = "BCD bcd", expDec = "ABC abc";
+
     std::string dec;
     ASSERT_NO_THROW(dec = CC.Decode(test));
     ASSERT_EQ(expDec, dec);
@@ -36,6 +43,7 @@ TEST(CEASAR_CIPHER, decode_is_working_as_expected_1) {
 TEST(CEASAR_CIPHER, encode_is_working_as_expected_1) {
     CeasarCipher CC;
     const std::string test = "ABC abc", expEnc = "BCD bcd";
+
     std::string enc;
     ASSERT_NO_THROW(enc = CC.Encode(test));
     ASSERT_EQ(expEnc, enc);
@@ -43,8 +51,10 @@ TEST(CEASAR_CIPHER, encode_is_working_as_expected_1) {
 
 TEST(CEASAR_CIPHER, decode_is_working_as_expected_with_big_key_mod1) {
     CeasarCipher CC;
-    int key = (rand() % 100) * 26 + 1;
+    std::mt19937 gen;
+    int key = (gen() % 100) * 26 + 1;
     const std::string test = "BCD bcd", expDec = "ABC abc";
+
     std::string dec;
     ASSERT_NO_THROW(dec = CC.Decode(test, key));
     ASSERT_EQ(expDec, dec);
@@ -52,8 +62,10 @@ TEST(CEASAR_CIPHER, decode_is_working_as_expected_with_big_key_mod1) {
 
 TEST(CEASAR_CIPHER, encode_is_working_as_expected_with_big_key_mod1) {
     CeasarCipher CC;
-    int key = (rand() % 100) * 26 + 1;
+    std::mt19937 gen;
+    int key = (gen() % 100) * 26 + 1;
     const std::string test = "ABC abc", expEnc = "BCD bcd";
+
     std::string enc;
     ASSERT_NO_THROW(enc = CC.Encode(test, key));
     ASSERT_EQ(expEnc, enc);
@@ -61,20 +73,25 @@ TEST(CEASAR_CIPHER, encode_is_working_as_expected_with_big_key_mod1) {
 
 TEST(CEASAR_CIPHER, decode_is_working_with_all_s_alphabet_with_big_key_m2) {
     CeasarCipher CC;
-    int key = (rand() % 100) * 26 + 2;
-    std::string test = "c d e f g h i j k l m n o p q r s t u v w x y z a b"; 
+    std::mt19937 gen;
+    int key = (gen() % 100) * 26 + 2;
+    std::string test = "c d e f g h i j k l m n o p q r s t u v w x y z a b";
     std::string expDec = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
     std::string dec;
+
     ASSERT_NO_THROW(dec = CC.Decode(test, key));
     ASSERT_EQ(expDec, dec);
 }
 
 TEST(CEASAR_CIPHER, encode_is_working_with_all_s_alphabet_with_big_key_m2) {
     CeasarCipher CC;
-    int key = (rand() % 100) * 26 + 2;
+    std::mt19937 gen;
+    int key = (gen() % 100) * 26 + 2;
+
     std::string test = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
     std::string expEnc = "c d e f g h i j k l m n o p q r s t u v w x y z a b";
     std::string enc;
+
     ASSERT_NO_THROW(enc = CC.Encode(test, key));
     ASSERT_EQ(expEnc, enc);
 }
@@ -84,17 +101,23 @@ TEST(CEASAR_CIPHER, encode_is_working_with_all_s_alphabet_with_big_key_m2) {
 TEST(CEASAR_CIPHER, decode_plus_encode_is_the_same_string_1) {
     CeasarCipher CC;
     const std::string test = "ABC";
+
     const std::string dec = CC.Decode(test, 1);
     const std::string encdec = CC.Encode(dec, 1);
+
     ASSERT_EQ(test, encdec);
 }
 
 TEST(CEASAR_CIPHER, decode_plus_encode_is_the_same_string_random_key) {
     CeasarCipher CC;
+    std::mt19937 gen;
+    gen.seed(time(0) + 1);
     const std::string test = "ABCabc";
-    int key = rand() % 100;
+    int key = gen() % 100;
+
     const std::string dec = CC.Decode(test, key);
     const std::string encdec = CC.Encode(dec, key);
+
     ASSERT_EQ(test, encdec);
 }
 
@@ -102,7 +125,10 @@ TEST(CEASAR_CIPHER, decode_plus_encode_is_the_same_string_random_key) {
 TEST(CEASAR_CIPHER, decode_plus_encode_is_the_same_now_big_string_random_key) {
     CeasarCipher CC;
     const std::string test = "I stood there, upright in front of the revolving";
-    int key = rand() % 100;
+    std::mt19937 gen;
+    gen.seed(time(0) + 1);
+    int key = gen() % 100;
+
     const std::string dec = CC.Decode(test, key);
     const std::string encdec = CC.Encode(dec, key);
     ASSERT_EQ(test, encdec);
@@ -110,7 +136,9 @@ TEST(CEASAR_CIPHER, decode_plus_encode_is_the_same_now_big_string_random_key) {
 
 TEST(CEASAR_CIPHER, decode_twice_with_random_key_equals_decode_with_double_k) {
     CeasarCipher CC;
-    int key = rand() % 100;
+    std::mt19937 gen;
+    gen.seed(time(0) + 1);
+    int key = gen() % 100;
     const std::string test = "Pulling slightly on the collar of my trench coat";
     const std::string dec = CC.Decode(test, key);
     const std::string decdec = CC.Decode(dec, key);
@@ -120,7 +148,9 @@ TEST(CEASAR_CIPHER, decode_twice_with_random_key_equals_decode_with_double_k) {
 
 TEST(CEASAR_CIPHER, encode_twice_with_random_key_equal_decode_with_double_k) {
     CeasarCipher CC;
-    int key = rand() % 100;
+    std::mt19937 gen;
+    gen.seed(time(0) + 1);
+    int key = gen() % 100;
     std::string test = "Aside from my short-cropped hair, I wore a modes";
     const std::string enc = CC.Encode(test, key);
     const std::string encenc = CC.Encode(enc, key);
