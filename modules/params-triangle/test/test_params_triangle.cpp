6914cad9becbb1053c5b74930759cba991de3ce7
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2021-04-24 20:45:22 +0300

    Update test_params_triangle.cpp

--- modules/params-triangle/test/test_params_triangle.cpp
+++ modules/params-triangle/test/test_params_triangle.cpp
@@ -22,7 +22,7 @@ TEST(Params_Triangle, Calculate_SideAB) {
     std::pair<double, double>  c(3, 0);
     Triangle tr(a, b, c);
     double sideAB = tr.SideAB();
-    EXPECT_DOUBLE_EQ(4.0, sideAB);
+    ASSERT_DOUBLE_EQ(4.0, sideAB);
 }
 
 TEST(Params_Triangle, Calculate_SideBC) {
@@ -31,7 +31,7 @@ TEST(Params_Triangle, Calculate_SideBC) {
     std::pair<double, double>  c(3, 0);
     Triangle tr(a, b, c);
     double sideBC = tr.SideBC();
-    EXPECT_DOUBLE_EQ(5.0, sideBC);
+    ASSERT_DOUBLE_EQ(5.0, sideBC);
 }
 
 TEST(Params_Triangle, Calculate_SideAC) {
@@ -40,7 +40,7 @@ TEST(Params_Triangle, Calculate_SideAC) {
     std::pair<double, double>  c(3, 0);
     Triangle tr(a, b, c);
     double sideAC = tr.SideAC();
-    EXPECT_DOUBLE_EQ(3.0, sideAC);
+    ASSERT_DOUBLE_EQ(3.0, sideAC);
 }
 
 TEST(Params_Triangle, Calculate_Perimeter) {
@@ -49,7 +49,7 @@ TEST(Params_Triangle, Calculate_Perimeter) {
     std::pair<double, double>  c(3, 0);
     Triangle tr(a, b, c);
     double Perimeter = tr.Perimeter();
-    EXPECT_DOUBLE_EQ(12.0, Perimeter);
+    ASSERT_DOUBLE_EQ(12.0, Perimeter);
 }
 
 TEST(Params_Triangle, Calculate_Area) {
@@ -58,5 +58,5 @@ TEST(Params_Triangle, Calculate_Area) {
     std::pair<double, double>  c(3, 0);
     Triangle tr(a, b, c);
     double Area = tr.Area();
-    EXPECT_DOUBLE_EQ(6.0, Area);
+    ASSERT_DOUBLE_EQ(6.0, Area);
 }
