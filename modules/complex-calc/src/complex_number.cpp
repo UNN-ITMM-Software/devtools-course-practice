191c95f178d647c0a830ca4e628d2f16e84a2293
Kolesin Andrey <kolesinandrei1910@gmail.com>
2021-04-25 14:34:00 +0300

    unify returns

--- modules/complex-calc/src/complex_number.cpp
+++ modules/complex-calc/src/complex_number.cpp
@@ -1,10 +1,11 @@
 // Copyright 2021 Kolesin Andrey
 
+#include "include/complex_number.h"
+
 #include <math.h>
+
 #include <limits>
 #include <sstream>
-
-#include "include/complex_number.h"
 
 double eps = std::numeric_limits<double>::epsilon();
 
@@ -37,7 +38,7 @@ const Complex Complex::operator*(const Complex& comp) {
 }
 const Complex Complex::operator/(const Complex& comp) {
     double d = comp.real * comp.real + comp.imag * comp.imag;
-    double re = real * comp.real + imag * comp.imag;
-    double im = -real * comp.imag + imag * comp.real;
-    return Complex(re / d, im / d);
+    double re = (real * comp.real + imag * comp.imag) / d;
+    double im = (-real * comp.imag + imag * comp.real) / d;
+    return Complex(re, im);
 }
