b4aad046e86e22f61b6514239dd7497b1473e325
Andrew <andrey.aleksandrychev2@mail.ru>
2021-04-26 17:15:38 +0300

    Coverage complete v_5

--- modules/grosfeld-cipher/src/grosfeld_cipher.cpp
+++ modules/grosfeld-cipher/src/grosfeld_cipher.cpp
@@ -42,7 +42,7 @@ std::string GronsfeldCipher::getCipher() {
 
         for (int i = 0; i < static_cast<int>(this->keyString.length()); i++) {
             int curOffset =
-                std::stoi(std::to_string(this->sourceString[i])) - 48;
+                std::stoi(std::to_string(this->keyString[i])) - 48;
             int letterPlace = alphabet.find(this->sourceString[i], 0);
             int newLetterIndex = this->fixOverflow(letterPlace + curOffset,
                 0, alphabet.length() - 1);
