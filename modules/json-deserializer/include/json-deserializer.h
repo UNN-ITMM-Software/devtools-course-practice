a0aecbdd7d4416b988e04c232045a4359900680d
Denis Ostapovich <vofksden@gmail.com>
2021-05-11 20:26:12 +0300

    moved to google code style in VS

--- modules/json-deserializer/include/json-deserializer.h
+++ modules/json-deserializer/include/json-deserializer.h
@@ -3,343 +3,340 @@
 #ifndef MODULES_JSON_DESERIALIZER_INCLUDE_JSON_DESERIALIZER_H_
 #define MODULES_JSON_DESERIALIZER_INCLUDE_JSON_DESERIALIZER_H_
 
-#include <string>
-#include <regex>
-#include <list>
-#include <utility>
-#include <ostream>
-#include <map>
 #include <algorithm>
+#include <list>
+#include <map>
+#include <ostream>
+#include <regex>
+#include <string>
+#include <utility>
 #include <vector>
 
 namespace simple {
-    class JsonNode;
-    class JsonData;
-    enum class TokenType;
-    struct Token;
+class JsonNode;
+class JsonData;
+enum class TokenType;
+struct Token;
 
-    typedef std::map<std::string, JsonNode> JSONObject;
-    typedef std::vector<JsonNode> JSONArray;
-    typedef std::pair<TokenType, std::string> specification;
+typedef std::map<std::string, JsonNode> JSONObject;
+typedef std::vector<JsonNode> JSONArray;
+typedef std::pair<TokenType, std::string> specification;
 
-    bool operator==(const JsonData& lhs, const JsonData& rhs);
-    bool operator!=(const JsonData& lhs, const JsonData& rhs);
-    bool operator==(const JsonNode& lhs, const JsonNode& rhs);
-    bool operator!=(const JsonNode& lhs, const JsonNode& rhs);
-    bool operator==(const Token& lhs, const Token& rhs);
-    bool operator!=(const Token& lhs, const Token& rhs);
+bool operator==(const JsonData& lhs, const JsonData& rhs);
+bool operator!=(const JsonData& lhs, const JsonData& rhs);
+bool operator==(const JsonNode& lhs, const JsonNode& rhs);
+bool operator!=(const JsonNode& lhs, const JsonNode& rhs);
+bool operator==(const Token& lhs, const Token& rhs);
+bool operator!=(const Token& lhs, const Token& rhs);
 
-    enum class TokenType {
-        Number,
-        String,
-        Object,
-        Array,
-        Boolean,
-        Null,
-        Whitespace,
-        Delimiter,
-        Colon,
-        LeftBrace,
-        RightBrace,
-        LeftBracket,
-        RightBracket,
-        Eof,
-        Unknown
-    };
+enum class TokenType {
+  Number,
+  String,
+  Object,
+  Array,
+  Boolean,
+  Null,
+  Whitespace,
+  Delimiter,
+  Colon,
+  LeftBrace,
+  RightBrace,
+  LeftBracket,
+  RightBracket,
+  Eof,
+  Unknown
+};
 
-    inline
-        std::ostream& operator<<(std::ostream& out, const TokenType& type) {
-        switch (type) {
-        case TokenType::Number: {
-            out << "Number";
-            break;
-        }
-        case TokenType::String: {
-            out << "String";
-            break;
-        }
-        case TokenType::Object: {
-            out << "Object";
-            break;
-        }
-        case TokenType::Array: {
-            out << "Array";
-            break;
-        }
-        case TokenType::Boolean: {
-            out << "Boolean";
-            break;
-        }
-        case TokenType::Null: {
-            out << "Null";
-            break;
-        }
-        case TokenType::Whitespace: {
-            out << "Whitespace";
-            break;
-        }
-        case TokenType::Delimiter: {
-            out << "Delimiter";
-            break;
-        }
-        case TokenType::Colon: {
-            out << "Colon";
-            break;
-        }
-        case TokenType::LeftBrace: {
-            out << "LeftBrace";
-            break;
-        }
-        case TokenType::RightBrace: {
-            out << "RightBrace";
-            break;
-        }
-        case TokenType::LeftBracket: {
-            out << "LeftBracket";
-            break;
-        }
-        case TokenType::RightBracket: {
-            out << "RightBracket";
-            break;
-        }
-        case TokenType::Eof: {
-            out << "Eof";
-            break;
-        }
-        case TokenType::Unknown: {
-            out << "Unknown";
-            break;
-        }
-        }
+inline std::ostream& operator<<(std::ostream& out, const TokenType& type) {
+  switch (type) {
+    case TokenType::Number: {
+      out << "Number";
+      break;
+    }
+    case TokenType::String: {
+      out << "String";
+      break;
+    }
+    case TokenType::Object: {
+      out << "Object";
+      break;
+    }
+    case TokenType::Array: {
+      out << "Array";
+      break;
+    }
+    case TokenType::Boolean: {
+      out << "Boolean";
+      break;
+    }
+    case TokenType::Null: {
+      out << "Null";
+      break;
+    }
+    case TokenType::Whitespace: {
+      out << "Whitespace";
+      break;
+    }
+    case TokenType::Delimiter: {
+      out << "Delimiter";
+      break;
+    }
+    case TokenType::Colon: {
+      out << "Colon";
+      break;
+    }
+    case TokenType::LeftBrace: {
+      out << "LeftBrace";
+      break;
+    }
+    case TokenType::RightBrace: {
+      out << "RightBrace";
+      break;
+    }
+    case TokenType::LeftBracket: {
+      out << "LeftBracket";
+      break;
+    }
+    case TokenType::RightBracket: {
+      out << "RightBracket";
+      break;
+    }
+    case TokenType::Eof: {
+      out << "Eof";
+      break;
+    }
+    case TokenType::Unknown: {
+      out << "Unknown";
+      break;
+    }
+  }
 
-        return out;
+  return out;
+}
+
+enum class NodeType {
+  Unknown,
+  Numeric,
+  String,
+  Object,
+  Array,
+  Boolean,
+  Null,
+  ArrayEnd,
+  ObjectEnd,
+  Delimiter,
+  Colon
+};
+
+class JsonData {
+ public:
+  JsonData();
+  explicit JsonData(const std::string value);
+  explicit JsonData(const JSONObject& object);
+  explicit JsonData(const JSONArray& array);
+  JsonData(const JsonData& other);
+
+  std::string& getValue();
+
+  JsonNode& operator[](std::string key);
+  JsonNode& operator[](int index);
+
+  bool equals(const JsonData& other) const;
+
+  /* bool operator==(const JsonData& other);
+  bool operator!=(const JsonData& other);*/
+
+  JsonData& operator=(const JsonData& other);
+
+ private:
+  JSONObject object;
+  JSONArray array;
+  std::string value;
+};
+
+class JsonNode {
+ public:
+  JsonNode();
+  explicit JsonNode(const NodeType type);
+  JsonNode(const JsonNode& other);
+  JsonNode(const NodeType type, const JsonData& data);
+  JsonNode& operator=(const JsonNode& other);
+  ~JsonNode();
+
+  NodeType getNodeType() const;
+  void setData(const JsonData& data);
+  JsonData& getData();
+
+  bool equals(const JsonNode& other) const;
+
+  /* bool operator==(const JsonNode& other);
+  bool operator!=(const JsonNode& other); */
+
+  template <class type>
+  type to(const type& defaultValue = type()) {
+    if (nodeType == NodeType::Unknown) {
+      return defaultValue;
     }
 
-    enum class NodeType {
-        Unknown,
-        Numeric,
-        String,
-        Object,
-        Array,
-        Boolean,
-        Null,
-        ArrayEnd,
-        ObjectEnd,
-        Delimiter,
-        Colon
-    };
+    std::string data = this->data->getValue();
+    return type(data);
+  }
 
-    class JsonData {
-    public:
-        JsonData();
-        explicit JsonData(const std::string value);
-        explicit JsonData(const JSONObject& object);
-        explicit JsonData(const JSONArray& array);
-        JsonData(const JsonData& other);
+ protected:
+  NodeType nodeType;
+  JsonData* data;
+};
 
-        std::string& getValue();
+class JsonDocument {
+ public:
+  JsonDocument();
+  JsonDocument(const JsonDocument& other);
+  JsonDocument& operator=(const JsonDocument& other);
+  ~JsonDocument();
 
-        JsonNode& operator[] (std::string key);
-        JsonNode& operator[] (int index);
+  JsonNode& getRoot() const;
+  void setRoot(const JsonNode& rootNode);
+  bool empty();
 
-        bool equals(const JsonData& other) const;
+  JsonNode& operator[](std::string key);
+  JsonNode& operator[](int index);
 
-        /* bool operator==(const JsonData& other);
-        bool operator!=(const JsonData& other);*/
+ private:
+  JsonNode* rootNode;
+  bool isEmpty;
+};
 
-        JsonData& operator=(const JsonData& other);
+struct Token {
+  TokenType tokenType;
+  std::string value;
 
-    private:
-        JSONObject object;
-        JSONArray array;
-        std::string value;
-    };
+  explicit Token(const TokenType type, const std::string& value = "") {
+    this->tokenType = type;
+    this->value = value;
+  }
 
-    class JsonNode {
-    public:
-        JsonNode();
-        explicit JsonNode(const NodeType type);
-        JsonNode(const JsonNode& other);
-        JsonNode(const NodeType type, const JsonData& data);
-        JsonNode& operator=(const JsonNode& other);
-        ~JsonNode();
+  Token(const Token& other) {
+    this->tokenType = other.tokenType;
+    this->value = other.value;
+  }
 
-        NodeType getNodeType() const;
-        void setData(const JsonData& data);
-        JsonData& getData();
+  bool equals(const Token& other) const {
+    return this->tokenType == other.tokenType && this->value == other.value;
+  }
 
-        bool equals(const JsonNode& other) const;
+  /* bool operator==(const Token& other) {
+      return this->tokenType == other.tokenType
+          && this->value == other.value;
+  }
 
-        /* bool operator==(const JsonNode& other);
-        bool operator!=(const JsonNode& other); */
+  bool operator!=(const Token& other) {
+      return !(*this == other);
+  } */
 
-        template <class type>
-        type to(const type& defaultValue = type()) {
-            if (nodeType == NodeType::Unknown) {
-                return defaultValue;
-            }
+  friend std::ostream& operator<<(std::ostream& out, const Token& token) {
+    out << "{\n\tType: " << token.tokenType << std::endl;
+    out << "\tValue: \"" << token.value << "\"\n}" << std::endl;
 
-            std::string data = this->data->getValue();
-            return type(data);
-        }
+    return out;
+  }
 
-    protected:
-        NodeType nodeType;
-        JsonData* data;
-    };
-
-    class JsonDocument {
-    public:
-        JsonDocument();
-        JsonDocument(const JsonDocument& other);
-        JsonDocument& operator=(const JsonDocument& other);
-        ~JsonDocument();
-
-        JsonNode& getRoot() const;
-        void setRoot(const JsonNode& rootNode);
-        bool empty();
-
-        JsonNode& operator[] (std::string key);
-        JsonNode& operator[] (int index);
-
-    private:
-        JsonNode* rootNode;
-        bool isEmpty;
-    };
-
-    struct Token {
-        TokenType tokenType;
-        std::string value;
-
-        explicit Token(const TokenType type, const std::string& value = "") {
-            this->tokenType = type;
-            this->value = value;
-        }
-
-        Token(const Token& other) {
-            this->tokenType = other.tokenType;
-            this->value = other.value;
-        }
-
-        bool equals(const Token& other) const{
-            return this->tokenType == other.tokenType
-                && this->value == other.value;
-        }
-
-        /* bool operator==(const Token& other) {
-            return this->tokenType == other.tokenType
-                && this->value == other.value;
-        }
-
-        bool operator!=(const Token& other) {
-            return !(*this == other);
-        } */
-
-        friend std::ostream& operator<<(std::ostream& out, const Token& token) {
-            out << "{\n\tType: " << token.tokenType << std::endl;
-            out << "\tValue: \"" << token.value << "\"\n}" << std::endl;
-
-            return out;
-        }
-
-        Token& operator=(const Token& other) {
-            if (this != &other) {
-                tokenType = other.tokenType;
-                value = other.value;
-            }
-
-            return *this;
-        }
-    };
-
-    class Lexer {
-    public:
-        Lexer();
-        explicit Lexer(const std::string& string);
-
-        std::string getString();
-        size_t getCursor();
-        void setString(const std::string& string);
-        Token getNextToken();
-        bool hasTokens();
-        std::vector<Token> getTokens();
-
-    private:
-        std::list<specification> specifications{
-           specification(TokenType::String, "^\"[^\"]*\""),
-           specification(TokenType::Number, "^-?\\d+"),
-           specification(TokenType::Whitespace, "^\\s+"),
-           specification(TokenType::Null, "^null"),
-           specification(TokenType::Boolean, "^(true|false)"),
-           specification(TokenType::Delimiter, "^,"),
-           specification(TokenType::Colon, "^:"),
-           specification(TokenType::LeftBrace, "^\\{"),
-           specification(TokenType::RightBrace, "^\\}"),
-           specification(TokenType::LeftBracket, "^\\["),
-           specification(TokenType::RightBracket, "^\\]"),
-        };
-        size_t cursor;
-        std::string string;
-        std::string match(const std::string& expression, const std::string& src);
-    };
-
-    class JsonDeserializer {
-    public:
-        JsonDeserializer();
-        explicit JsonDeserializer(const std::string& jsonString);
-        ~JsonDeserializer();
-
-        JsonDocument parse();
-        JsonDocument parse(const std::string& jsonString);
-
-        Lexer getLexer();
-
-    private:
-        Lexer lexer;
-        Token* lookahead;
-        Token eat(const TokenType tokenType);
-        JsonNode literal();
-        JsonNode numericLiteral();
-        JsonNode stringLiteral();
-        JsonNode nullLiteral();
-        JsonNode booleanLiteral();
-        JsonNode array();
-        JsonNode object();
-    };
-
-    bool equalsIgnoreCase(std::string lhs, std::string rhs);
-
-    template <> inline
-        bool JsonNode::to(const bool& defaultValue) {
-        if (nodeType == NodeType::Unknown) {
-            return defaultValue;
-        }
-
-        std::string data = this->data->getValue();
-
-        if (equalsIgnoreCase(data, "true")) {
-            return true;
-        }
-
-        return false;
+  Token& operator=(const Token& other) {
+    if (this != &other) {
+      tokenType = other.tokenType;
+      value = other.value;
     }
 
-    template <> inline
-        int JsonNode::to<int>(const int& defaultValue) {
-        if (nodeType == NodeType::Unknown) {
-            return defaultValue;
-        }
+    return *this;
+  }
+};
 
-        return atoi(this->data->getValue().c_str());
-    }
+class Lexer {
+ public:
+  Lexer();
+  explicit Lexer(const std::string& string);
 
-    inline
-        bool equalsIgnoreCase(std::string lhs, std::string rhs) {
-        std::transform(lhs.begin(), lhs.end(), lhs.begin(), ::tolower);
-        std::transform(rhs.begin(), rhs.end(), rhs.begin(), ::tolower);
+  std::string getString();
+  size_t getCursor();
+  void setString(const std::string& string);
+  Token getNextToken();
+  bool hasTokens();
+  std::vector<Token> getTokens();
 
-        return lhs.compare(rhs) == 0;
-    }
-} // namespace simple
+ private:
+  std::list<specification> specifications{
+      specification(TokenType::String, "^\"[^\"]*\""),
+      specification(TokenType::Number, "^-?\\d+"),
+      specification(TokenType::Whitespace, "^\\s+"),
+      specification(TokenType::Null, "^null"),
+      specification(TokenType::Boolean, "^(true|false)"),
+      specification(TokenType::Delimiter, "^,"),
+      specification(TokenType::Colon, "^:"),
+      specification(TokenType::LeftBrace, "^\\{"),
+      specification(TokenType::RightBrace, "^\\}"),
+      specification(TokenType::LeftBracket, "^\\["),
+      specification(TokenType::RightBracket, "^\\]"),
+  };
+  size_t cursor;
+  std::string string;
+  std::string match(const std::string& expression, const std::string& src);
+};
+
+class JsonDeserializer {
+ public:
+  JsonDeserializer();
+  explicit JsonDeserializer(const std::string& jsonString);
+  ~JsonDeserializer();
+
+  JsonDocument parse();
+  JsonDocument parse(const std::string& jsonString);
+
+  Lexer getLexer();
+
+ private:
+  Lexer lexer;
+  Token* lookahead;
+  Token eat(const TokenType tokenType);
+  JsonNode literal();
+  JsonNode numericLiteral();
+  JsonNode stringLiteral();
+  JsonNode nullLiteral();
+  JsonNode booleanLiteral();
+  JsonNode array();
+  JsonNode object();
+};
+
+bool equalsIgnoreCase(std::string lhs, std::string rhs);
+
+template <>
+inline bool JsonNode::to(const bool& defaultValue) {
+  if (nodeType == NodeType::Unknown) {
+    return defaultValue;
+  }
+
+  std::string data = this->data->getValue();
+
+  if (equalsIgnoreCase(data, "true")) {
+    return true;
+  }
+
+  return false;
+}
+
+template <>
+inline int JsonNode::to<int>(const int& defaultValue) {
+  if (nodeType == NodeType::Unknown) {
+    return defaultValue;
+  }
+
+  return atoi(this->data->getValue().c_str());
+}
+
+inline bool equalsIgnoreCase(std::string lhs, std::string rhs) {
+  std::transform(lhs.begin(), lhs.end(), lhs.begin(), ::tolower);
+  std::transform(rhs.begin(), rhs.end(), rhs.begin(), ::tolower);
+
+  return lhs.compare(rhs) == 0;
+}
+}  // namespace simple
 
 #endif  // MODULES_JSON_DESERIALIZER_INCLUDE_JSON_DESERIALIZER_H_
