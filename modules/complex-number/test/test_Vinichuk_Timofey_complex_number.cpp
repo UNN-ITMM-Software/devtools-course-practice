6e62ad7ec4fe906789dd59b51041ddad7dd1c961
V-inTim <vinichuk.timofey@mail.ru>
2024-03-23 13:51:38 +0300

    cpplint changes

--- modules/complex-number/test/test_Vinichuk_Timofey_complex_number.cpp
+++ modules/complex-number/test/test_Vinichuk_Timofey_complex_number.cpp
@@ -21,29 +21,29 @@ TEST(Vinichuk_Timofey_ComplexNumberTest, test_sum_int_opposites_is_zero) {
 }
 
 TEST(Vinichuk_Timofey_ComplexNumberTest, test_sum_double_opposites_is_zero) {
-	ComplexNumber z1(2.4, -3.8);
-	ComplexNumber z2(-2.4, 3.8);
-	ComplexNumber p = z1 + z2;
-	ASSERT_EQ(0, p.getRe());
-	ASSERT_EQ(0, p.getIm());
+    ComplexNumber z1(2.4, -3.8);
+    ComplexNumber z2(-2.4, 3.8);
+    ComplexNumber p = z1 + z2;
+    ASSERT_EQ(0, p.getRe());
+    ASSERT_EQ(0, p.getIm());
 }
 
 TEST(Vinichuk_Timofey_ComplexNumberTest, test_invertion_int) {
-	ComplexNumber z(2, 2);
-	ComplexNumber z1(-2, -2);
-	ComplexNumber z2(0, 0);
-	ComplexNumber p = z2 - z;
-	ASSERT_EQ(z1.getRe(), p.getRe());
-	ASSERT_EQ(z1.getIm(), p.getIm());
+    ComplexNumber z(2, 2);
+    ComplexNumber z1(-2, -2);
+    ComplexNumber z2(0, 0);
+    ComplexNumber p = z2 - z;
+    ASSERT_EQ(z1.getRe(), p.getRe());
+    ASSERT_EQ(z1.getIm(), p.getIm());
 }
 
 TEST(Vinichuk_Timofey_ComplexNumberTest, test_invertion_double) {
-	ComplexNumber z(48.89, 67.45);
-	ComplexNumber z1(-48.89, -67.45);
-	ComplexNumber z2(0, 0);
-	ComplexNumber p = z2 - z;
-	ASSERT_EQ(z1.getRe(), p.getRe());
-	ASSERT_EQ(z1.getIm(), p.getIm());
+    ComplexNumber z(48.89, 67.45);
+    ComplexNumber z1(-48.89, -67.45);
+    ComplexNumber z2(0, 0);
+    ComplexNumber p = z2 - z;
+    ASSERT_EQ(z1.getRe(), p.getRe());
+    ASSERT_EQ(z1.getIm(), p.getIm());
 }
 
 
