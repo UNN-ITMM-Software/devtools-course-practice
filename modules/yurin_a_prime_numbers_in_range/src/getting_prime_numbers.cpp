20e000e118c6257b074e9ccad29d80b89f6cae89
Yurin Andrey <andreyyurin@icloud.com>
2024-03-24 14:25:34 +0300

    рефакторинг (cpplint)

--- modules/yurin_a_prime_numbers_in_range/src/getting_prime_numbers.cpp
+++ modules/yurin_a_prime_numbers_in_range/src/getting_prime_numbers.cpp
@@ -5,7 +5,8 @@
 #include <algorithm>
 
 
-std::vector<uint32_t> GettingPrimeNumbers::getPrimeNumbersInRange(uint32_t lowerBound, uint32_t upperBound) {
+std::vector<uint32_t> GettingPrimeNumbers::getPrimeNumbersInRange(
+        uint32_t lowerBound, uint32_t upperBound) {
     _lowerBound = lowerBound;
     _upperBound = upperBound;
     validateInputData();
@@ -26,7 +27,8 @@ std::vector<uint32_t> GettingPrimeNumbers::getPrimeNumbersInRange(uint32_t lower
 
 void GettingPrimeNumbers::validateInputData() {
     if (_lowerBound > _upperBound) {
-        throw std::string("The left bound should be larger than the right one!");
+        throw std::string(
+                "The left bound should be larger than the right one!");
     }
 
     if (_upperBound == UINT32_MAX) {
@@ -36,7 +38,8 @@ void GettingPrimeNumbers::validateInputData() {
 
 std::vector<uint32_t> GettingPrimeNumbers::generateListOfPrimeNumbers() {
     std::vector<uint32_t> primeNumbers{};
-    for (uint64_t p = std::max(_lowerBound, uint32_t{2}); p <= _upperBound; ++p) {
+    uint64_t startP = std::max(_lowerBound, uint32_t{2});
+    for (uint64_t p = startP; p <= _upperBound; ++p) {
         if (_isPrime[p]) {
             primeNumbers.push_back(p);
         }
