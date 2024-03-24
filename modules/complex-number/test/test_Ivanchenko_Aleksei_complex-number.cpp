d6a8bd559fd0c9b55227e0fdde4e5ea5f16d03c5
aaldebarann <amivanchenko@yandex.ru>
2024-03-24 17:18:17 +0300

    fraction tests added

--- modules/complex-number/test/test_Ivanchenko_Aleksei_complex-number.cpp
+++ modules/complex-number/test/test_Ivanchenko_Aleksei_complex-number.cpp
@@ -82,3 +82,55 @@ TEST(Ivanchenko_Aleksei_ComplexNumberTest, Can_Divide) {
     EXPECT_EQ(re1, z.getRe());
     EXPECT_EQ(im1, z.getIm());
 }
+TEST(Ivanchenko_Aleksei_ComplexNumberTest, Can_Add_Fraction) {
+    // Arrange
+    double re1 = 1.1, re2 = 3.3;
+    double im1 = 2.1, im2 = -4.4;
+
+    // Act
+    ComplexNumber z1(re1, im1), z2(re2, im2);
+    ComplexNumber z = z1 + z2;
+
+    // Assert
+    EXPECT_DOUBLE_EQ(re1 + re2, z.getRe());
+    EXPECT_DOUBLE_EQ(im1 + im2, z.getIm());
+}
+TEST(Ivanchenko_Aleksei_ComplexNumberTest, Can_Subtract_Fraction) {
+    // Arrange
+    double re1 = 1.1, re2 = 3.3;
+    double im1 = 2.2, im2 = -4.4;
+
+    // Act
+    ComplexNumber z1(re1, im1), z2(re2, im2);
+    ComplexNumber z = z1 - z2;
+
+    // Assert
+    EXPECT_DOUBLE_EQ(re1 - re2, z.getRe());
+    EXPECT_DOUBLE_EQ(im1 - im2, z.getIm());
+}
+TEST(Ivanchenko_Aleksei_ComplexNumberTest, Can_Multiply_Fraction) {
+    // Arrange
+    double re1 = 1.1, re2 = 3.3;
+    double im1 = 2.2, im2 = -4.4;
+
+    // Act
+    ComplexNumber z1(re1, im1), z2(re2, im2);
+    ComplexNumber z = z1 * z2;
+
+    // Assert
+    EXPECT_DOUBLE_EQ(re1 * re2 - im1 * im2, z.getRe());
+    EXPECT_DOUBLE_EQ(re1 * im2 + re2 * im1, z.getIm());
+}
+TEST(Ivanchenko_Aleksei_ComplexNumberTest, Can_Divide_Fraction) {
+    // Arrange
+    double re1 = 100.7, re2 = 0.5;
+    double im1 = 2.9, im2 = 0.0;
+
+    // Act
+    ComplexNumber z1(re1, im1), z2(re2, im2);
+    ComplexNumber z = z1 / z2;
+
+    // Assert
+    EXPECT_DOUBLE_EQ(re1 / re2, z.getRe());
+    EXPECT_DOUBLE_EQ(im1 / re2, z.getIm());
+}
