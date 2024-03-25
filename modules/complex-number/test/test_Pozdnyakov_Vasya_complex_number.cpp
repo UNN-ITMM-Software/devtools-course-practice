8ab5297bccd2c8a5c4d79e7f78f788f81b88d8c5
Василий Поздняков <pozvas03@mail.ru>
2024-03-25 11:12:34 +0300

    codestyle fix

--- modules/complex-number/test/test_Pozdnyakov_Vasya_complex_number.cpp
+++ modules/complex-number/test/test_Pozdnyakov_Vasya_complex_number.cpp
@@ -5,7 +5,6 @@
 #include "include/complex_number.h"
 
 TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_sum_int) {
-
     ComplexNumber z1(1, 2);
     ComplexNumber z2(-5, 4);
     ComplexNumber res = z1 + z2;
@@ -15,7 +14,6 @@ TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_sum_int) {
 }
 
 TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_sub_int) {
-
     ComplexNumber z1(4, -2);
     ComplexNumber z2(2, -1);
     ComplexNumber res = z1 - z2;
@@ -25,7 +23,6 @@ TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_sub_int) {
 }
 
 TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_setRe_int) {
-
     ComplexNumber res(1, 2);
 
     res.setRe(10);
@@ -34,23 +31,20 @@ TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_setRe_int) {
 }
 
 TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_setIm_int) {
-
     ComplexNumber res(1, 2);
-    
+
     res.setIm(10);
 
     EXPECT_EQ(10, res.getIm());
 }
 
 TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_comprasion_themselves) {
-
     ComplexNumber z(3, -8);
 
     EXPECT_EQ(true, z == z);
 }
 
 TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_comprasion_different) {
-
     ComplexNumber z1(3, -8);
     ComplexNumber z2(3, -8);
 
