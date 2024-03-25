f230663a750d28a175dc31c68692821acfc733b3
Василий Поздняков <pozvas03@mail.ru>
2024-03-25 10:50:08 +0300

    added 4 tests

---
+++ modules/complex-number/test/test_Pozdnyakov_Vasya_complex_number.cpp
@@ -0,0 +1,43 @@
+// Copyright 2024 Pozdnyakov Vasya
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_sum_int) {
+
+    ComplexNumber z1(1, 2);
+    ComplexNumber z2(-5, 4);
+    ComplexNumber res = z1 + z2;
+
+    EXPECT_EQ(-4, res.getRe());
+    EXPECT_EQ(6, res.getIm());
+}
+
+TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_setRe_int) {
+
+    ComplexNumber res(1, 2);
+
+    res.setRe(10);
+
+    EXPECT_EQ(10, res.getRe());
+}
+
+TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_setIm_int) {
+
+    ComplexNumber res(1, 2);
+    
+    res.setIm(10);
+
+    EXPECT_EQ(10, res.getIm());
+}
+
+TEST(Pozdnaykov_Vasya_ComplexNumberTest, test_sub_int) {
+
+    ComplexNumber z1(4, -2);
+    ComplexNumber z2(2, -1);
+    ComplexNumber res = z1 - z2;
+
+    EXPECT_EQ(2, res.getRe());
+    EXPECT_EQ(-1, res.getIm());
+}
