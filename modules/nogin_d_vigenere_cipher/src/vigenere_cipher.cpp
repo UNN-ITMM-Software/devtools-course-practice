1f5ac8054a4374fd1523ed5a72de6962cd6606b2
Denchek <d.n0gin@yandex.ru>
2024-03-26 21:53:56 +0300

    fix style 4

--- modules/nogin_d_vigenere_cipher/src/vigenere_cipher.cpp
+++ modules/nogin_d_vigenere_cipher/src/vigenere_cipher.cpp
@@ -3,14 +3,14 @@
 #include "include/vigenere_cipher.h"
 
 namespace var {
-    extern const int lowerCaseBegin = 97;
-    extern const int lowerCaseEnd = 122;
-    extern const int upperCaseBegin = 65;
-    extern const int upperCaseEnd = 90;
-    extern const int alphabetSize = 26;
-}
+extern const int lowerCaseBegin = 97;
+extern const int lowerCaseEnd = 122;
+extern const int upperCaseBegin = 65;
+extern const int upperCaseEnd = 90;
+extern const int alphabetSize = 26;
+} // namespace var
 
-std::string vigenereCipher(const std::string& data, const std::string& key) {
+std::string vigenereCipher(const std::string &data, const std::string &key) {
   std::string transformKey = key;
   std::transform(transformKey.begin(), transformKey.end(), transformKey.begin(),
                  [](unsigned char symbol) { return std::tolower(symbol); });
@@ -24,11 +24,13 @@ std::string vigenereCipher(const std::string& data, const std::string& key) {
     unsigned char new_symbol;
     if ((data[i] >= var::upperCaseBegin) && (data[i] <= var::lowerCaseEnd) &&
         ((data[i] <= var::upperCaseEnd) || (data[i] >= var::lowerCaseBegin))) {
-      new_symbol = data[i] + transformKey[(i - space) % lenKey] - var::lowerCaseBegin;
+      new_symbol =
+          data[i] + transformKey[(i - space) % lenKey] - var::lowerCaseBegin;
       if ((data[i] <= var::upperCaseEnd) && (data[i] >= var::upperCaseBegin)) {
         while (new_symbol > var::upperCaseEnd)
           new_symbol -= var::alphabetSize;
-      } else if ((data[i] <= var::lowerCaseEnd) && (data[i] >= var::lowerCaseBegin)) {
+      } else if ((data[i] <= var::lowerCaseEnd) &&
+                 (data[i] >= var::lowerCaseBegin)) {
         while (new_symbol > var::lowerCaseEnd)
           new_symbol -= var::alphabetSize;
       }
@@ -42,7 +44,8 @@ std::string vigenereCipher(const std::string& data, const std::string& key) {
   return result;
 }
 
-std::string vigenereDecipher(const std::string& cipher, const std::string& key) {
+std::string vigenereDecipher(const std::string &cipher,
+                             const std::string &key) {
   std::string transformKey = key;
   std::transform(transformKey.begin(), transformKey.end(), transformKey.begin(),
                  [](unsigned char symbol) { return std::tolower(symbol); });
@@ -54,13 +57,18 @@ std::string vigenereDecipher(const std::string& cipher, const std::string& key) 
 
   for (int i = 0; i < lenCipher; ++i) {
     unsigned char new_symbol;
-    if ((cipher[i] >= var::upperCaseBegin) && (cipher[i] <= var::lowerCaseEnd) &&
-        ((cipher[i] <= var::upperCaseEnd) || (cipher[i] >= var::lowerCaseBegin))) {
-      new_symbol = cipher[i] - transformKey[(i - space) % lenKey] + var::lowerCaseBegin;
-      if ((cipher[i] <= var::upperCaseEnd) && (cipher[i] >= var::upperCaseBegin)) {
+    if ((cipher[i] >= var::upperCaseBegin) &&
+        (cipher[i] <= var::lowerCaseEnd) &&
+        ((cipher[i] <= var::upperCaseEnd) ||
+         (cipher[i] >= var::lowerCaseBegin))) {
+      new_symbol =
+          cipher[i] - transformKey[(i - space) % lenKey] + var::lowerCaseBegin;
+      if ((cipher[i] <= var::upperCaseEnd) &&
+          (cipher[i] >= var::upperCaseBegin)) {
         while (new_symbol < var::upperCaseBegin)
           new_symbol += var::alphabetSize;
-      } else if ((cipher[i] <= var::lowerCaseEnd) && (cipher[i] >= var::lowerCaseBegin)) {
+      } else if ((cipher[i] <= var::lowerCaseEnd) &&
+                 (cipher[i] >= var::lowerCaseBegin)) {
         while (new_symbol < var::lowerCaseBegin)
           new_symbol += var::alphabetSize;
       }
