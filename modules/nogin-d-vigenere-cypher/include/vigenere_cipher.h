d368a880bb4fbe5d2655169ff50913a2fad2fcab
Denchek <d.n0gin@yandex.ru>
2024-03-26 20:30:38 +0300

    fix

---
+++ modules/nogin-d-vigenere-cypher/include/vigenere_cipher.h
@@ -0,0 +1,14 @@
+// Copyright 2024 Nogin Denis
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#ifndef MODULES_NOGIN_D_VIGENERE_CIPHER_INCLUDE_VIGENERE_CIPHER_H_
+#define MODULES_NOGIN_D_VIGENERE_CIPHER_INCLUDE_VIGENERE_CIPHER_H_
+
+std::string vigenereCipher(std::string data, std::string key);
+
+std::string vigenereDecipher(std::string cypher, std::string key);
+
+#endif  // MODULES_NOGIN_D_VIGENERE_CIPHER_INCLUDE_VIGENERE_CIPHER_H_
--- modules/vigenere-cypher/include/vigenere_cipher.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-std::string vigenereCipher(std::string data, std::string key);
-
-std::string vigenereDecipher(std::string cypher, std::string key);
