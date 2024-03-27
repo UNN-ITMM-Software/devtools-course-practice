11280c7fbe79bc7c822bc6c561c36be47288e556
BlackAngel2108 <luba.sobol.nn@mail.ru>
2024-03-27 21:19:46 +0300

    second commit

--- modules/complex-number/test/test_Sobol_Liubov_complex_number.cpp
+++ modules/complex-number/test/test_Sobol_Liubov_complex_number.cpp
@@ -5,46 +5,38 @@
 #include "include/complex_number.h"
 
 TEST(Sobol_Liubov_ComplexNumberTest, Can_Add_Complex_numbers) {
-
-
     ComplexNumber z1(1.2, 2.2);
     ComplexNumber z2(3.2, 4.2);
 
-    ComplexNumber answer= z1+z2;
+    ComplexNumber answer = z1+z2;
 
-    EXPECT_EQ(4.4, answer.getRe());
-    EXPECT_EQ(6.4, answer.getIm());
+    ASSERT_DOUBLE_EQ(4.4, answer.getRe());
+    ASSERT_DOUBLE_EQ(6.4, answer.getIm());
 }
 TEST(Sobol_Liubov_ComplexNumberTest, Can_Multiply_Complex_numbers) {
-
-
     ComplexNumber z1(1.0, 1.0);
     ComplexNumber z2(2.0, 2.0);
 
     ComplexNumber answer = z1 * z2;
 
-    EXPECT_EQ(0.0, answer.getRe());
-    EXPECT_EQ(4.0, answer.getIm());
+    ASSERT_DOUBLE_EQ(0.0, answer.getRe());
+    ASSERT_DOUBLE_EQ(4.0, answer.getIm());
 }
 TEST(Sobol_Liubov_ComplexNumberTest, Can_Subtract_Complex_numbers) {
-
-
     ComplexNumber z1(1.0, 1.0);
     ComplexNumber z2(2.0, 2.0);
 
     ComplexNumber answer = z1 - z2;
 
-    EXPECT_EQ(-1.0, answer.getRe());
-    EXPECT_EQ(-1.0, answer.getIm());
+    ASSERT_DOUBLE_EQ(-1.0, answer.getRe());
+    ASSERT_DOUBLE_EQ(-1.0, answer.getIm());
 }
 TEST(Sobol_Liubov_ComplexNumberTest, Can_Divide_Complex_numbers) {
-
-
     ComplexNumber z1(1.0, 1.0);
     ComplexNumber z2(2.0, 2.0);
 
     ComplexNumber answer = z2/z1;
 
-    EXPECT_EQ(2, answer.getRe());
-    EXPECT_EQ(0.0, answer.getIm());
+    ASSERT_DOUBLE_EQ(2, answer.getRe());
+    ASSERT_DOUBLE_EQ(0.0, answer.getIm());
 }
