eca253b0e80c83508b9ee362d279c51d26c2ea78
hadnle-less <salaev-08@mail.ru>
2024-03-29 18:39:15 +0300

    Updated tests

--- modules/complex-number/test/test_salaev_vlad_complex_number.cpp
+++ modules/complex-number/test/test_salaev_vlad_complex_number.cpp
@@ -33,7 +33,7 @@ TEST(Salaev_Vlad_ComplexNumberTest, Can_Copy_Complex_Number) {
 }
 
 TEST(Salaev_Vlad_ComplexNumberTest, Can_Assign_Complex_Numbers) {
-    ComplexNumber z1(5.0, 4.0);
+    ComplexNumber z1(3.4, 6.1);
     ComplexNumber z2 = z1;
 
     EXPECT_EQ(z1.getRe(), z2.getRe());
@@ -41,12 +41,12 @@ TEST(Salaev_Vlad_ComplexNumberTest, Can_Assign_Complex_Numbers) {
 }
 
 TEST(Salaev_Vlad_ComplexNumberTest, Can_Add_Complex_Numbers) {
-    ComplexNumber z1(1.0, 2.0);
-    ComplexNumber z2(3.0, 4.0);
+    ComplexNumber z1(5.6, 1.1);
+    ComplexNumber z2(4.5, 3.3);
     ComplexNumber z = z1 + z2;
 
-    EXPECT_EQ(4.0, z.getRe());
-    EXPECT_EQ(6.0, z.getIm());
+    EXPECT_EQ(10.1, z.getRe());
+    EXPECT_EQ(4.4, z.getIm());
 }
 
 TEST(Salaev_Vlad_ComplexNumberTest, Can_Subtract_Complex_Numbers) {
