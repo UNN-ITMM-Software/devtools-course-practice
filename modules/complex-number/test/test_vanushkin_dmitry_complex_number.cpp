199059615d635385c2ea7076d9943bf59a907fcc
Dmitry <xtopz1337@gmail.com>
2024-03-29 21:38:02 +0300

    change: add 3 additional tests

--- modules/complex-number/test/test_vanushkin_dmitry_complex_number.cpp
+++ modules/complex-number/test/test_vanushkin_dmitry_complex_number.cpp
@@ -31,3 +31,36 @@ TEST(Vanushkin_Dmitry_ComplexNumberTest, MultiplyByConjugateComplexNumber) {
 
     ASSERT_EQ(number * conjugateNumber, expectProduct);
 }
+
+TEST(Vanushkin_Dmitry_ComplexNumberTest, InvertComplexNumber) {
+    auto real = 81.62062;
+    auto imaginary = 0.999952;
+
+    auto divisible = ComplexNumber(1, 0);
+    auto divider = ComplexNumber(real, imaginary);
+
+    auto expectDivision = ComplexNumber(
+            real / (real * real + imaginary * imaginary),
+            -imaginary / (real * real + imaginary * imaginary));
+
+    ASSERT_EQ(divisible / divider, expectDivision);
+}
+
+TEST(Vanushkin_Dmitry_ComplexNumberTest, AddComplexNumbers) {
+    auto first = ComplexNumber(955.1585925, -91.999952);
+    auto second = ComplexNumber(-39, 91.999952);
+
+    auto expectAdd = ComplexNumber(first.getRe() + second.getRe(), 0);
+
+    ASSERT_EQ(first + second, expectAdd);
+}
+
+TEST(Vanushkin_Dmitry_ComplexNumberTest,
+     EqualsFractionalPartOfComplexNumbersNearZero) {
+    auto first = ComplexNumber(3.000000000000000000000003,
+                               15.0000000000000000000000006);
+    auto second = ComplexNumber(3.0000000000000000000000091,
+                                15.0000000000000000000000008);
+
+    ASSERT_EQ(first, second);
+}
