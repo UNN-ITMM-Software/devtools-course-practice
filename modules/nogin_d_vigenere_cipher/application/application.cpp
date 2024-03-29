0c624583b8a98eb75d65962af0c55d38e22a29f2
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-03-29 20:52:16 +0300

    create template app

---
+++ modules/nogin_d_vigenere_cipher/application/application.cpp
@@ -0,0 +1,15 @@
+// Copyright 2024 Kuznetsov Artem
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// #include <string>
+
+#include "include/vigenere_cipher_app.h"
+
+int main(int argc, char* argv[]) {
+//   ComplexCalculator app;
+//   std::string output = app(argc, argv);
+//   printf("%s\n", output.c_str());
+  return 0;
+}
---
+++ modules/nogin_d_vigenere_cipher/include/vigenere_cipher_app.h
@@ -0,0 +1,6 @@
+// Copyright 2024 Kuznetsov Artem
+#pragma once
+
+#include "include/vigenere_cipher_app.h"
+
+class VigenereApplication {};
