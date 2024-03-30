d67190b228c6e37a848dff9f11872e942716a964
ThreeToads <ivan.kokin.465@gmail.com>
2024-03-31 01:03:41 +0300

    fix lab1

--- modules/complex-number/test/test_Kokin_Ivan_complex_number.cpp
+++ modules/complex-number/test/test_Kokin_Ivan_complex_number.cpp
@@ -2,11 +2,10 @@
 
 #include <gtest/gtest.h>
 
-#include "../include/complex_number.h"
+#include "/include/complex_number.h"
 
 TEST(Kokin_Ivan_ComplexNumberTest, Getto) {
     ComplexNumber z(222.875, 23.454);
-
     ASSERT_DOUBLE_EQ(z.getRe(), 222.875);
     ASSERT_DOUBLE_EQ(z.getIm(), 23.454);
 }
@@ -15,18 +14,16 @@ TEST(Kokin_Ivan_ComplexNumberTest, CanUmn) {
     ComplexNumber z1(11, 12);
     ComplexNumber z2(13, 14);
     ComplexNumber z3 = z1 * z2;
-
     ASSERT_EQ(z3.getRe(), 143);
     ASSERT_EQ(z3.getIm(), 168);
 }
 
 TEST(Kokin_Ivan_ComplexNumberTest, Setto) {
-    ComplexNumber s1{};
-    s1.setRe(121.45);
-    s1.setIm(434.21);
-
-    ASSERT_DOUBLE_EQ(s1.getRe(), 121.45);
-    ASSERT_DOUBLE_EQ(s1.getIm(), 434.21);
+    ComplexNumber z1{};
+    z1.setRe(121.45);
+    z1.setIm(434.21);
+    ASSERT_DOUBLE_EQ(z1.getRe(), 121.45);
+    ASSERT_DOUBLE_EQ(z1.getIm(), 434.21);
 }
 
 TEST(Kokin_Ivan_ComplexNumberTest, falseTrue) {
