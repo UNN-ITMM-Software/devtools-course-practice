49a56f4f67512e79bfbc8e7577c749913650a907
esthete014 <nikkochetov@outlook.com>
2024-03-31 00:38:50 +0300

    Kochetov-Nikolay-lab1

--- modules/complex-number/test/test_kochetov_nikolay_complex_number.cpp
+++ modules/complex-number/test/test_kochetov_nikolay_complex_number.cpp
@@ -1,17 +1,13 @@
 // Copyright 2024 Kochetov Nikolay
-
 #include <gtest/gtest.h>
-
 #include "include/complex_number.h"
 
 TEST(Kochetov_Nikolay_ComplexNumberTest, Can_Create_Zero) {
     // Arrange
     double re = 0.0;
     double im = 0.0;
-
     // Act
     ComplexNumber z(re, im);
-
     // Assert
     EXPECT_EQ(re, z.getRe());
     EXPECT_EQ(im, z.getIm());
@@ -24,11 +20,9 @@ TEST(Kochetov_Nikolay_ComplexNumberTest, Can_Sum) {
     double re2 = 0.4;
     double im2 = 2.0;
     ComplexNumber z2(re2, im2);
-
     double re3 = 1.6;
     double im3 = 3.0;
     ComplexNumber z3(re3, im3);
-
     EXPECT_EQ(1, 1);
 }
 
@@ -37,7 +31,6 @@ TEST(Kochetov_Nikolay_ComplexNumberTest, Can_Equal) {
     double im = 2.0;
     ComplexNumber z1(re, im);
     ComplexNumber z2(re, im);
-    
     EXPECT_EQ(z1 == z2, true);
 }
 
@@ -48,11 +41,9 @@ TEST(Kochetov_Nikolay_ComplexNumberTest, Can_Subtract) {
     double re2 = 4.2;
     double im2 = 3.0;
     ComplexNumber z2(re2, im2);
-
     double re3 = -2.7;
     double im3 = -1.0;
     ComplexNumber z3(re3, im3);
-
     EXPECT_EQ(z1 - z2, z3);
 }
 
@@ -63,11 +54,9 @@ TEST(Kochetov_Nikolay_ComplexNumberTest, Can_Add_negative) {
     double re2 = -4.5;
     double im2 = -5.0;
     ComplexNumber z2(re2, im2);
-
     double re3 = -6.9;
     double im3 = -6.0;
     ComplexNumber z3(re3, im3);
-
     EXPECT_EQ(z1 + z2, z3);
 }
 
@@ -78,10 +67,8 @@ TEST(Kochetov_Nikolay_ComplexNumberTest, Can_Subtract_negative) {
     double re2 = -4.5;
     double im2 = -5.0;
     ComplexNumber z2(re2, im2);
-
     double re3 = 2.1;
     double im3 = 4.0;
     ComplexNumber z3(re3, im3);
-
     EXPECT_EQ(z1 - z2, z3);
 }
