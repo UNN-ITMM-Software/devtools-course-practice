edfd88518565ca0915c82b56bc05ef2f7afaf48a
Ivan Novostroev <54308529+GERONlMO@users.noreply.github.com>
2024-03-31 17:25:45 +0300

    Update test_Novostroev_Ivan_complex-number.cpp

--- modules/complex-number/test/test_Novostroev_Ivan_complex-number.cpp
+++ modules/complex-number/test/test_Novostroev_Ivan_complex-number.cpp
@@ -43,3 +43,64 @@ TEST(Novostroev_Ivan_ComplexNumberTest, Check_Equality) {
 
     ASSERT_TRUE(z1 == z2);
 }
+
+TEST(Novostroev_Ivan_ComplexNumberTest, Add) {
+    ComplexNumber z1(1, 2);
+    ComplexNumber z2(3, 4);
+    ComplexNumber z3 = z1 + z2;
+
+    ASSERT_EQ(z3.getRe(), 4);
+    ASSERT_EQ(z3.getIm(), 6);
+}
+
+TEST(Novostroev_Ivan_ComplexNumberTest, Subtract) {
+    ComplexNumber z1(1, 2);
+    ComplexNumber z2(3, 4);
+    ComplexNumber z3 = z1 - z2;
+
+    ASSERT_EQ(z3.getRe(), -2);
+    ASSERT_EQ(z3.getIm(), -2);
+}
+
+TEST(Novostroev_Ivan_ComplexNumberTest, Compare) {
+    ComplexNumber z1(1, 2);
+    ComplexNumber z2(3, 4);
+    ComplexNumber z3(1, 2);
+
+    ASSERT_TRUE(z1 == z3);
+    ASSERT_TRUE(z1 != z2);
+}
+
+TEST(Novostroev_Ivan_ComplexNumberTest, Change) {
+    ComplexNumber z1(1, 2);
+    z1.setRe(3);
+    z1.setIm(4);
+
+    ASSERT_EQ(z1.getRe(), 3);
+    ASSERT_EQ(z1.getIm(), 4);
+}
+
+TEST(Novostroev_Ivan_ComplexNumberTest, Create_Copy) {
+    ComplexNumber z1(1, 2);
+    ComplexNumber z2(z1);
+
+    ASSERT_TRUE(z1 == z2);
+}
+
+TEST(Novostroev_Ivan_ComplexNumberTest, Create_Default) {
+    ComplexNumber z1;
+
+    ASSERT_EQ(z1.getRe(), 0);
+    ASSERT_EQ(z1.getIm(), 0);
+}
+
+TEST(Novostroev_Ivan_ComplexNumberTest, DivisionOperatorWithFractions) {
+    ComplexNumber z1(2.25, 5.25);
+    ComplexNumber z2(-2.25, 5.25);
+
+    ComplexNumber z3 = z1 / z2;
+
+    ASSERT_NEAR(z3.getRe(), 0.6, 0.1);
+    ASSERT_NEAR(z3.getIm(), -0.7, 0.1);
+    ASSERT_ANY_THROW(ComplexNumber() / ComplexNumber());
+}
