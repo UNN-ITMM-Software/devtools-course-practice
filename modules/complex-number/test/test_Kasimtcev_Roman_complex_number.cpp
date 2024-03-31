d45f4614a32f79397c2f4578b1c31649aa93683b
Касимцев Роман <Romasias1@outlook.com>
2024-03-31 10:51:40 +0300

    Added new tests for lab1_3

--- modules/complex-number/test/test_Kasimtcev_Roman_complex_number.cpp
+++ modules/complex-number/test/test_Kasimtcev_Roman_complex_number.cpp
@@ -68,4 +68,42 @@ TEST(Kasimtcev_Roman_ComplexNumberTest, Can_Complex_Divide) {
     EXPECT_EQ(re1, z.getRe());
     EXPECT_EQ(im1, z.getIm());
 }
+TEST(Kasimtcev_Roman_ComplexNumberTest, Can_Complex_Multiply_Drob) {
+    // Arrange
+    double re1 = 5.7, re2 = -7.2;
+    double im1 = -7.2, im2 = 5.7;
+
+    // Act
+    ComplexNumber z1(re1, im1), z2(re2, im2);
+    ComplexNumber z = z1 * z2;
+
+    // Assert
+    EXPECT_EQ(re1 * re2 - im1 * im2, z.getRe());
+    EXPECT_EQ(re1 * im2 + re2 * im1, z.getIm());
+}
+TEST(Kasimtcev_Roman_ComplexNumberTest, Can_Complex_Divide_Drob) {
+    // Arrange
+    double re1 = 1.7, re2 = 1.4;
+    double im1 = 2.2, im2 = 1.3;
+
+    // Act
+    ComplexNumber z1(re1, im1), z2(re2, im2);
+    ComplexNumber z = z1 / z2;
+
+    // Assert
+    EXPECT_EQ(re1, z.getRe());
+    EXPECT_EQ(im1, z.getIm());
+}
+TEST(Khramov_Ivan_ComplexNumberTest, Can_Complex_Divide_Zero) {
+    // Arrange
+    double re1 = 2.5, re2=0.0;
+    double im1 = 3.1; im2=0.0;
+
+    // Act
+    ComplexNumber z1(re1, im1);
+    ComplexNumber z2(re2, im2);
+
+    // Assert
+    ASSERT_ANY_THROW(z1 / z2);
+}
 
