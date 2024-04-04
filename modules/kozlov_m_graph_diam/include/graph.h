badea7a33b5505692144e47a376c088aeef4c634
Mishaizlesa <misha11ru@gmail.com>
2024-04-04 19:57:02 +0300

    1st

--- modules/kozlov_m_graph_diam/include/graph.h
+++ modules/kozlov_m_graph_diam/include/graph.h
@@ -16,12 +16,11 @@ const uint64_t INF = std::numeric_limits<uint64_t>::max();
 
 template<uint32_t N>
 class Graph {
-protected:
+ protected:
     std::vector<std::vector<uint64_t>> dist;
     std::vector<std::vector<std::pair<uint32_t, u_int64_t>>> adj;
     bool valid = true;
-    void find_dist(uint32_t start){
-
+    void find_dist(uint32_t start) {
         std::priority_queue<std::pair<uint64_t, uint32_t>> q;
 
         q.emplace(0, start);
@@ -46,8 +45,7 @@ protected:
 
 
  public:
-
-    Graph(){
+    Graph() {
         dist = std::vector<std::vector<uint64_t>> (N, std::vector<uint64_t>(N));
         adj = std::vector<std::vector<std::pair<uint32_t, u_int64_t>>>(N);
     }
@@ -55,16 +53,14 @@ protected:
     void add_edge(uint32_t v, uint32_t u, uint64_t weight = 0ULL){
         if (v >= N || u >= N)
             throw std::invalid_argument("vertex out of range");
-        
-        if (v==u) throw std::invalid_argument("same vertex");
+        if (v == u) throw std::invalid_argument("same vertex");
 
         valid = false;
 
         adj[v].emplace_back(u,weight);
     }
 
-    uint64_t find_diam(){
-
+    uint64_t find_diam() {
         uint64_t ans = 0;
 
         if (!valid){
@@ -74,15 +70,14 @@ protected:
 
             find_dist(i);
 
-            for(uint32_t j=0;j<N;++j){ 
+            for(uint32_t j = 0 ; j < N ; ++j){ 
                 if (dist[i][j]==INF) return INF;
-                if (i != j){
+                if (i != j) {
                     ans = std::max(ans, dist[i][j]);
                 }
             }
 
         }
-
         return ans;
 
     }
