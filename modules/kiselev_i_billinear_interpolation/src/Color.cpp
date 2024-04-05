2d923711512c82db9cb668320097c2f7d8bdf97b
ISK7 <105171814+ISK7@users.noreply.github.com>
2024-04-05 11:08:12 +0300

    Lab 2 fixed tests syntax

--- modules/kiselev_i_billinear_interpolation/src/Color.cpp
+++ modules/kiselev_i_billinear_interpolation/src/Color.cpp
@@ -4,18 +4,15 @@
 
 Color::Color(int r, int g, int b) {
     if (r > 255) R = 255;
-    else 
-        if (r < 0) R = 0;
+    else if (r < 0) R = 0;
     else
             R = r;
     if (g > 255) G = 255;
-    else
-        if (g < 0) G = 0;
+    else if (g < 0) G = 0;
     else
             G = g;
     if (b > 255) B = 255;
-    else
-        if (b < 0) B = 0;
+    else if (b < 0) B = 0;
     else
             B = b;
 }
@@ -40,24 +37,21 @@ char Color::getB() const {
 
 void Color::setR(int r) {
     if (r > 255) R = 255;
-    else
-        if (r < 0) R = 0;
+    else if (r < 0) R = 0;
     else
             R = r;
 }
 
 void Color::setG(int g) {
     if (g > 255) G = 255;
-    else
-        if (g < 0) G = 0;
+    else if (g < 0) G = 0;
     else
             G = g;
 }
 
 void Color::setB(int b) {
     if (b > 255) B = 255;
-    else 
-        if (b < 0) B = 0;
+    else if (b < 0) B = 0;
     else
             B = b;
 }
--- modules/kiselev_i_billinear_interpolation/test/test_kiselev_i_billinear_interpolation.cpp
+++ modules/kiselev_i_billinear_interpolation/test/test_kiselev_i_billinear_interpolation.cpp
@@ -6,7 +6,8 @@
 #include "include/Billinear_interpolation.h"
 #include "include/Color.h"
 
-TEST(Kiselev_Igor_Billinear_interpolation_test, test_color_constructors_and_getters) {
+TEST(Kiselev_Igor_Billinear_interpolation_test,
+    test_color_constructors_and_getters) {
     Color c1 = Color(12, 23, 34);
     Color c2 = Color(c1);
     ASSERT_EQ(c1.getR(), 12);
@@ -27,26 +28,31 @@ TEST(Kiselev_Igor_Billinear_interpolation_test, test_color_setters) {
     ASSERT_EQ(c1.getB(), 34);
 }
 
-TEST(Kiselev_Igor_Billinear_interpolation_test, test_billinear_interpolation_constructors) {
-    std::vector<std::vector<Color>> v(2,std::vector<Color>(2, Color(0, 0, 0)));
+TEST(Kiselev_Igor_Billinear_interpolation_test,
+    test_billinear_interpolation_constructors) {
+    std::vector<std::vector<Color>> v(2, std::vector<Color>
+        (2, Color(0, 0, 0)));
     ASSERT_NO_THROW(BillinearInterpolation bi1 = BillinearInterpolation(v));
     BillinearInterpolation bi1 = BillinearInterpolation(v);
     ASSERT_NO_THROW(BillinearInterpolation bi2 = BillinearInterpolation(bi1));
 }
 
 TEST(Kiselev_Igor_Billinear_interpolation_test, test_twoCore_is_no_fall) {
-    std::vector<std::vector<Color>> v(3, std::vector<Color>(3, Color(0, 0, 0)));
+    std::vector<std::vector<Color>> v(3, std::vector<Color>
+        (3, Color(0, 0, 0)));
     BillinearInterpolation bi1 = BillinearInterpolation(v);
     ASSERT_NO_THROW(bi1.twoCore());
 }
 
 TEST(Kiselev_Igor_Billinear_interpolation_test, test_twoCore_is_correct) {
-    std::vector<std::vector<Color>> v(2, std::vector<Color>(2, Color(40, 40, 40)));
+    std::vector<std::vector<Color>> v(2, std::vector<Color>
+        (2, Color(40, 40, 40)));
     v[1][1] = Color(60, 60, 60);
     v[1][0] = Color(50, 50, 50);
     v[0][1] = Color(50, 50, 50);
     BillinearInterpolation bi1 = BillinearInterpolation(v);
-    std::vector<std::vector<Color>> ans(1, std::vector<Color>(1, Color(50, 50, 50)));
+    std::vector<std::vector<Color>> ans(1, std::vector<Color>
+        (1, Color(50, 50, 50)));
     std::vector<std::vector<Color>> res = bi1.twoCore();
     ASSERT_EQ(ans[0][0].getR(), res[0][0].getR());
     ASSERT_EQ(res[0][0].getG(), ans[0][0].getG());
@@ -54,13 +60,15 @@ TEST(Kiselev_Igor_Billinear_interpolation_test, test_twoCore_is_correct) {
 }
 
 TEST(Kiselev_Igor_Billinear_interpolation_test, test_threeCore_is_no_throw) {
-    std::vector<std::vector<Color>> v(3, std::vector<Color>(3, Color(0, 0, 0)));
+    std::vector<std::vector<Color>> v(3, std::vector<Color>
+        (3, Color(0, 0, 0)));
     BillinearInterpolation bi1 = BillinearInterpolation(v);
     ASSERT_NO_THROW(bi1.threeCore());
 }
 
 TEST(Kiselev_Igor_Billinear_interpolation_test, test_threeCore_is_correct) {
-    std::vector<std::vector<Color>> v(3, std::vector<Color>(3, Color(0, 0, 0)));
+    std::vector<std::vector<Color>> v(3, std::vector<Color>
+        (3, Color(0, 0, 0)));
     v[2][2] = Color(60, 60, 60);
     v[2][0] = Color(50, 50, 50);
     v[0][2] = Color(50, 50, 50);
