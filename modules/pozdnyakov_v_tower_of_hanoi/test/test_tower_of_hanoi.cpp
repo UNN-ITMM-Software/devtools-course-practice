67545c79f0843907a40e3fc0e7b9623c8b986274
Василий Поздняков <pozvas03@mail.ru>
2024-04-07 16:46:24 +0300

    changed tests

--- modules/pozdnyakov_v_tower_of_hanoi/test/test_tower_of_hanoi.cpp
+++ modules/pozdnyakov_v_tower_of_hanoi/test/test_tower_of_hanoi.cpp
@@ -5,47 +5,65 @@
 #include "include/tower_of_hanoi.h"
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_negative) {
-    ASSERT_ANY_THROW(TowerOfHanoi::CalculateSteps(-10));
+    int rings = -10;
+
+    ASSERT_ANY_THROW(TowerOfHanoi::CalculateSteps(rings));
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_zero) {
-    ASSERT_ANY_THROW(TowerOfHanoi::CalculateSteps(0));
+    int rings = 0;
+
+    ASSERT_ANY_THROW(TowerOfHanoi::CalculateSteps(rings));
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_positive) {
-    ASSERT_NO_THROW(TowerOfHanoi::CalculateSteps(10));
+    int rings = 10;
+
+    ASSERT_NO_THROW(TowerOfHanoi::CalculateSteps(rings));
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_1) {
+    int rings = 1;
     std::vector<std::vector<int>> answer = {{1, 3}};
-    std::vector<std::vector<int>> result = TowerOfHanoi::CalculateSteps(1);
+    std::vector<std::vector<int>> result;
+
+    result = TowerOfHanoi::CalculateSteps(rings);
 
     ASSERT_EQ(answer, result);
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_2) {
+    int rings = 2;
     std::vector<std::vector<int>> answer = {{1, 2}, {1, 3}, {2, 3}};
-    std::vector<std::vector<int>> result = TowerOfHanoi::CalculateSteps(2);
+    std::vector<std::vector<int>> result;
+
+    result = TowerOfHanoi::CalculateSteps(rings);
 
     ASSERT_EQ(answer, result);
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_3) {
+    int rings = 3;
     std::vector<std::vector<int>> answer = {{1, 3}, {1, 2}, {3, 2},
                                             {1, 3}, {2, 1}, {2, 3},
                                             {1, 3}};
-    std::vector<std::vector<int>> result = TowerOfHanoi::CalculateSteps(3);
+    std::vector<std::vector<int>> result;
+
+    result = TowerOfHanoi::CalculateSteps(rings);
 
     ASSERT_EQ(answer, result);
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_4) {
+    int rings = 4;
     std::vector<std::vector<int>> answer = {{1, 2}, {1, 3}, {2, 3},
                                             {1, 2}, {3, 1}, {3, 2},
                                             {1, 2}, {1, 3}, {2, 3},
                                             {2, 1}, {3, 1}, {2, 3},
                                             {1, 2}, {1, 3}, {2, 3}};
-    std::vector<std::vector<int>> result = TowerOfHanoi::CalculateSteps(4);
+    std::vector<std::vector<int>> result;
+
+    result = TowerOfHanoi::CalculateSteps(rings);
 
     ASSERT_EQ(answer, result);
 }
