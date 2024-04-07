b2c124fee9c25538981caca3eab65b74c1f55d0e
Manymoera <soloninko80@gmail.com>
2024-04-07 22:25:06 +0300

    style fixes

--- modules/soloninko_a_huffman_code/include/huffman.h
+++ modules/soloninko_a_huffman_code/include/huffman.h
@@ -4,131 +4,121 @@
 #define MODULES_SOLONINKO_A_HUFFMAN_CODE_INCLUDE_HUFFMAN_H_
 
 #include <iostream>
-#include <string>
 #include <queue>
+#include <string>
 #include <unordered_map>
 
-std::string dec_str("");
-
-struct Node
-{
-	char ch;
-	int freq;
-	Node* left, * right;
+struct Node {
+  char ch;
+  int freq;
+  Node *left, *right;
 };
 
-Node* getNode(char ch, int freq, Node* left, Node* right)
-{
-	Node* node = new Node();
+Node *getNode(char ch, int freq, Node *left, Node *right) {
+  Node *node = new Node();
 
-	node->ch = ch;
-	node->freq = freq;
-	node->left = left;
-	node->right = right;
+  node->ch = ch;
+  node->freq = freq;
+  node->left = left;
+  node->right = right;
 
-	return node;
+  return node;
 }
 
-struct comp
-{
-	bool operator()(Node* l, Node* r)
-	{
-		return l->freq > r->freq;
-	}
+struct comp {
+  bool operator()(Node *l, Node *r) { return l->freq > r->freq; }
 };
 
-void encode(Node* root, std::string str,
-	std::unordered_map<char, std::string>& huffmanCode)
-{
-	if (root == nullptr)
-		return;
+void encode(Node *root, std::string str,
+            std::unordered_map<char, std::string> &huffmanCode) {
+  if (root == nullptr)
+    return;
 
-	// found a leaf node
-	if (!root->left && !root->right) {
-		huffmanCode[root->ch] = str;
-	}
+  if (!root->left && !root->right) {
+    huffmanCode[root->ch] = str;
+  }
 
-	encode(root->left, str + "0", huffmanCode);
-	encode(root->right, str + "1", huffmanCode);
+  encode(root->left, str + "0", huffmanCode);
+  encode(root->right, str + "1", huffmanCode);
 }
 
-void decode(Node* root, int& index, std::string str)
-{
-	if (root == nullptr) {
-		return;
-	}
+std::string decode(Node *root, int &index, std::string str,
+                   std::string &dec_str) {
 
-	// found a leaf node
-	if (!root->left && !root->right)
-	{
-		dec_str += root->ch;
-		return;
-	}
+  if (root == nullptr) {
+    return dec_str;
+  }
 
-	index++;
+  if (!root->left && !root->right) {
+    dec_str += root->ch;
+    return dec_str;
+  }
 
-	if (str[index] == '0')
-		decode(root->left, index, str);
-	else
-		decode(root->right, index, str);
+  index++;
+
+  if (str[index] == '0')
+    decode(root->left, index, str, dec_str);
+  else
+    decode(root->right, index, str, dec_str);
+
+  return dec_str;
 }
 
-// Builds Huffman Tree and decode given input text
-void buildHuffmanTree(std::string text)
-{
-	std::unordered_map<char, int> freq;
-	for (char ch : text) {
-		freq[ch]++;
-	}
+void buildHuffmanTree(std::string text, std::string &dec_str) {
+  std::unordered_map<char, int> freq;
+  for (char ch : text) {
+    freq[ch]++;
+  }
 
-	std::priority_queue<Node*, std::vector<Node*>, comp> pq;
+  std::priority_queue<Node *, std::vector<Node *>, comp> pq;
 
-	for (auto pair : freq) {
-		pq.push(getNode(pair.first, pair.second, nullptr, nullptr));
-	}
+  for (auto pair : freq) {
+    pq.push(getNode(pair.first, pair.second, nullptr, nullptr));
+  }
 
-	while (pq.size() != 1)
-	{
-		Node* left = pq.top(); pq.pop();
-		Node* right = pq.top();	pq.pop();
+  while (pq.size() != 1) {
+    Node *left = pq.top();
+    pq.pop();
+    Node *right = pq.top();
+    pq.pop();
 
-		int sum = left->freq + right->freq;
-		pq.push(getNode('\0', sum, left, right));
-	}
+    int sum = left->freq + right->freq;
+    pq.push(getNode('\0', sum, left, right));
+  }
 
-	Node* root = pq.top();
+  Node *root = pq.top();
 
-	std::unordered_map<char, std::string> huffmanCode;
-	encode(root, "", huffmanCode);
+  std::unordered_map<char, std::string> huffmanCode;
+  encode(root, "", huffmanCode);
 
-	std::cout << "Huffman Codes are :\n" << '\n';
-	for (auto pair : huffmanCode) {
-		std::cout << pair.first << " " << pair.second << '\n';
-	}
+  std::cout << "Huffman Codes are :\n" << '\n';
+  for (auto pair : huffmanCode) {
+    std::cout << pair.first << " " << pair.second << '\n';
+  }
 
-	std::cout << "\nOriginal string was :\n" << text << '\n';
+  std::cout << "\nOriginal string was :\n" << text << '\n';
 
-	std::string str = "";
-	for (char ch : text) {
-		str += huffmanCode[ch];
-	}
+  std::string str = "";
+  for (char ch : text) {
+    str += huffmanCode[ch];
+  }
 
-	std::cout << "\nEncoded string is :\n" << str << '\n';
+  std::cout << "\nEncoded string is :\n" << str << '\n';
 
-	int index = -1;
-	std::cout << "\nDecoded string is: \n";
-	while (index < (int)str.size() - 1) {
-		decode(root, index, str);
-	}
-    std::cout << dec_str;
+  int index = -1;
+  std::cout << "\nDecoded string is: \n";
+  while (index < (int)str.size() - 1) {
+    dec_str = decode(root, index, str, dec_str);
+  }
+  std::cout << dec_str << "\n";
 }
 
 bool compare_str(std::string str1, std::string str2) {
-    if(str1.compare(str2) == 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (str1.compare(str2) == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-#endif  // MODULES_SOLONINKO_A_HUFFMAN_CODE_INCLUDE_HUFFMAN_H_
+#endif //  MODULES_SOLONINKO_A_HUFFMAN_CODE_INCLUDE_HUFFMAN_H_
--- modules/soloninko_a_huffman_code/test/huffman_test.cpp
+++ modules/soloninko_a_huffman_code/test/huffman_test.cpp
@@ -5,37 +5,37 @@
 
 TEST(Soloninko_Huffman, Test_1) {
   std::string s1 = "amebus";
-  buildHuffmanTree(s1);
-  ASSERT_NO_THROW(buildHuffmanTree(s1));
-  dec_str="";
+  std::string dec_s1;
+  buildHuffmanTree(s1, dec_s1);
+  ASSERT_NO_THROW(buildHuffmanTree(s1, dec_s1));
 }
 
 TEST(Soloninko_Huffman, Test_2) {
   std::string s1 = "Amogus amebus abobus";
-  buildHuffmanTree(s1);
-  ASSERT_TRUE(compare_str(s1, dec_str));
-  dec_str="";
+  std::string dec_s1;
+  buildHuffmanTree(s1, dec_s1);
+  ASSERT_TRUE(compare_str(s1, dec_s1));
 }
 
 TEST(Soloninko_Huffman, Test_3) {
   std::string s1 = "Aa";
-  buildHuffmanTree(s1);
-  ASSERT_TRUE(compare_str(s1, dec_str));
-  dec_str="";
+  std::string dec_s1;
+  buildHuffmanTree(s1, dec_s1);
+  ASSERT_TRUE(compare_str(s1, dec_s1));
 }
 
 TEST(Soloninko_Huffman, Test_4) {
   std::string s1 = "erthrtth rthyjyjth sadgdfghth rtjhtyjh rteghtjty";
-  buildHuffmanTree(s1);
-  ASSERT_TRUE(compare_str(s1, dec_str));
-  dec_str="";
+  std::string dec_s1;
+  buildHuffmanTree(s1, dec_s1);
+  ASSERT_TRUE(compare_str(s1, dec_s1));
 }
 
 TEST(Soloninko_Huffman, Test_5) {
   std::string s1 = "amebus";
-  buildHuffmanTree(s1);
-  ASSERT_TRUE(compare_str(s1, dec_str));
-  dec_str="";
+  std::string dec_s1;
+  buildHuffmanTree(s1, dec_s1);
+  ASSERT_TRUE(compare_str(s1, dec_s1));
 }
 
 int main(int argc, char **argv) {
