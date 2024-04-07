f06be8e2dffa2192018fcd65cd06b52e93101701
Instmois <nikita-moiseev-2003@ya.ru>
2024-04-07 21:00:15 +0300

    3

--- modules/moiseev_n_deposit_calculator/include/deposit_calculator.h
+++ modules/moiseev_n_deposit_calculator/include/deposit_calculator.h
@@ -1,18 +1,19 @@
 // Copyright 2024 Moiseev Nikita
 
-#ifndef MODULES_DEPOSIT_CALCULATOR_H_
-#define MODULES_DEPOSIT_CALCULATOR_H_
+#ifndef MODULES_MOISEEV_N_DEPOSIT_CALCULATOR_INCLUDE_DEPOSIT_CALCULATOR_H_
+#define MODULES_MOISEEV_N_DEPOSIT_CALCULATOR_INCLUDE_DEPOSIT_CALCULATOR_H_
 
 #include <string>
 
-class DepositCalculator
-{
-public:
+class DepositCalculator {
+ public:
     DepositCalculator();
     ~DepositCalculator();
 
-    double calculateProfit(double depositAmount, double interestRate, int months);
-    double calculateProfitCapitalization(double depositAmount, double interestRate, int months);
+    double calculateProfit(double depositAmount, double interestRate,
+        int months);
+    double calculateProfitCapitalization(double depositAmount,
+        double interestRate, int months);
 };
 
-#endif  // MODULES_DEPOSIT_CALCULATOR_H_
+#endif  // MODULES_MOISEEV_N_DEPOSIT_CALCULATOR_INCLUDE_DEPOSIT_CALCULATOR_H_
--- modules/moiseev_n_deposit_calculator/src/deposit_calculator.cpp
+++ modules/moiseev_n_deposit_calculator/src/deposit_calculator.cpp
@@ -5,43 +5,34 @@
 
 #include "include/deposit_calculator.h"
 
-DepositCalculator::DepositCalculator()
-{
-}
+DepositCalculator::DepositCalculator() {}
 
-DepositCalculator::~DepositCalculator()
-{
-}
+DepositCalculator::~DepositCalculator() {}
 
-double DepositCalculator::calculateProfitCapitalization(double depositAmount, double interestRate, int months)
-{
-    if (depositAmount <= 0 || interestRate < 0 || months <= 0)
-    {
+double DepositCalculator::calculateProfitCapitalization(double depositAmount,
+    double interestRate, int months) {
+    if (depositAmount <= 0 || interestRate < 0 || months <= 0) {
         throw std::invalid_argument("Invalid arguments");
     }
 
     double monthlyInterestRate = interestRate / 100 / 12;
-
     double profit = 0.0;
     double currentDepositAmount = depositAmount;
 
-    for (int i = 0; i < months; ++i)
-    {
+    for (int i = 0; i < months; ++i) {
         profit += currentDepositAmount * monthlyInterestRate;
         currentDepositAmount += currentDepositAmount * monthlyInterestRate;
     }
 
     return currentDepositAmount;
 }
-double DepositCalculator::calculateProfit(double depositAmount, double interestRate, int months)
-{
-    if (depositAmount <= 0 || interestRate < 0 || months <= 0)
-    {
+double DepositCalculator::calculateProfit(double depositAmount,
+    double interestRate, int months) {
+    if (depositAmount <= 0 || interestRate < 0 || months <= 0) {
         throw std::invalid_argument("Invalid arguments");
     }
 
     double monthlyInterestRate = interestRate / 100 / 12;
-
 
     return depositAmount * monthlyInterestRate * months;
 }
