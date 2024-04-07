0d6939c4b3279b009a63c8953450eb546dd2cb03
Instmois <nikita-moiseev-2003@ya.ru>
2024-04-07 21:05:52 +0300

    3

--- modules/moiseev_n_deposit_calculator/test/test_Moiseev_Nikita_moiseev_n_deposit_calculator.cpp
+++ modules/moiseev_n_deposit_calculator/test/test_Moiseev_Nikita_moiseev_n_deposit_calculator.cpp
@@ -6,32 +6,35 @@
 #include "include/deposit_calculator.h"
 
 class DepositCalculatorTest : public ::testing::Test {
-protected:
+ protected:
     DepositCalculator calculator;
 };
 
-TEST(DepositCalculatorTests, CalculateProfit_ZeroDepositAmount_ReturnsZero)
-{
+TEST(DepositCalculatorTests, CalculateProfit_ZeroDepositAmount_ReturnsZero) {
     // Arrange
     DepositCalculator calculator;
 
     // Act & Assert
-    EXPECT_THROW(calculator.calculateProfit(0, 10, 12), std::invalid_argument);
-    EXPECT_THROW(calculator.calculateProfitCapitalization(0, 10, 12), std::invalid_argument);
+    EXPECT_THROW(calculator.calculateProfit(0, 10, 12),
+        std::invalid_argument);
+    EXPECT_THROW(calculator.calculateProfitCapitalization(0, 10, 12),
+        std::invalid_argument);
 }
 
-TEST(DepositCalculatorTests, CalculateProfit_NegativeInterestRate_ThrowsException)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfit_NegativeInterestRate_ThrowsException) {
     // Arrange
     DepositCalculator calculator;
 
     // Act & Assert
-    EXPECT_THROW(calculator.calculateProfit(1000, -1, 12), std::invalid_argument);
-    EXPECT_THROW(calculator.calculateProfitCapitalization(1000, -1, 12), std::invalid_argument);
+    EXPECT_THROW(calculator.calculateProfit(1000, -1, 12),
+        std::invalid_argument);
+    EXPECT_THROW(calculator.calculateProfitCapitalization(1000, -1, 12),
+        std::invalid_argument);
 }
 
-TEST(DepositCalculatorTests, CalculateProfit_SimpleCase_ReturnsCorrectProfit)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfit_SimpleCase_ReturnsCorrectProfit) {
     // Arrange
     DepositCalculator calculator;
 
@@ -39,11 +42,12 @@ TEST(DepositCalculatorTests, CalculateProfit_SimpleCase_ReturnsCorrectProfit)
     double profit = calculator.calculateProfitCapitalization(1000, 10, 12);
 
     // Assert
-    EXPECT_NEAR(profit,1000 + 1000 * (std::pow(1 + 10.0 / 100 / 12, 12) - 1), 0.01);
+    EXPECT_NEAR(profit,
+        1000 + 1000 * (std::pow(1 + 10.0 / 100 / 12, 12) - 1), 0.01);
 }
 
-TEST(DepositCalculatorTests, CalculateProfit_SimpleCase_ReturnsCorrectProfit2)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfit_SimpleCase_ReturnsCorrectProfit2) {
     // Arrange
     DepositCalculator calculator;
 
@@ -53,28 +57,32 @@ TEST(DepositCalculatorTests, CalculateProfit_SimpleCase_ReturnsCorrectProfit2)
     // Assert
     EXPECT_NEAR(profit, 1000 * (10.0 / 100 / 12) * 12, 0.01);
 }
-TEST(DepositCalculatorTests, CalculateProfit_NegativeDepositAmount_ThrowsException)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfit_NegativeDepositAmount_ThrowsException) {
     // Arrange
     DepositCalculator calculator;
 
     // Act & Assert
-    EXPECT_THROW(calculator.calculateProfit(-1000, 10, 12), std::invalid_argument);
-    EXPECT_THROW(calculator.calculateProfitCapitalization(-1000, 10, 12), std::invalid_argument);
+    EXPECT_THROW(calculator.calculateProfit(-1000, 10, 12),
+        std::invalid_argument);
+    EXPECT_THROW(calculator.calculateProfitCapitalization(-1000, 10, 12),
+        std::invalid_argument);
 }
 
-TEST(DepositCalculatorTests, CalculateProfit_ZeroMonths_ThrowsException)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfit_ZeroMonths_ThrowsException) {
     // Arrange
     DepositCalculator calculator;
 
     // Act & Assert
-    EXPECT_THROW(calculator.calculateProfit(1000, 10, 0), std::invalid_argument);
-    EXPECT_THROW(calculator.calculateProfitCapitalization(1000, 10, 0), std::invalid_argument);
+    EXPECT_THROW(calculator.calculateProfit(1000, 10, 0),
+        std::invalid_argument);
+    EXPECT_THROW(calculator.calculateProfitCapitalization(1000, 10, 0),
+        std::invalid_argument);
 }
 
-TEST(DepositCalculatorTests, CalculateProfit_ZeroInterestRate_ReturnsZero)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfit_ZeroInterestRate_ReturnsZero) {
     // Arrange
     DepositCalculator calculator;
 
@@ -85,21 +93,22 @@ TEST(DepositCalculatorTests, CalculateProfit_ZeroInterestRate_ReturnsZero)
     EXPECT_EQ(profit, 0);
 }
 
-TEST(DepositCalculatorTests, CalculateProfit_MaximumMonths_ReturnsCorrectProfit)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfit_MaximumMonths_ReturnsCorrectProfit) {
     // Arrange
     DepositCalculator calculator;
 
     // Act
-    double profit = calculator.calculateProfit(1000, 10, std::numeric_limits<int>::max());
+    double profit = calculator.calculateProfit(1000, 10,
+        std::numeric_limits<int>::max());
 
     // Assert
     EXPECT_GT(profit, 0);
 }
 
 
-TEST(DepositCalculatorTests, CalculateProfitCapitalization_ZeroInterestRate_ReturnsInitialDeposit)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfitCapitalization_ZeroInterestRate_ReturnsInitialDeposit) {
     // Arrange
     DepositCalculator calculator;
 
@@ -110,8 +119,8 @@ TEST(DepositCalculatorTests, CalculateProfitCapitalization_ZeroInterestRate_Retu
     EXPECT_EQ(profit, 1000);
 }
 
-TEST(DepositCalculatorTests, CalculateProfitCapitalization_OneMonth_ReturnsCorrectProfit)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfitCapitalization_OneMonth_ReturnsCorrectProfit) {
     // Arrange
     DepositCalculator calculator;
 
@@ -122,25 +131,27 @@ TEST(DepositCalculatorTests, CalculateProfitCapitalization_OneMonth_ReturnsCorre
     EXPECT_NEAR(profit, 1000 + 1000 * (10.0 / 100 / 12), 0.01);
 }
 
-TEST(DepositCalculatorTests, CalculateProfitCapitalization_LargeDeposit_ReturnsCorrectProfit)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfitCapitalization_LargeDeposit_ReturnsCorrectProfit) {
     // Arrange
     DepositCalculator calculator;
 
     // Act
-    double profit = calculator.calculateProfitCapitalization(std::numeric_limits<double>::max(), 10, 12);
+    double profit = calculator.calculateProfitCapitalization(
+        std::numeric_limits<double>::max(), 10, 12);
 
     // Assert
     EXPECT_GT(profit, 0);
 }
 
-TEST(DepositCalculatorTests, CalculateProfitCapitalization_LargeInterestRate_ReturnsCorrectProfit)
-{
+TEST(DepositCalculatorTests,
+    CalculateProfitCapitalization_LargeInterestRate_ReturnsCorrectProfit) {
     // Arrange
     DepositCalculator calculator;
 
     // Act
-    double profit = calculator.calculateProfitCapitalization(1000, std::numeric_limits<double>::max(), 12);
+    double profit = calculator.calculateProfitCapitalization(
+        1000, std::numeric_limits<double>::max(), 12);
 
     // Assert
     EXPECT_GT(profit, 0);
