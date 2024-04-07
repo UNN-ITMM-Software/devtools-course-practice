fa7bedbc39dd812490ee900ea3115f1113ffa20e
Frenetz <xodyrev.fedya@mail.ru>
2024-04-07 21:26:35 +0300

    Third commit

--- modules/khodyrev_f_point_hits_a_triangle/include/point_hits_a_triangle.h
+++ modules/khodyrev_f_point_hits_a_triangle/include/point_hits_a_triangle.h
@@ -4,27 +4,27 @@
 #define MODULES_KHODYREV_F_POINT_HITS_A_TRIANGLE_INCLUDE_POINT_HITS_A_TRIANGLE_H_
 
 class Point {
-private:
-   double x;
-   double y;
+ private:
+  double x;
+  double y;
 
-public:
-   Point(double x_coord, double y_coord);
-   double getX() const;
-   double getY() const;
+ public:
+  Point(double x_coord, double y_coord);
+  double getX() const;
+  double getY() const;
 };
 
 class Triangle {
-private:
-   Point v1;
-   Point v2;
-   Point v3;
-   static double distance(const Point& p1, const Point& p2);
+ private:
+  Point v1;
+  Point v2;
+  Point v3;
+  static double distance(const Point& p1, const Point& p2);
 
-public:
-   Triangle(const Point& v1, const Point& v2, const Point& v3);
-   bool isValid() const;
-   bool isInside(const Point& p) const;
+ public:
+  Triangle(const Point& v1, const Point& v2, const Point& v3);
+  bool isValid() const;
+  bool isInside(const Point& p) const;
 };
 
 #endif  // MODULES_KHODYREV_F_POINT_HITS_A_TRIANGLE_INCLUDE_POINT_HITS_A_TRIANGLE_H_
