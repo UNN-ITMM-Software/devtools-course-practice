1ef2ce4e0a68aff88c24ec592d5678511dd4d84a
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-04-10 02:15:42 +0300

    Getters and getNorm tests

--- modules/kriseev_m_vector_3d/test/vector_3d_tests.cpp
+++ modules/kriseev_m_vector_3d/test/vector_3d_tests.cpp
@@ -1,3 +1,74 @@
 // Copyright 2024 Kriseev Mikhail
-
 #include <gtest/gtest.h>
+
+#include "include/vector_3d.h"
+
+TEST(Vector3d, getX_correct) {
+  // Arrange
+  double x = 2.0;
+  double y = 3.0;
+  double z = 4.0;
+  Vector3d vector(x, y, z);
+  // Act
+  double actualValue = vector.getX();
+  // Assert
+  ASSERT_EQ(x, actualValue);
+}
+TEST(Vector3d, getY_correct) {
+  // Arrange
+  double x = 2.0;
+  double y = 3.0;
+  double z = 4.0;
+  Vector3d vector(x, y, z);
+  // Act
+  double actualValue = vector.getY();
+  // Assert
+  ASSERT_EQ(y, actualValue);
+}
+TEST(Vector3d, getZ_correct) {
+  // Arrange
+  double x = 2.0;
+  double y = 3.0;
+  double z = 4.0;
+  Vector3d vector(x, y, z);
+  // Act
+  double actualValue = vector.getZ();
+  // Assert
+  ASSERT_EQ(z, actualValue);
+}
+TEST(Vector3d, getNorm_from_valid_vector_correct) {
+  // Arrange
+  double x = 2.0;
+  double y = 2.0;
+  double z = 1.0;
+  double norm = 3.0;
+  Vector3d vector(x, y, z);
+  // Act
+  double actualValue = vector.getNorm();
+  // Assert
+  ASSERT_NEAR(norm, actualValue, 1e-9);
+}
+TEST(Vector3d, getNorm_from_unit_vector_correct) {
+  // Arrange
+  double x = 0.0;
+  double y = 0.0;
+  double z = 1.0;
+  double norm = 1.0;
+  Vector3d vector(x, y, z);
+  // Act
+  double actualValue = vector.getNorm();
+  // Assert
+  ASSERT_NEAR(norm, actualValue, 1e-9);
+}
+TEST(Vector3d, getNorm_from_zero_vector_correct) {
+  // Arrange
+  double x = 0.0;
+  double y = 0.0;
+  double z = 0.0;
+  double norm = 0.0;
+  Vector3d vector(x, y, z);
+  // Act
+  double actualValue = vector.getNorm();
+  // Assert
+  ASSERT_NEAR(norm, actualValue, 1e-9);
+}
