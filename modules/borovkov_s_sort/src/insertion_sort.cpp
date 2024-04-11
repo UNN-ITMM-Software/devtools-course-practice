4be81562181e9e27e526319ab6f9019edf22a0f0
slx52 <sergeyborovkov2003@mail.ru>
2024-04-11 15:20:48 +0300

    fixx

--- modules/borovkov_s_sort/src/insertion_sort.cpp
+++ modules/borovkov_s_sort/src/insertion_sort.cpp
@@ -1,17 +1,2 @@
 // Copyright 2024 Borovkov Sergey
-#include "include/insertion_sort.h"
-
-namespace InsertionSort {
-void sort(std::vector<int>& arr) {
-  int n = arr.size();
-  for (int i = 1; i < n; ++i) {
-    int key = arr[i];
-    int j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j = j - 1;
-    }
-    arr[j + 1] = key;
-  }
-}
-}  // namespace InsertionSort
+// :)
