4fb374d726141b7591e778369ff559c9cdb64602
luchshiy-shmel <shmelevivan02@yandex.ru>
2024-04-12 23:48:00 +0300

    First commit

---
+++ modules/shmelev_i_graham_algorithm/include/graham_algorithm.h
@@ -0,0 +1,20 @@
+// Copyright 2024 Shmelev Ivan
+
+#ifndef MODULES_SHMELEV_I_GRAHAM_ALGORITHM_INCLUDE_GRAHAM_ALGORITHM_H_
+#define MODULES_SHMELEV_I_GRAHAM_ALGORITHM_INCLUDE_GRAHAM_ALGORITHM_H_
+
+#include <vector>
+#include <utility>
+
+class GrahamScan {
+public:
+    static std::vector<std::pair<double, double>> convexHull
+                        (std::vector<std::pair<double, double>>& points);
+private:
+    static int orientation(std::pair<double, double>& p,
+             std::pair<double, double>& q, std::pair<double, double>& r);
+    static std::vector<std::pair<double, double>> sortPoints
+                        (std::vector<std::pair<double, double>>& points);
+};
+
+#endif  // MODULES_SHMELEV_I_GRAHAM_ALGORITHM_INCLUDE_GRAHAM_ALGORITHM_H_
---
+++ modules/shmelev_i_graham_algorithm/test/test_graham_algorithm.cpp
@@ -0,0 +1,47 @@
+// Copyright 2024 Shmelev Ivan
+
+#include <gtest/gtest.h>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+
+#include "include/graham_algorithm.h"
+
+TEST(GrahamScanTest, TestEmptyVector) {
+    std::vector<std::pair<double, double>> points;
+    std::vector<std::pair<double, double>> result = GrahamScan::convexHull(points);
+    EXPECT_TRUE(result.empty());
+}
+
+TEST(GrahamScanTest, TestOnePoint) {
+    std::vector<std::pair<double, double>> points = {{0, 0}};
+    std::vector<std::pair<double, double>> result = GrahamScan::convexHull(points);
+    EXPECT_TRUE(result.empty());
+}
+
+TEST(GrahamScanTest, TestTwoPoints) {
+    std::vector<std::pair<double, double>> points = {{0, 0}, {1, 0}};
+    std::vector<std::pair<double, double>> result = GrahamScan::convexHull(points);
+    EXPECT_TRUE(result.empty());
+}
+
+TEST(GrahamScanTest, TestCollinearPoints) {
+    std::vector<std::pair<double, double>> points = {{0, 0}, {1, 0}, {2, 0}, {3, 0}};
+    std::vector<std::pair<double, double>> result = GrahamScan::convexHull(points);
+    std::vector<std::pair<double, double>> expected = {{0, 0}, {3, 0}};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(GrahamScanTest, TestSquare) {
+    std::vector<std::pair<double, double>> points = {{0, 0}, {0, 1}, {1, 1}, {1, 0}};
+    std::vector<std::pair<double, double>> result = GrahamScan::convexHull(points);
+    std::vector<std::pair<double, double>> expected = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(GrahamScanTest, DuplicatePoints) {
+    std::vector<std::pair<double, double>> points = {{1.0, 1.0}, {1.0, 1.0}, {2.0, 2.0}, {2.0, 2.0}};
+    std::vector<std::pair<double, double>> result = GrahamScan::convexHull(points);
+    std::vector<std::pair<double, double>> expectedResult = {{1.0, 1.0}, {2.0, 2.0}};
+    EXPECT_EQ(result, expectedResult);
+}
