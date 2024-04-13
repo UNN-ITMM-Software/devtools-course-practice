0bee4e4cf68db58c436759fbb1656b098a89a1b4
unknown <slipfis@gmail.com>
2024-04-13 03:25:40 +0300

    rewrote logic, added all tests

--- modules/derun_a_bisymmetric/include/bisymmetric.h
+++ modules/derun_a_bisymmetric/include/bisymmetric.h
@@ -6,47 +6,149 @@
 #include <iostream>
 #include <vector>
 
-template <typename T> class BisymmetricMatrix {
+template <typename T>
+class BisymmetricMatrix {
  private:
-  std::vector<std::vector<T>> matrix;
+  std::vector<T> matrix;
   int size;
+  int n;
 
- public:
-  explicit BisymmetricMatrix(int n) : size(n) {
-    matrix.resize(n, std::vector<T>(n, T()));
+  static int convertSize(int n) { return ((n + 1) / 2) * ((n + 1) / 2); }
+  static bool isInside(int i, int j, int n) {
+    int middle = n % 2 == 0 ? n / 2 : (n + 1) / 2;
+
+    if (i < middle) {
+      if (j > i) return false;
+    } else {
+      if (j > n - i - 1) return false;
+    }
+    return true;
+  }
+  static int convertIndex(int i1, int j1, int n) {
+    int i2 = j1;
+    int j2 = i1;
+
+    int i3 = n - j1 - 1;
+    int j3 = n - i1 - 1;
+
+    int i4 = n - i1 - 1;
+    int j4 = n - j1 - 1;
+
+    int i0 = 0;
+    int j0 = 0;
+
+    if (isInside(i1, j1, n)) {
+      i0 = i1;
+      j0 = j1;
+    } else if (isInside(i2, j2, n)) {
+      i0 = i2;
+      j0 = j2;
+    } else if (isInside(i3, j3, n)) {
+      i0 = i3;
+      j0 = j3;
+    } else if (isInside(i4, j4, n)) {
+      i0 = i4;
+      j0 = j4;
+    } else {
+      return -1;
+    }
+
+    int k = 0;
+    int side = n;
+    for (int j = 0; j < j0; j++) {
+      k += side;
+      side -= 2;
+    }
+    k += i0 - j0;
   }
 
-  int getSize() const { return size; }
+  static bool checkBisymmetric(
+      const std::initializer_list<std::initializer_list<T>>& list) {
+    int n = static_cast<int>(list.size());
+    std::vector<std::vector<T>> m(n);
 
-  void setElement(int i, int j, const T &value) {
+    int i = 0;
+    for (const auto& row : list) {
+      m[i] = std::vector<T>(row);
+      i++;
+    }
+
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < i; j++)
+        if (m[i][j] != m[j][i]) return false;
+
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n - i; j++)
+        if (m[i][j] != m[n - j - 1][n - i - 1]) return false;
+
+    return true;
+  }
+
+ public:
+  explicit BisymmetricMatrix() : size(0), n(0) {}
+
+  explicit BisymmetricMatrix(int n) : size(convertSize(n)), n(n) {
+    matrix.resize(convertSize(n), T());
+  }
+
+  explicit BisymmetricMatrix(
+      const std::initializer_list<std::initializer_list<T>>& list) {
+    if (!checkBisymmetric(list)) {
+      std::cerr << "List is not bisymmetric\n";
+      throw std::exception("List is not bisymmetric\n");
+    }
+
+    n = static_cast<int>(list.size());
+    size = convertSize(n);
+    matrix.resize(size, T());
+
+    std::vector<std::vector<T>> m(n);
+
+    {
+      int i = 0;
+      for (const auto& row : list) {
+        m[i] = std::vector<T>(row);
+        i++;
+      }
+    }
+
+    {
+      int k = 0;
+
+      int middle = (n % 2 == 0) ? (n / 2) : (n + 1) / 2;
+      for (int j = 0; j < middle; j++)
+        for (int i = j; i < n - j; i++) {
+          matrix[k] = m[i][j];
+          k++;
+        }
+    }
+  }
+
+  int getN() const { return n; }
+
+  void setElement(int i, int j, const T& value) {
     if (i < 0 || i >= size || j < 0 || j >= size) {
       std::cerr << "Invalid index\n";
-      return;
+      throw std::exception("Invalid index\n");
     }
-    matrix[i][j] = value;
-    matrix[j][i] = value;
+    int i0 = convertIndex(i, j, n);
+    matrix[i0] = value;
   }
 
   T getElement(int i, int j) const {
     if (i < 0 || i >= size || j < 0 || j >= size) {
       std::cerr << "Invalid index\n";
-      return T();
+      throw std::exception("Invalid index\n");
     }
-    return matrix[i][j];
-  }
-
-  std::vector<T> &operator[](int index) {
-    if (index < 0 || index >= size) {
-      std::cerr << "Invalid index\n";
-      return matrix[0];
-    }
-    return matrix[index];
+    int i0 = convertIndex(i, j, n);
+    return matrix[i0];
   }
 
   void display() const {
-    for (int i = 0; i < size; ++i) {
-      for (int j = 0; j < size; ++j) {
-        std::cout << matrix[i][j] << " ";
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        int i0 = convertIndex(i, j, n);
+        std::cout << matrix[i0] << " ";
       }
       std::cout << "\n";
     }
