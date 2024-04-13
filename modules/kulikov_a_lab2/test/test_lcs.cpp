9cfeb3ba9bd66d8c17f4e0db1e2ce174e5cd7d5d
AveZorgen <78862872+AveZorgen@users.noreply.github.com>
2024-04-14 01:54:36 +0300

    add asked cases

--- modules/kulikov_a_lab2/test/test_lcs.cpp
+++ modules/kulikov_a_lab2/test/test_lcs.cpp
@@ -39,3 +39,25 @@ TEST(LCSTest, ComplexStrings) {
     std::string str2 = "GXTXAYB";
     ASSERT_EQ("GTAB", LCS::findLCS(str1, str2));
 }
+
+TEST(LCSTest, EmptyAndNonEmptyStrings) {
+    std::string str1 = "";
+    std::string str2 = "abc";
+    ASSERT_EQ("", LCS::findLCS(str1, str2));
+
+    str1 = "abc";
+    str2 = "";
+    ASSERT_EQ("", LCS::findLCS(str1, str2));
+}
+
+TEST(LCSTest, SameCharsDifferentOrder) {
+    std::string str1 = "aabbcc";
+    std::string str2 = "abcabc";
+    ASSERT_EQ("abc", LCS::findLCS(str1, str2));
+}
+
+TEST(LCSTest, StringsWithWhitespace) {
+    std::string str1 = "a b c";
+    std::string str2 = "ab c";
+    ASSERT_EQ("ab c", LCS::findLCS(str1, str2));
+}
