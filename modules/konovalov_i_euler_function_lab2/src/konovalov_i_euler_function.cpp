daea13e31d0fb93d47058ef6f6d2ce7ad9592395
igkonov <igkonov@yandex.ru>
2024-04-13 18:17:37 +0300

    init

---
+++ modules/konovalov_i_euler_function_lab2/src/konovalov_i_euler_function.cpp
@@ -0,0 +1,50 @@
+// Copyright 2024 Konovalov Igor
+
+#include "include/konovalov_i_euler_function.h"
+#include <stdexcept>
+#include <math.h>
+
+bool EulerFunction::contains_divider(int n, std::vector<int>& v)
+{
+  for (int i : v) {
+    if (n % i == 0)
+      return true;
+  }
+  return false;
+}
+
+int EulerFunction::euler(int n)
+{
+  if (n < 1)
+    throw std::runtime_error("invalid number for euler function");
+
+  int res = 1;
+  for (int i = 2; i <= n; i++) {
+    int r = 1;
+    while (n % i == 0) {
+      r *= i;
+      n /= i;
+    }
+    if (r != 1)
+      res *= (r - r / i);
+  }
+  return res;
+}
+
+int EulerFunction::euler_naive(int n)
+{
+  if (n < 1)
+    throw std::runtime_error("invalid number for euler function");
+
+  int res = 0;
+  std::vector<int> prev{};
+  for (int i = 1; i < n; i++) {
+    if (i != 1 && n % i == 0) {
+      prev.push_back(i);
+    }
+    else if (!contains_divider(i, prev)) {
+      res++;
+    }
+  }
+  return res;
+}
---
+++ modules/konovalov_i_euler_function_lab2/test/test_konovalov_i_euler_function.cpp
@@ -0,0 +1,39 @@
+// Copyright 2024 Konovalov Igor
+
+#include <gtest/gtest.h>
+
+#include "include/konovalov_i_euler_function.h"
+
+#include <random>
+
+
+TEST(Konovalov_Igor_Euler_Funcion, throws_when_negative_number_is_given) {
+    EXPECT_ANY_THROW(EulerFunction::euler(-5));
+}
+
+TEST(Konovalov_Igor_Euler_Funcion, returns_1_when_given_1) {
+  EXPECT_EQ(1, EulerFunction::euler(1));
+}
+
+TEST(Konovalov_Igor_Euler_Funcion, returns_1_when_given_2) {
+  EXPECT_EQ(1, EulerFunction::euler(2));
+}
+
+TEST(Konovalov_Igor_Euler_Funcion, returns_2_when_given_6) {
+  EXPECT_EQ(2, EulerFunction::euler_naive(6));
+}
+
+TEST(Konovalov_Igor_Euler_Funcion, returns_400_when_given_1000) {
+  EXPECT_EQ(400, EulerFunction::euler(1000));
+}
+
+TEST(Konovalov_Igor_Euler_Funcion, naive_implementation_gives_same_result_on_random_input) {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> dist6(1, 1000);
+  for (int i = 0; i < 20; i++) {
+    int in = dist6(rng);
+    ASSERT_EQ(EulerFunction::euler_naive(in), EulerFunction::euler(in));
+  }
+}
+
