250aed60acff98984bfa923f92e96bbed690236a
luchshiy-shmel <shmelevivan02@yandex.ru>
2024-04-14 00:08:41 +0300

    fix 2 another

--- modules/shmelev_i_graham_algorithm/test/test_graham_algorithm.cpp
+++ modules/shmelev_i_graham_algorithm/test/test_graham_algorithm.cpp
@@ -39,13 +39,8 @@ TEST(GrahamScanTest, TestCollinearPoints) {
     EXPECT_EQ(result, expected);
 }
 
-TEST(GrahamScanTest, TestSquare) {
-    std::vector<std::pair<double, double>> points = {{0, 0},
-                                                 {0, 1}, {1, 1}, {1, 0}};
-    std::vector<std::pair<double, double>> result =
-                                 GrahamScan::convexHull(points);
-    std::vector<std::pair<double, double>> expected = {{0, 0},
-                                                 {1, 0}, {1, 1}, {0, 1}};
-    EXPECT_EQ(result, expected);
+TEST(GrahamScan, CollinearPoints) {
+    std::vector<std::pair<double, double>> points{{0, 0}, {1, 1}, {2, 2}};
+    std::vector<std::pair<double, double>> expected{{0, 0}, {2, 2}};
+    EXPECT_EQ(GrahamScan::convexHull(points), expected);
 }
-
