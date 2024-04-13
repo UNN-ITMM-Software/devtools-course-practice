f701466f7f71585e592ff8d9b51f8cd5ca39ab17
V-inTim <vinichuk.timofey@mail.ru>
2024-04-13 15:18:41 +0300

    Lab work #2

---
+++ modules/vinichuk_t_binary_search/include/BinarySearch.h
@@ -0,0 +1,28 @@
+// Copyright 2024 Vinichuk Timofey
+
+#ifndef MODULES_VINICHUK_T_BINARY_SEARCH_INCLUDE_BINARYSEARCH_H_
+#define MODULES_VINICHUK_T_BINARY_SEARCH_INCLUDE_BINARYSEARCH_H_
+
+#include <iostream>
+#include <vector>
+
+// implementing a template function implies
+// its definition in the place of declaration(header file)
+template <typename T>
+int binary_search(const std::vector<T>& arr, const T& target) {
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+#endif  // MODULES_VINICHUK_T_BINARY_SEARCH_INCLUDE_BINARYSEARCH_H_
