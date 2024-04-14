0936bd7ab6a38bd670dfd2a448a91f34a457ebd8
Mikhail Kriseev <mish.kryseev@gmail.com>
2024-04-14 23:24:37 +0300

    Fix other issues

--- modules/kriseev_m_vector_3d/src/vector_3d.cpp
+++ modules/kriseev_m_vector_3d/src/vector_3d.cpp
@@ -9,61 +9,61 @@
 
 Vector3d::Vector3d(double x, double y, double z) : x(x), y(y), z(z) {}
 
-double Vector3d::getX() const { return this->x; }
+double Vector3d::getX() const { return x; }
 
-double Vector3d::getY() const { return this->y; }
+double Vector3d::getY() const { return y; }
 
-double Vector3d::getZ() const { return this->z; }
+double Vector3d::getZ() const { return z; }
 
-double Vector3d::getNorm() const { return sqrt(this->squareNorm()); }
+double Vector3d::getNorm() const { return sqrt(squareNorm()); }
 
 Vector3d Vector3d::getNormalizedVector() const {
   if (std::abs(x) < EPSILON && std::abs(y) < EPSILON && std::abs(z) < EPSILON) {
-    throw std::runtime_error("Cannot normalize zero vector");
+    throw std::invalid_argument("Cannot normalize zero vector");
   }
-  return (*this) / (this->getNorm());
+  return (*this) / (getNorm());
 }
 
 double Vector3d::dotProduct(const Vector3d &vector) const {
-  return this->x * vector.x + this->y * vector.y + this->z * vector.z;
+  return x * vector.x + y * vector.y + z * vector.z;
 }
 
 Vector3d Vector3d::crossProduct(const Vector3d &vector) const {
   return Vector3d{
-      this->y * vector.z - this->z * vector.y,
-      this->z * vector.x - this->x * vector.z,
-      this->x * vector.y - this->y * vector.x,
+      y * vector.z - z * vector.y,
+      z * vector.x - x * vector.z,
+      x * vector.y - y * vector.x,
   };
 }
 
 double Vector3d::squareNorm() const {
-  return this->x * this->x + this->y * this->y + this->z * this->z;
+  return x * x + y * y + z * z;
 }
 
 Vector3d Vector3d::operator/(double factor) const {
   if (std::abs(factor) < EPSILON) {
-    throw std::runtime_error("Attempted division by zero");
+    throw std::invalid_argument("Attempted division by zero");
   }
-  return Vector3d{this->x / factor, this->y / factor, this->z / factor};
+  return Vector3d{x / factor, y / factor, z / factor};
 }
 
 Vector3d Vector3d::operator*(double factor) const {
-  return Vector3d{this->x * factor, this->y * factor, this->z * factor};
+  return Vector3d{x * factor, y * factor, z * factor};
 }
 
 Vector3d Vector3d::operator+(const Vector3d &v) const {
-  return Vector3d{this->x + v.x, this->y + v.y, this->z + v.z};
+  return Vector3d{x + v.x, y + v.y, z + v.z};
 }
 
 Vector3d Vector3d::operator-(const Vector3d &v) const {
-  return Vector3d{this->x - v.x, this->y - v.y, this->z - v.z};
+  return Vector3d{x - v.x, y - v.y, z - v.z};
 }
 
 bool Vector3d::operator==(const Vector3d &v) const {
-  return std::abs(this->x - v.x) < EPSILON &&
-         std::abs(this->y - v.y) < EPSILON && std::abs(this->z - v.z) < EPSILON;
+  return std::abs(x - v.x) < EPSILON &&
+         std::abs(y - v.y) < EPSILON && std::abs(z - v.z) < EPSILON;
 }
 
 bool Vector3d::operator!=(const Vector3d &v) const {
-  return !this->operator==(v);
+  return !operator==(v);
 }
