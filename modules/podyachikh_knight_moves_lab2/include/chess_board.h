8ac79baced43dd41052085faa3b4346f9ee54f9a
MuUJa <mihail.podyachih@gmail.com>
2024-04-15 00:03:10 +0300

    Add lab2

---
+++ modules/podyachikh_knight_moves_lab2/include/chess_board.h
@@ -0,0 +1,30 @@
+// Copyright 2024 Podyachikh Mikhail
+#ifndef MODULES_PODYACHIKH_KNIGHT_MOVES_LAB2_INCLUDE_CHESS_BOARD_H_
+#define MODULES_PODYACHIKH_KNIGHT_MOVES_LAB2_INCLUDE_CHESS_BOARD_H_
+
+#include <set>
+#include <utility>
+
+class ChessBoard {
+  using Point = std::pair<int, int>;
+  int _size;
+  std::set<Point> obstacles;
+
+ public:
+  explicit ChessBoard(int size = 8, const std::set<Point> &obstacles = {})
+      : _size(size), obstacles(obstacles) {}
+  void addObstacle(const ChessBoard::Point &point);
+  int knightDistance(Point start, Point end);
+
+ private:
+  bool inside(const ChessBoard::Point &point) const;
+  bool available(const ChessBoard::Point &point);
+  std::set<Point> getPossibleMoves(const ChessBoard::Point &point);
+  int bfs(Point start, Point end);
+  std::set<Point> knightMoves = {{1, 2},   {2, 1},    /*I quarter*/
+                                 {2, -1},  {1, -2},   /*II quarter*/
+                                 {-1, -2}, {-2, -1},  /*III quarter*/
+                                 {-2, 1},  {-1, 2}};  /*IV quarter*/
+};
+
+#endif  // MODULES_PODYACHIKH_KNIGHT_MOVES_LAB2_INCLUDE_CHESS_BOARD_H_
