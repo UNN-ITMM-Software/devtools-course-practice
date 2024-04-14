9210479254ede84adb375132e6b7374a495908fa
TAS <tas@MacBook-Air-TAS.local>
2024-04-15 01:03:17 +0300

    lab2 new

--- modules/troitskiy_a_lab2/include/lab2.h
+++ modules/troitskiy_a_lab2/include/lab2.h
@@ -4,66 +4,64 @@
 #define MODULES_TROITSKIY_A_LAB2_INCLUDE_LAB2_H_
 
 #include <algorithm>
+#include <array>
 #include <cctype>
 #include <cstdint>
-#include <array>
-#include <vector>
 #include <iostream>
 #include <string>
+#include <vector>
 
-template <int N>
-class Heap {
+template <int N> class Heap {
 private:
-    std::array<double, N + 2>t;
-    int real_size=0;
-    void heapify(int n) {
-        int left = 2*n+1;
-        int right = 2*n+2;
-        if (left < real_size) {
-            if (t[n] < t[left]) {
-                std::swap(t[n], t[left]);
-                heapify(left);
-            }
-        }
-        if (right < real_size) {
-            if (t[n] < t[right]) {
-                std::swap(t[n], t[right]);
-                heapify(right);
-            }
-        }
+  std::array<double, N + 2> t;
+  int real_size = 0;
+  void heapify(int n) {
+    int left = 2 * n + 1;
+    int right = 2 * n + 2;
+    if (left < real_size) {
+      if (t[n] < t[left]) {
+        std::swap(t[n], t[left]);
+        heapify(left);
+      }
     }
-public:
-    Heap() = default;
-    Heap(const std::vector<double>& arr) {
-        for(const auto& el: arr) push(el);
+    if (right < real_size) {
+      if (t[n] < t[right]) {
+        std::swap(t[n], t[right]);
+        heapify(right);
+      }
     }
-    void push(const double& el) {
-        int i = real_size;
-        t[i] = el;
-        int parent = (i-1)/2;
-        while (parent >= 0 && i > 0) {
-            if (t[i] > t[parent]) {
-                std::swap(t[i], t[parent]);
-            }
-            i = parent;
-            parent = (i-1)/2;
-        }
-        real_size++;
-    }
-    void pop() {
-        if (real_size == 0)
-            throw std::logic_error("Empty heap");
-        t[0] = t[real_size--];
-        heapify(0);
-    }
-    double top() {
-        if (real_size == 0)
-            throw std::logic_error("Empty heap");
-        return t[0];
-    }
-    int size() {
-            return real_size;
-    }
-};
-#endif  // MODULES_TROITSKIY_A_LAB2_INCLUDE_LAB2_H_
+  }
 
+public:
+  Heap() = default;
+  Heap(const std::vector<double> &arr) {
+    for (const auto &el : arr)
+      push(el);
+  }
+  void push(const double &el) {
+    int i = real_size;
+    t[i] = el;
+    int parent = (i - 1) / 2;
+    while (parent >= 0 && i > 0) {
+      if (t[i] > t[parent]) {
+        std::swap(t[i], t[parent]);
+      }
+      i = parent;
+      parent = (i - 1) / 2;
+    }
+    real_size++;
+  }
+  void pop() {
+    if (real_size == 0)
+      throw std::logic_error("Empty heap");
+    t[0] = t[real_size--];
+    heapify(0);
+  }
+  double top() {
+    if (real_size == 0)
+      throw std::logic_error("Empty heap");
+    return t[0];
+  }
+  int size() { return real_size; }
+};
+#endif // MODULES_TROITSKIY_A_LAB2_INCLUDE_LAB2_H_
--- modules/troitskiy_a_lab2/test/lab2.cpp
+++ modules/troitskiy_a_lab2/test/lab2.cpp
@@ -1,40 +1,39 @@
 // Copyright 2024 Troitskiy Alexander
 
+#include "include/lab2.h"
 #include <gtest/gtest.h>
 #include <limits>
-#include "include/lab2.h"
-
 
 TEST(Troitskiy_A_Lab2Test, PushPopTop) {
-    Heap<10> heap;
-    heap.push(5);
-    heap.push(10);
-    heap.push(3);
-    heap.push(7);
-    
-    EXPECT_EQ(heap.top(), 10);
-    
-    heap.pop();
-    EXPECT_EQ(heap.top(), 7);
-    
-    heap.pop();
-    EXPECT_EQ(heap.top(), 5);
-    
-    heap.push(15);
-    EXPECT_EQ(heap.top(), 15);
+  Heap<10> heap;
+  heap.push(5);
+  heap.push(10);
+  heap.push(3);
+  heap.push(7);
+
+  EXPECT_EQ(heap.top(), 10);
+
+  heap.pop();
+  EXPECT_EQ(heap.top(), 7);
+
+  heap.pop();
+  EXPECT_EQ(heap.top(), 5);
+
+  heap.push(15);
+  EXPECT_EQ(heap.top(), 15);
 }
 
 TEST(Troitskiy_A_Lab2Test, ConstructFromVector) {
-    std::vector <double> vec = {8, 2, 6, 1, 9};
-    Heap<5> heap(vec);
-    
-    EXPECT_EQ(heap.top(), 9);
-    
-    heap.pop();
-    EXPECT_EQ(heap.top(), 8);
+  std::vector<double> vec = {8, 2, 6, 1, 9};
+  Heap<5> heap(vec);
+
+  EXPECT_EQ(heap.top(), 9);
+
+  heap.pop();
+  EXPECT_EQ(heap.top(), 8);
 }
 
 TEST(Troitskiy_A_Lab2Test, EmptyHeapTop) {
-    Heap<5> heap;
-    EXPECT_THROW(heap.top(), std::logic_error);
+  Heap<5> heap;
+  EXPECT_THROW(heap.top(), std::logic_error);
 }
