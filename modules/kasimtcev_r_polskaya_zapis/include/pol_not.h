881168f6d53d5223c9d84d69055274d9dcd0547c
Касимцев Роман <Romasias1@outlook.com>
2024-04-14 15:43:04 +0300

    7 commit

--- modules/kasimtcev_r_polskaya_zapis/include/pol_not.h
+++ modules/kasimtcev_r_polskaya_zapis/include/pol_not.h
@@ -9,10 +9,10 @@
 #include "include/stack.h"
 
 class PolNot {
-  public:
+ public:
     bool number(char c);
     int prior(Lex tmp);
     TQueue<P_Lex>* sEOL(std::string _s);
     std::string revPolNot(TQueue<P_Lex>* q);
 };
-#endif  //  MODULES_REV_POL_NOT_INCLUDE_POL_NOT_H_
+#endif  // MODULES_KASIMTCEV_R_POLSKAYA_ZAPIS_INCLUDE_POL_NOT_H_"
