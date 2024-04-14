5ab41a5325a1dfe8824d61cab4317626318847cd
Иван Васильев ФИИТ1 <Vanovano03@mail.ru>
2024-04-14 22:47:30 +0300

    try5

--- modules/Vasilev_i_lab2/test/test_Combinations.cpp
+++ modules/Vasilev_i_lab2/test/test_Combinations.cpp
@@ -8,7 +8,9 @@
 TEST(CombinationGeneratorTest, BasicTest) {
     std::vector<int> arr = {1, 2, 3, 4};
     int k = 2;
-    std::vector<std::vector<int>> expected = {{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}};
+    std::vector<std::vector<int>> expected = {{1, 2}, {1, 3},
+                                              {1, 4}, {2, 3},
+                                              {2, 4}, {3, 4}};
     auto result = KCombinations::generateCombinations(arr, k);
     ASSERT_EQ(result, expected);
 }
@@ -47,11 +49,16 @@ TEST(CombinationGeneratorTest, SingleElementArray) {
 TEST(CombinationGeneratorTest, LargerArray) {
     std::vector<int> arr = {1, 2, 3, 4, 5, 6};
     int k = 3;
-    std::vector<std::vector<int>> expected = {{1, 2, 3}, {1, 2, 4}, {1, 2, 5}, {1, 2, 6},
-                                              {1, 3, 4}, {1, 3, 5}, {1, 3, 6}, {1, 4, 5},
-                                              {1, 4, 6}, {1, 5, 6}, {2, 3, 4}, {2, 3, 5},
-                                              {2, 3, 6}, {2, 4, 5}, {2, 4, 6}, {2, 5, 6}, 
-                                              {3, 4, 5}, {3, 4, 6}, {3, 5, 6}, {4, 5, 6}};
+    std::vector<std::vector<int>> expected = {{1, 2, 3}, {1, 2, 4},
+                                              {1, 2, 5}, {1, 2, 6},
+                                              {1, 3, 4}, {1, 3, 5},
+                                              {1, 3, 6}, {1, 4, 5},
+                                              {1, 4, 6}, {1, 5, 6},
+                                              {2, 3, 4}, {2, 3, 5},
+                                              {2, 3, 6}, {2, 4, 5},
+                                              {2, 4, 6}, {2, 5, 6}, 
+                                              {3, 4, 5}, {3, 4, 6}, 
+                                              {3, 5, 6}, {4, 5, 6}};
     auto result = KCombinations::generateCombinations(arr, k);
     ASSERT_EQ(result, expected);
 }
