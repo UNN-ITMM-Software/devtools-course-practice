a5559c9fa6de03db7acf0e550f3f50621644dde7
Stepan Morgachev <stepan.morgachev@gmail.com>
2024-04-14 15:44:21 +0300

    feat: done lab2

---
+++ modules/morgachev_s_batcher_merge/include/batcher_merge.h
@@ -0,0 +1,48 @@
+// Copyright 2024 Morgachev Stepan
+
+#ifndef MODULES_MORGACHEV_S_LAB2_INCLUDE_BATHCER_MERGE_H_
+#define MODULES_MORGACHEV_S_LAB2_INCLUDE_BATHCER_MERGE_H_
+
+#include <vector>
+#include <random>
+#include <utility>
+
+template<typename T>
+class BatcherMerge {
+ public:
+  static void batcherMerge(const std::vector<T>& vector) {
+      size_t size = vector.size();
+      if (size == 0) {
+          throw std::invalid_argument("The array is empty");
+       }
+      for (int p = 1; p < size; p += p) {
+          for (int k = p; k > 0; k /= 2) {
+              for (int j = k % p; j + k < size; j += 2 * k) {
+                  for (int i = 0; i < size - j - k; i++) {
+                      if ((i + j) / (2 * p) == (i + j + k) / (2 * p)) {
+                          compareAndSwap(vector[j + i], vector[j + i + k]);
+                      }
+                  }
+              }
+          }
+      }
+  }
+
+  static std::vector<T> getRandomVector(size_t size) {
+      std::random_device dev;
+      std::mt19937 gen(dev());
+      std::vector<T> vector(size);
+      for (size_t i = 0; i < size; i++) {
+          vector[i] = gen() % 100;
+      }
+      return vector;
+  }
+
+ private:
+  static void compareAndSwap(T& a, T& b) {
+      if (b < a)
+          std::swap(a, b);
+  }
+};
+
+#endif  // MODULES_MORGACHEV_S_LAB2_INCLUDE_BATHCER_MERGE_H_
