9730a2b0832dd35ee7b8b9f6b06adc687efaf1e2
Касимцев Роман <Romasias1@outlook.com>
2024-04-14 14:00:27 +0300

    first commit

---
+++ modules/kasimtcev_r_polskaya_zapis/src/lex.cpp
@@ -0,0 +1,16 @@
+// Copyright 2024 Kasimtcev Roman
+
+#include "include/lex.h"
+
+Lex::Lex() {}
+Lex::Lex(std::string _s, Type _t, int _val) {
+    s = _s;
+    t = _t;
+    val = _val;
+    if (_t == operation && _val != -1) throw "Error";
+    if (_t == vall && _val == -1) throw "Error";
+}
+
+Type Lex::getType() { return t; }
+int Lex::getVal() { return vall; }
+std::string Lex::getS() { return s; }
