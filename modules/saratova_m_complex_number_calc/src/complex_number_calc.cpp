7defec2bc0be6b569faf70ee0aa7e8321a81d124
Marissa696 <mrnsrtv@gmail.com>
2024-04-14 11:38:16 +0300

    fix codestyle

--- modules/saratova_m_complex_number_calc/src/complex_number_calc.cpp
+++ modules/saratova_m_complex_number_calc/src/complex_number_calc.cpp
@@ -13,31 +13,37 @@ double ComplexCalculator::getImaginary() const {
     return imaginary;
 }
 
-// Overloaded operators
-ComplexCalculator ComplexCalculator::operator+(const ComplexCalculator& other) const {
+ComplexCalculator ComplexCalculator::operator+
+                                (const ComplexCalculator& other) const {
     double result_real = real + other.real;
     double result_imaginary = imaginary + other.imaginary;
     return ComplexCalculator(result_real, result_imaginary);
 }
 
-ComplexCalculator ComplexCalculator::operator-(const ComplexCalculator& other) const {
+ComplexCalculator ComplexCalculator::operator-
+                                    (const ComplexCalculator& other) const {
     double result_real = real - other.real;
     double result_imaginary = imaginary - other.imaginary;
     return ComplexCalculator(result_real, result_imaginary);
 }
 
-ComplexCalculator ComplexCalculator::operator*(const ComplexCalculator& other) const {
+ComplexCalculator ComplexCalculator::operator*
+                                    (const ComplexCalculator& other) const {
     double result_real = real * other.real - imaginary * other.imaginary;
     double result_imaginary = real * other.imaginary + imaginary * other.real;
     return ComplexCalculator(result_real, result_imaginary);
 }
 
-ComplexCalculator ComplexCalculator::operator/(const ComplexCalculator& other) const {
-    double denominator = other.real * other.real + other.imaginary * other.imaginary;
+ComplexCalculator ComplexCalculator::operator/
+                                    (const ComplexCalculator& other) const {
+    double denominator = other.real * other.real + other.imaginary
+                                                         * other.imaginary;
     if (denominator == 0) {
         throw std::invalid_argument("Error: Division by zero");
     }
-    double result_real = (real * other.real + imaginary * other.imaginary) / denominator;
-    double result_imaginary = (imaginary * other.real - real * other.imaginary) / denominator;
+    double result_real = (real * other.real + imaginary * other.imaginary)
+                                                 / denominator;
+    double result_imaginary = (imaginary * other.real - real *
+                                 other.imaginary) / denominator;
     return ComplexCalculator(result_real, result_imaginary);
 }
