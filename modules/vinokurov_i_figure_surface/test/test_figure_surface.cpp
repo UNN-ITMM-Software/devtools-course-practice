027bfb82ea56f93c474764d84c17428026e098e6
LeakMachine <dimos43467@gmail.com>
2024-04-14 12:33:28 +0300

    code fix 2

--- modules/vinokurov_i_figure_surface/test/test_figure_surface.cpp
+++ modules/vinokurov_i_figure_surface/test/test_figure_surface.cpp
@@ -11,42 +11,48 @@ TEST(Vinokurov_I_Figure_Surface, Non_Valid_Arguements_Parallelepiped) {
     double a = 2.0, b = 3.0, c = 0.0;
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
-    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceParallelepiped(a, b, c), std::invalid_argument);
+    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceParallelepiped(a, b, c),
+    std::invalid_argument);
 }
 
 TEST(Vinokurov_I_Figure_Surface, Non_Valid_Arguements_Cylinder_Side) {
     double r = 0.0, h = 3.0;
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
-    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceCylinderSide(r, h), std::invalid_argument);
+    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceCylinderSide(r, h),
+    std::invalid_argument);
 }
 
 TEST(Vinokurov_I_Figure_Surface, Non_Valid_Arguements_Cylinder_Full) {
     double r = 2.0, h = -3.0;
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
-    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceCylinderFull(r, h), std::invalid_argument);
+    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceCylinderFull(r, h),
+    std::invalid_argument);
 }
 
 TEST(Vinokurov_I_Figure_Surface, Non_Valid_Arguements_Cone_Side) {
     double r = 0.0, l = 3.0;
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
-    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceConeSide(r, l), std::invalid_argument);
+    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceConeSide(r, l),
+    std::invalid_argument);
 }
 
 TEST(Vinokurov_I_Figure_Surface, Non_Valid_Arguements_Cone_Full) {
     double r = 2.0, l = -3.0;
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
-    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceConeFull(r, l), std::invalid_argument);
+    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceConeFull(r, l),
+    std::invalid_argument);
 }
 
 TEST(Vinokurov_I_Figure_Surface, Non_Valid_Arguements_Sphere) {
     double r = -2.0;
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
-    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceSphere(r), std::invalid_argument);
+    ASSERT_THROW(figureSurfaceCalc.fnCalculateSurfaceSphere(r),
+    std::invalid_argument);
 }
 
 TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Parallelepiped) {
@@ -54,7 +60,9 @@ TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Parallelepiped) {
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
     ASSERT_NO_THROW(figureSurfaceCalc.fnCalculateSurfaceParallelepiped(a, b, c));
-    ASSERT_LE(figureSurfaceCalc.fnCalculateSurfaceParallelepiped(r) - resApproximate, 0.001)
+    ASSERT_LE(
+    figureSurfaceCalc.fnCalculateSurfaceParallelepiped(r) - resApproximate,
+    0.001)
 }
 
 TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Cylinder_Side) {
@@ -62,7 +70,9 @@ TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Cylinder_Side) {
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
     ASSERT_NO_THROW(figureSurfaceCalc.fnCalculateSurfaceCylinderSide(r, h));
-    ASSERT_LE(figureSurfaceCalc.fnCalculateSurfaceCylinderSide(r) - resApproximate, 0.001)
+    ASSERT_LE(f
+    figureSurfaceCalc.fnCalculateSurfaceCylinderSide(r) - resApproximate,
+    0.001)
 }
 
 TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Cylinder_Full) {
@@ -70,7 +80,9 @@ TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Cylinder_Full) {
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
     ASSERT_NO_THROW(figureSurfaceCalc.fnCalculateSurfaceCylinderFull(r, h));
-    ASSERT_LE(figureSurfaceCalc.fnCalculateSurfaceCylinderFull(r) - resApproximate, 0.001)
+    ASSERT_LE(
+    figureSurfaceCalc.fnCalculateSurfaceCylinderFull(r) - resApproximate,
+    0.001)
 }
 
 TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Cone_Side) {
@@ -78,7 +90,9 @@ TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Cone_Side) {
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
     ASSERT_NO_THROW(figureSurfaceCalc.fnCalculateSurfaceConeSide(r, l));
-    ASSERT_LE(figureSurfaceCalc.fnCalculateSurfaceConeSide(r) - resApproximate, 0.001)
+    ASSERT_LE(
+    figureSurfaceCalc.fnCalculateSurfaceConeSide(r) - resApproximate,
+    0.001)
 }
 
 TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Cone_Full) {
@@ -86,7 +100,9 @@ TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Cone_Full) {
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
     ASSERT_NO_THROW(figureSurfaceCalc.fnCalculateSurfaceConeFull(r, l));
-    ASSERT_LE(figureSurfaceCalc.fnCalculateSurfaceConeFull(r) - resApproximate, 0.001)
+    ASSERT_LE(
+    figureSurfaceCalc.fnCalculateSurfaceConeFull(r) - resApproximate,
+    0.001)
 }
 
 TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Sphere) {
@@ -94,5 +110,7 @@ TEST(Vinokurov_I_Figure_Surface, Valid_Arguements_Sphere) {
     FigureSurface figureSurfaceCalc = new FigureSurface();
 
     ASSERT_NO_THROW(figureSurfaceCalc.fnCalculateSurfaceSphere(r));
-    ASSERT_LE(figureSurfaceCalc.fnCalculateSurfaceSphere(r) - resApproximate, 0.001)
+    ASSERT_LE(
+    figureSurfaceCalc.fnCalculateSurfaceSphere(r) - resApproximate,
+    0.001)
 }
