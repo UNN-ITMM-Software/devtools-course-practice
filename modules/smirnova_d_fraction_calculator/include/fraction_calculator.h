463791ec46b2a14189a0165cc8d60e4e0cb28220
Дарья Смирнова <Smirnova.darya.vl@gmail.com>
2024-04-14 19:46:46 +0300

    fix code 4

--- modules/smirnova_d_fraction_calculator/include/fraction_calculator.h
+++ modules/smirnova_d_fraction_calculator/include/fraction_calculator.h
@@ -4,24 +4,24 @@
 #define MODULES_SMIRNOVA_D_FRACTION_CALCULATOR_INCLUDE_FRACTION_CALCULATOR_H_
 
 class FractionCalculator {
-    private:
-        int num;
-        int den;
+ private:
+    int num;
+    int den;
 
-    public:
-        FractionCalculator();
-        FractionCalculator(const int numerator, const int denominator);
+ private:
+    FractionCalculator();
+    FractionCalculator(const int numerator, const int denominator);
 
-        int GetNum() const;
-        int GetDen() const;
-        void SetNum(const int numerator);
-        void SetDen(const int denominator);
-        int NOD(int a, int b) const;
+    int GetNum() const;
+    int GetDen() const;
+    void SetNum(const int numerator);
+    void SetDen(const int denominator);
+    int NOD(int a, int b) const;
 
-        FractionCalculator operator+(const  FractionCalculator& z) const;
-        FractionCalculator operator-(const  FractionCalculator& z) const;
-        FractionCalculator operator*(const  FractionCalculator& z) const;
-        FractionCalculator operator/(const  FractionCalculator& z) const;
+    FractionCalculator operator+(const  FractionCalculator& z) const;
+    FractionCalculator operator-(const  FractionCalculator& z) const;
+    FractionCalculator operator*(const  FractionCalculator& z) const;
+    FractionCalculator operator/(const  FractionCalculator& z) const;
 };
 
 #endif  // MODULES_SMIRNOVA_D_FRACTION_CALCULATOR_INCLUDE_FRACTION_CALCULATOR_H_
