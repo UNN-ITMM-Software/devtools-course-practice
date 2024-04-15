2b1e9de0547dc93ba7358eed0aefc218f23bba3e
nik <lesnikowka@mail.ru>
2024-04-15 23:27:28 +0300

    ref

--- modules/lesnikov_long_number/include/LongNumber.h
+++ modules/lesnikov_long_number/include/LongNumber.h
@@ -11,36 +11,36 @@
 
 class LongNumber {
 public:
-	LongNumber();
-	LongNumber(const LongNumber& n);
-	LongNumber(LongNumber&& n) noexcept;
-	LongNumber(std::string s);
-	LongNumber& operator=(const LongNumber& n);
-	LongNumber& operator=(LongNumber&& n) noexcept;
-	LongNumber operator+(const LongNumber& n) const;
-	LongNumber& operator+=(const LongNumber& n);
-	LongNumber operator-(const LongNumber& n) const;
-	LongNumber& operator-=(const LongNumber& n);
-	LongNumber operator*(const LongNumber& n);
-	LongNumber& operator*=(const LongNumber& n);
-	LongNumber operator-() const;
-	bool operator==(const LongNumber& n) const;
-	bool operator!=(const LongNumber& n) const;
-	bool operator<(const LongNumber& n) const;
-	bool operator>(const LongNumber& n) const;
-	bool operator<=(const LongNumber& n) const;
-	bool operator>=(const LongNumber& n) const;
+    LongNumber();
+    LongNumber(const LongNumber& n);
+    LongNumber(LongNumber&& n) noexcept;
+    LongNumber(std::string s);
+    LongNumber& operator=(const LongNumber& n);
+    LongNumber& operator=(LongNumber&& n) noexcept;
+    LongNumber operator+(const LongNumber& n) const;
+    LongNumber& operator+=(const LongNumber& n);
+    LongNumber operator-(const LongNumber& n) const;
+    LongNumber& operator-=(const LongNumber& n);
+    LongNumber operator*(const LongNumber& n);
+    LongNumber& operator*=(const LongNumber& n);
+    LongNumber operator-() const;
+    bool operator==(const LongNumber& n) const;
+    bool operator!=(const LongNumber& n) const;
+    bool operator<(const LongNumber& n) const;
+    bool operator>(const LongNumber& n) const;
+    bool operator<=(const LongNumber& n) const;
+    bool operator>=(const LongNumber& n) const;
 
-	std::string getString() const;
-	
+    std::string getString() const;
+    
 private:
 
-	bool _absIsLess(const LongNumber& n) const;
-	bool _absIsGreater(const LongNumber& n) const;
-	bool _absIsEqual(const LongNumber& n) const;
-	static void _checkZero(LongNumber& n);
-	static BIG_TYPE _getMaxTypeValue();
-	
-	std::vector<TYPE> _data;
-	bool _negative = false;
+    bool _absIsLess(const LongNumber& n) const;
+    bool _absIsGreater(const LongNumber& n) const;
+    bool _absIsEqual(const LongNumber& n) const;
+    static void _checkZero(LongNumber& n);
+    static BIG_TYPE _getMaxTypeValue();
+    
+    std::vector<TYPE> _data;
+    bool _negative = false;
 };
