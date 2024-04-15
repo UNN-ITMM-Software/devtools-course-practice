8b3524efb9100367d84f009589d4ef5b6c016810
KirFedotov <k.fedotov.guit29@mail.ru>
2024-04-15 16:20:50 +0300

    fifth test and fixes

--- modules/complex-number/test/test_Fedotov_Kirill_complex_number.cpp
+++ modules/complex-number/test/test_Fedotov_Kirill_complex_number.cpp
@@ -7,8 +7,8 @@
 TEST(Fedotov_Kirill_ComplexNumberTest, defaultConstruct) {
     ComplexNumber z(re, im);
 
-    ASSERT_EQ(0.0, z.getRe());
-    ASSERT_EQ(0.0, z.getIm());
+    ASSERT_DOUBLE_EQ(0.0, z.getRe());
+    ASSERT_DOUBLE_EQ(0.0, z.getIm());
 }
 
 TEST(Fedotov_Kirill_ComplexNumberTest, divByZero) {
@@ -18,7 +18,24 @@ TEST(Fedotov_Kirill_ComplexNumberTest, divByZero) {
     ASSERT_ANY_THROW(u/v);
 }
 
+TEST(Fedotov_Kirill_ComplexNumberTest, setter) {
+    ComplexNumber u;
+    u.setRe(2.0);
+    u.setIm(5.1);
+
+    ASSERT_DOUBLE_EQ(u, ComplexNumber(2.0,5.1));
+}
+
+TEST(Fedotov_Kirill_ComplexNumberTest, equalOperator) {
+    ComplexNumber u;
+    ComplexNumber v(2.0,3.0);
+    u = v;
+
+    ASSERT_DOUBLE_EQ(2.0, u.getRe());
+    ASSERT_DOUBLE_EQ(3.0, u.getIm());
+}
+
 TEST(Fedotov_Kirill_ComplexNumberTest, substract) {
-   ASSERT_EQ(ComplexNumber u(2.0,1.0) - ComplexNumber(0.1, 1.0), ComplexNumber(1.9, 0.0));
+   ASSERT_DOUBLE_EQ(ComplexNumber u(2.0,1.0) - ComplexNumber(0.1, 1.0), ComplexNumber(1.9, 0.0));
 }
 
