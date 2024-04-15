1cb4a29168ca6a02b00b140203821a22bd601140
esthete014 <nikkochetov@outlook.com>
2024-04-15 03:22:10 +0300

    fix

--- modules/kochetov_n_djarvis/src/djarvis.cpp
+++ modules/kochetov_n_djarvis/src/djarvis.cpp
@@ -8,7 +8,7 @@ ConvexHull::ConvexHull(std::vector<Point>& _points) : points(_points) {}
 int ConvexHull::orientation(Point p, Point q, Point r) {
     int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
     if (val == 0) return 0;     // точки коллинеарны
-    return (val > 0) ? 1 : -1;  // положительный или отрицательный поворот
+    return (val > 0) ? 1 : 2;  // положительный или отрицательный поворот
 }
 
 std::vector<Point> ConvexHull::findConvexHull() {
--- modules/kochetov_n_djarvis/test/test_djarvis.cpp
+++ modules/kochetov_n_djarvis/test/test_djarvis.cpp
@@ -28,11 +28,11 @@ TEST(Kochetov_n_djarvis, ConvexPolygon) {
     EXPECT_EQ(hull[0].x, 0);
     EXPECT_EQ(hull[0].y, 0);
     EXPECT_EQ(hull[1].x, 1);
-    EXPECT_EQ(hull[1].y, 0);
-    EXPECT_EQ(hull[2].x, 3);
-    EXPECT_EQ(hull[2].y, 1);
-    EXPECT_EQ(hull[3].x, 2);
-    EXPECT_EQ(hull[3].y, 3);
+    EXPECT_EQ(hull[1].y, 2);
+    EXPECT_EQ(hull[2].x, 2);
+    EXPECT_EQ(hull[2].y, 3);
+    EXPECT_EQ(hull[3].x, 3);
+    EXPECT_EQ(hull[3].y, 1);
 }
 
 // Тест для поиска выпуклой оболочки для набора точек,
@@ -42,16 +42,16 @@ TEST(Kochetov_n_djarvis, ConcavePolygon) {
     ConvexHull convexHull(points);
     std::vector<Point> hull = convexHull.findConvexHull();
 
-    // Ожидаем, что выпуклая оболочка содержит 4 точки
-    ASSERT_EQ(hull.size(), 4);
+    // Ожидаем, что выпуклая оболочка содержит 5 точек
+    ASSERT_EQ(hull.size(), 5);
 
     // Ожидаем, что выпуклая оболочка содержит вершины выпуклой оболочки
     EXPECT_EQ(hull[0].x, 0);
     EXPECT_EQ(hull[0].y, 0);
     EXPECT_EQ(hull[1].x, 1);
-    EXPECT_EQ(hull[1].y, 0);
-    EXPECT_EQ(hull[2].x, 3);
-    EXPECT_EQ(hull[2].y, 1);
-    EXPECT_EQ(hull[3].x, 2);
-    EXPECT_EQ(hull[3].y, 3);
+    EXPECT_EQ(hull[1].y, 1);
+    EXPECT_EQ(hull[2].x, 2);
+    EXPECT_EQ(hull[2].y, 3);
+    EXPECT_EQ(hull[3].x, 3);
+    EXPECT_EQ(hull[3].y, 1);
 }
