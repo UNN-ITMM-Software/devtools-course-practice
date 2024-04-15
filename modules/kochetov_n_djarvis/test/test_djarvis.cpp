e9e0614b433de94282cb863755077bfcad6ea44b
esthete014 <nikkochetov@outlook.com>
2024-04-15 03:46:36 +0300

    дополнение для ekistrimova

--- modules/kochetov_n_djarvis/test/test_djarvis.cpp
+++ modules/kochetov_n_djarvis/test/test_djarvis.cpp
@@ -55,3 +55,18 @@ TEST(Kochetov_n_djarvis, ConcavePolygon) {
     EXPECT_EQ(hull[3].x, 3);
     EXPECT_EQ(hull[3].y, 1);
 }
+
+// Тест для поиска выпуклой оболочки для набора точек,
+// все из которых находятся на одной прямой
+TEST(ConvexHullTest, CollinearPoints) {
+    vector<Point> points = {{0, 0}, {1, 1}, {2, 2}, {3, 3}};
+    ConvexHull convexHull(points);
+    vector<Point> hull = convexHull.findConvexHull();
+
+    ASSERT_EQ(hull.size(), (size_t)2); // Ожидаем, что выпуклая оболочка содержит 2 точки (крайние)
+    // Ожидаем, что выпуклая оболочка содержит крайние точки
+    EXPECT_EQ(hull[0].x, 0);
+    EXPECT_EQ(hull[0].y, 0);
+    EXPECT_EQ(hull[1].x, 3);
+    EXPECT_EQ(hull[1].y, 3);
+}
