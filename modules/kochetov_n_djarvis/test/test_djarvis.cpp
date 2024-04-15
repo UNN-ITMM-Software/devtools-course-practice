ea48ac9492db812cdad2d4b75bf2e128274809df
esthete014 <nikkochetov@outlook.com>
2024-04-15 04:04:21 +0300

    fix для maximFLUNN

--- modules/kochetov_n_djarvis/test/test_djarvis.cpp
+++ modules/kochetov_n_djarvis/test/test_djarvis.cpp
@@ -71,3 +71,21 @@ TEST(Kochetov_n_djarvis, CollinearPoints) {
     EXPECT_EQ(hull[3].x, 3);
     EXPECT_EQ(hull[3].y, 3);
 }
+
+// Тест для поиска выпуклой оболочки для набора точек с повторяющимися точками
+TEST(Kochetov_n_djarvis, DuplicatePoints) {
+    std::vector<Point> points =
+    {{0, 0}, {1, 1}, {2, 2}, {0, 0}, {1, 1}, {2, 2}};
+    ConvexHull convexHull(points);
+    std::vector<Point> hull = convexHull.findConvexHull();
+
+    // Ожидаем, что выпуклая оболочка содержит 3 различные точки
+    ASSERT_EQ(hull.size(), (size_t)3);
+    // Ожидаем, что выпуклая оболочка содержит различные угловые точки
+    EXPECT_EQ(hull[0].x, 0);
+    EXPECT_EQ(hull[0].y, 0);
+    EXPECT_EQ(hull[1].x, 1);
+    EXPECT_EQ(hull[1].y, 1);
+    EXPECT_EQ(hull[2].x, 2);
+    EXPECT_EQ(hull[2].y, 2);
+}
