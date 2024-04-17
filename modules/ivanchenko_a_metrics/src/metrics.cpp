28712f27588270e014304f6c51bbc7ad59da4c95
aaldebarann <amivanchenko@yandex.ru>
2024-04-17 10:36:20 +0300

    size check in a separate function

--- modules/ivanchenko_a_metrics/src/metrics.cpp
+++ modules/ivanchenko_a_metrics/src/metrics.cpp
@@ -4,9 +4,12 @@
 
 #include <cmath>
 
-float metrics::l1(const std::vector<float>&v1, const std::vector<float>&v2) {
+void checkSize(const std::vector<float>&v1, const std::vector<float>&v2) {
     if (v1.size() != v2.size())
         throw std::invalid_argument("vectors have different sizes");
+}
+float metrics::l1(const std::vector<float>&v1, const std::vector<float>&v2) {
+    checkSize(v1, v2);
     float res = 0;
     for (size_t i = 0; i < v1.size(); i++) {
         res += std::abs(v1[i] - v2[i]);
@@ -14,8 +17,7 @@ float metrics::l1(const std::vector<float>&v1, const std::vector<float>&v2) {
     return res;
 }
 float metrics::l2(const std::vector<float>&v1, const std::vector<float>&v2) {
-    if (v1.size() != v2.size())
-        throw std::invalid_argument("vectors have different sizes");
+    checkSize(v1, v2);
     float res = 0;
     for (size_t i = 0; i < v1.size(); i++) {
         res += std::pow(std::abs(v1[i] - v2[i]), 2);
@@ -23,8 +25,7 @@ float metrics::l2(const std::vector<float>&v1, const std::vector<float>&v2) {
     return std::pow(res, 1.0 / 2);
 }
 float metrics::l3(const std::vector<float>&v1, const std::vector<float>&v2) {
-    if (v1.size() != v2.size())
-        throw std::invalid_argument("vectors have different sizes");
+    checkSize(v1, v2);
     float res = 0;
     for (size_t i = 0; i < v1.size(); i++) {
         res += std::pow(std::abs(v1[i] - v2[i]), 3);
@@ -32,8 +33,7 @@ float metrics::l3(const std::vector<float>&v1, const std::vector<float>&v2) {
     return std::pow(res, 1.0 / 3);
 }
 float metrics::l4(const std::vector<float>&v1, const std::vector<float>&v2) {
-    if (v1.size() != v2.size())
-        throw std::invalid_argument("vectors have different sizes");
+    checkSize(v1, v2);
     float res = 0;
     for (size_t i = 0; i < v1.size(); i++) {
         res += std::pow(std::abs(v1[i] - v2[i]), 4);
@@ -41,8 +41,7 @@ float metrics::l4(const std::vector<float>&v1, const std::vector<float>&v2) {
     return std::pow(res, 1.0 / 4);
 }
 float metrics::linf(const std::vector<float>&v1, const std::vector<float>&v2) {
-    if (v1.size() != v2.size())
-        throw std::invalid_argument("vectors have different sizes");
+    checkSize(v1, v2);
     float res = 0;
     for (size_t i = 0; i < v1.size(); i++) {
         res = std::max(res, std::abs(v1[i] - v2[i]));
