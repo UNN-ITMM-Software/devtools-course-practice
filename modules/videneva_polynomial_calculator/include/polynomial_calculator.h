6b04f1695c68611a9088450fd51b6ffd9a25d905
katrin000 <katya.videneva.03@mail.ru>
2024-04-17 15:56:42 +0300

    new commit

--- modules/videneva_polynomial_calculator/include/polynomial_calculator.h
+++ modules/videneva_polynomial_calculator/include/polynomial_calculator.h
@@ -10,17 +10,16 @@
 class polynomial_calculator {
  private:
     std::vector<double> coeff_a;
+    
  public:
     polynomial_calculator();
-    ~polynomial_calculator()=default;
+    ~polynomial_calculator() = default;
     explicit polynomial_calculator(double a0);
     polynomial_calculator(std::vector<double> A, int len);
     polynomial_calculator(const polynomial_calculator& P);
     polynomial_calculator& operator=(const polynomial_calculator& v);
     polynomial_calculator& operator=(polynomial_calculator&& v) noexcept;
-    polynomial_calculator (polynomial_calculator&& v) noexcept;
-   
-   
+    polynomial_calculator(polynomial_calculator&& v) noexcept;
     int GetSize() const;
     double value(double x);
 
@@ -31,7 +30,7 @@ class polynomial_calculator {
     polynomial_calculator operator +
         (const polynomial_calculator& P);
     polynomial_calculator operator -
-        (const polynomial_calculator& P); 
+        (const polynomial_calculator& P);
     polynomial_calculator operator *
         (const polynomial_calculator& P);
     polynomial_calculator operator + (const double& _Num) const;
