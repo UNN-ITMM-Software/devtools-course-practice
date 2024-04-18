5bca302b58deeb080e2f6b73faad400b6a4e7c6a
V-inTim <vinichuk.timofey@mail.ru>
2024-04-18 09:45:29 +0300

    changes in vector intialization

--- modules/vinichuk_t_binary_search/test/test_Vinichuk_Timofey_binary_search.cpp
+++ modules/vinichuk_t_binary_search/test/test_Vinichuk_Timofey_binary_search.cpp
@@ -6,33 +6,33 @@
 
 
 TEST(BinarySearchTest, test_find_middle) {
-    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+    std::vector<int> v {1, 2, 3, 4, 5, 6, 7};
     ASSERT_EQ(binary_search(v, 4), 3);
 }
 
 TEST(BinarySearchTest, test_not_found) {
-    std::vector<int> v = { 1, 2, 3, 4, 5, 6, 7 };
+    std::vector<int> v { 1, 2, 3, 4, 5, 6, 7 };
     ASSERT_EQ(binary_search(v, 8), -1);
 }
 
 TEST(BinarySearchTest, test_empty_array) {
-    std::vector<int> v1 = { };
-    std::vector<double> v2 = { };
+    std::vector<int> v1;
+    std::vector<double> v2;
 
     ASSERT_EQ(binary_search(v1, 8), -1);
     ASSERT_EQ(binary_search(v2, 5.89), -1);
 }
 
 TEST(BinarySearchTest, test_one_element_found) {
-    std::vector<int> v1 = { 1 };
-    std::vector<double> v2 = { 2.005 };
+    std::vector<int> v1 { 1 };
+    std::vector<double> v2 { 2.005 };
 
     ASSERT_EQ(binary_search(v1, 1), 0);
     ASSERT_EQ(binary_search(v2, 2.005), 0);
 }
 
 TEST(BinarySearchTest, test_one_element_not_found) {
-    std::vector<int> v = { 1 };
+    std::vector<int> v { 1 };
 
     ASSERT_EQ(binary_search(v, 2), -1);
     ASSERT_EQ(binary_search(v, 10), -1);
@@ -48,16 +48,16 @@ TEST(BinarySearchTest, test_large_array) {
 }
 
 TEST(BinarySearchTest, test_first_element) {
-    std::vector<int> v1 = { 1, 4, 6, 59, 345 };
-    std::vector<char> v2 = { 'a', 'd', 'u', 'z'};
+    std::vector<int> v1 { 1, 4, 6, 59, 345 };
+    std::vector<char> v2 { 'a', 'd', 'u', 'z'};
 
     ASSERT_EQ(binary_search(v1, 1), 0);
     ASSERT_EQ(binary_search(v2, 'a'), 0);
 }
 
 TEST(BinarySearchTest, test_last_element) {
-    std::vector<int> v1 = { 1, 4, 6, 59, 345 };
-    std::vector<char> v2 = { 'a', 'd', 'u', 'y', 'z'};
+    std::vector<int> v1 { 1, 4, 6, 59, 345 };
+    std::vector<char> v2 { 'a', 'd', 'u', 'y', 'z'};
 
     ASSERT_EQ(binary_search(v1, 345), 4);
     ASSERT_EQ(binary_search(v2, 'z'), 4);
