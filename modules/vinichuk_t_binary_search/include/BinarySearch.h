761776ea81e071dc79b004978c71321bf53afff6
V-inTim <vinichuk.timofey@mail.ru>
2024-04-20 11:30:51 +0300

    remove static

--- modules/vinichuk_t_binary_search/include/BinarySearch.h
+++ modules/vinichuk_t_binary_search/include/BinarySearch.h
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <vector>
 
-static const int NOT_LISTED = -1;
+const int NOT_LISTED = -1;
 
 // implementing a template function implies
 // its definition in the place of declaration(header file)
