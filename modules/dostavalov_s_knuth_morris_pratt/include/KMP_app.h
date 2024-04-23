697100aa3cf40497e3c390e045651d972af5b837
Kudinov Nikita <kudinov.nikita@gmail.com>
2024-04-23 22:29:22 +0300

    feat: add task 3

---
+++ modules/dostavalov_s_knuth_morris_pratt/include/KMP_app.h
@@ -0,0 +1,14 @@
+// Copyright 2024 Kudinov Nikita
+#pragma once
+
+#include "include/KMP.h"
+
+class KMPApplication {
+ public:
+  KMPApplication() = default;
+  std::string operator()(int argc, char* argv[]);
+ private:
+  std::string outputMessage;
+  bool validate(int argc, char* argv[]);
+  void help(const char* appName);
+};
