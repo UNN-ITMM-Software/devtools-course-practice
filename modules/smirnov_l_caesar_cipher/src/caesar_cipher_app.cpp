92a12d00e7206175e96f8076a2de679ca370213c
Frenetz <xodyrev.fedya@mail.ru>
2024-04-25 22:46:04 +0300

    5

--- modules/smirnov_l_caesar_cipher/src/caesar_cipher_app.cpp
+++ modules/smirnov_l_caesar_cipher/src/caesar_cipher_app.cpp
@@ -14,7 +14,8 @@ bool CaesarCipherApplication::Validate(int argc, char *argv[]) {
     } else if (std::strcmp(argv[1], "--help") == 0) {
         Help(argv[0]);
         return false;
-    } else if (std::strcmp(argv[2], "encryption") != 0 && std::strcmp(argv[2], "decryption") != 0){
+    } else if (std::strcmp(argv[2], "encryption") != 0
+     && std::strcmp(argv[2], "decryption") != 0) {
         Help(argv[0], "Operation must be either 'encryption' or 'decryption'.");
         return false;
     } else if (argc != required_argc) {
@@ -33,7 +34,8 @@ bool CaesarCipherApplication::Validate(int argc, char *argv[]) {
     return true;
 }
 
-void CaesarCipherApplication::Help(const char *application, const char *errMsg) {
+void CaesarCipherApplication::Help(const char *application,
+                                         const char *errMsg) {
     std::stringstream message;
 
     if (errMsg) {
@@ -44,22 +46,22 @@ void CaesarCipherApplication::Help(const char *application, const char *errMsg) 
     message << '\t' << application << " <offset> <operation> <string>\n";
     message << "Where:\n";
     message << '\t' << " <offset> is offset during encryption/decryption\n";
-    message << '\t' << " <operation> is the current operation, which can take the value 'encryption' and 'decryption'\n";
-    message << '\t' << " <string> is the current line that the program is working with\n";
+    message << '\t' << " <operation> is the current operation\n";
+    message << '\t' << " <string> is the input\n";
     msg = message.str();
 }
 
-std::string CaesarCipherApplication::CaesarCipherCryptography(int argc, char *argv[]) {
+std::string CaesarCipherApplication::CaesarCipherCryptography
+(int argc, char *argv[]) {
     if (Validate(argc, argv)) {
         try {
             if (std::strcmp(argv[2], "encryption") == 0) {
                 std::string output = cipher.CaesarCipherEncoder(argv[3]);
                 return output;
-            }
-            else if (std::strcmp(argv[2], "decryption") == 0) {
-                std::string output = cipher.CaesarCipherDecoder(argv[3]);                
+            } else if (std::strcmp(argv[2], "decryption") == 0) {
+                std::string output = cipher.CaesarCipherDecoder(argv[3]);
                 return output;
-            }            
+            }
         }
         catch(const std::exception& e) {
             return "Error: Invalid input.";
