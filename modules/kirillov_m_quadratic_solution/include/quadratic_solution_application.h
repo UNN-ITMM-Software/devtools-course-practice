aec87dacdbece66cc41fe45f6714dda2d3361a08
SmirnovLM <smirnoff.2003@yandex.ru>
2024-04-27 01:09:56 +0300

    fix

--- modules/kirillov_m_quadratic_solution/include/quadratic_solution_application.h
+++ modules/kirillov_m_quadratic_solution/include/quadratic_solution_application.h
@@ -7,13 +7,13 @@
 #include <string>
 
 class QuadraticSolverApplication {
-private:
+ private:
     std::string msg;
     QuadraticSolver solver;
     void Help(const char* appName, const char* errorMsg = nullptr);
     bool Validate(int argc, char* argv[]);
 
-public:
+ public:
     QuadraticSolverApplication() = default;
     std::string SolveQuadraticEquation(int argc, char* argv[]);
 };
