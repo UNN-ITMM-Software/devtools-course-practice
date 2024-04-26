7ab72dbd147ed7edc18be2b629c780f4c3b14c51
luchshiy-shmel <150447256+luchshiy-shmel@users.noreply.github.com>
2024-04-26 19:16:48 +0300

    Шмелев Иван. Лабораторная работа №3 (#294)
    * task1
    
    * fix codestyle
    
    * fix codestyle2
    
    * fix codestyle3
    
    * add test

--- modules/pozdnyakov_v_tower_of_hanoi/test/test_tower_of_hanoi.cpp
+++ modules/pozdnyakov_v_tower_of_hanoi/test/test_tower_of_hanoi.cpp
@@ -6,56 +6,60 @@
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_negative) {
     int rings = -10;
-
-    ASSERT_ANY_THROW(TowerOfHanoi::CalculateSteps(rings));
+    TowerOfHanoi tower;
+    ASSERT_ANY_THROW(tower.CalculateSteps(rings));
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_zero) {
     int rings = 0;
-
-    ASSERT_ANY_THROW(TowerOfHanoi::CalculateSteps(rings));
+    TowerOfHanoi tower;
+    ASSERT_ANY_THROW(tower.CalculateSteps(rings));
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_positive) {
     int rings = 10;
-
-    ASSERT_NO_THROW(TowerOfHanoi::CalculateSteps(rings));
+    TowerOfHanoi tower;
+    ASSERT_NO_THROW(tower.CalculateSteps(rings));
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_1) {
     int rings = 1;
+    TowerOfHanoi tower;
     std::vector<std::vector<int>> answer = {{1, 3}};
     std::vector<std::vector<int>> result;
 
-    result = TowerOfHanoi::CalculateSteps(rings);
+    result = tower.CalculateSteps(rings);
 
     ASSERT_EQ(answer, result);
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_2) {
     int rings = 2;
+    TowerOfHanoi tower;
     std::vector<std::vector<int>> answer = {{1, 2}, {1, 3}, {2, 3}};
     std::vector<std::vector<int>> result;
 
-    result = TowerOfHanoi::CalculateSteps(rings);
+    result = tower.CalculateSteps(rings);
 
     ASSERT_EQ(answer, result);
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_3) {
     int rings = 3;
+    TowerOfHanoi tower;
     std::vector<std::vector<int>> answer = {{1, 3}, {1, 2}, {3, 2},
                                             {1, 3}, {2, 1}, {2, 3},
                                             {1, 3}};
     std::vector<std::vector<int>> result;
 
-    result = TowerOfHanoi::CalculateSteps(rings);
+    result = tower.CalculateSteps(rings);
 
     ASSERT_EQ(answer, result);
 }
 
 TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_4) {
     int rings = 4;
+    TowerOfHanoi tower;
     std::vector<std::vector<int>> answer = {{1, 2}, {1, 3}, {2, 3},
                                             {1, 2}, {3, 1}, {3, 2},
                                             {1, 2}, {1, 3}, {2, 3},
@@ -63,7 +67,23 @@ TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_4) {
                                             {1, 2}, {1, 3}, {2, 3}};
     std::vector<std::vector<int>> result;
 
-    result = TowerOfHanoi::CalculateSteps(rings);
+    result = tower.CalculateSteps(rings);
+
+    ASSERT_EQ(answer, result);
+}
+
+TEST(Pozdnyakov_Vasya_HanoiTests, number_of_rings_is_5) {
+    int rings = 5;
+    TowerOfHanoi tower;
+    std::vector<std::vector<int>> answer = { { 1, 3 }, { 1, 2 },
+    { 3, 2 }, { 1, 3 }, { 2, 1 }, { 2, 3 }, { 1, 3 }, { 1, 2 },
+    { 3, 2 }, { 3, 1 }, { 2, 1 }, { 3, 2 }, { 1, 3 }, { 1, 2 },
+    { 3, 2 }, { 1, 3 }, { 2, 1 }, { 2, 3 }, { 1, 3 }, { 2, 1 },
+    { 3, 2 }, { 3, 1 }, { 2, 1 }, { 2, 3 }, { 1, 3 }, { 1, 2 },
+    { 3, 2 }, { 1, 3 }, { 2, 1 }, { 2, 3 }, { 1, 3 } };
+    std::vector<std::vector<int>> result;
+
+    result = tower.CalculateSteps(rings);
 
     ASSERT_EQ(answer, result);
 }
