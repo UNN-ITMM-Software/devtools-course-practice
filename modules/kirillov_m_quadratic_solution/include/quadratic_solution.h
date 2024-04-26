cc8bdaa5e8f3f82a24a868cbf76f99c3647f8546
SmirnovLM <smirnoff.2003@yandex.ru>
2024-04-27 01:57:17 +0300

    fix

--- modules/kirillov_m_quadratic_solution/include/quadratic_solution.h
+++ modules/kirillov_m_quadratic_solution/include/quadratic_solution.h
@@ -27,7 +27,7 @@ class QuadraticSolver {
     double b;
     double c;
  public:
-    explicit QuadraticSolver(double a = 0, double b = 0, double c = 0)
+    explicit QuadraticSolver(const double a = 0, const double b = 0, const double c = 0)
     : a(a), b(b), c(c) {}
     double getA() const;
     double getB() const;
