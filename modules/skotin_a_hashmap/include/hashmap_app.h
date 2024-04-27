2dcbd2e42cdd364394ac7c1846625d649dcec3e7
hadnle-less <salaev-08@mail.ru>
2024-04-27 18:46:55 +0300

    application header

---
+++ modules/skotin_a_hashmap/include/hashmap_app.h
@@ -0,0 +1,19 @@
+// Copyright 2024 Salaev Vladislav
+
+#ifndef MODULES_SKOTIN_A_HASHMAP_INCLUDE_HASHMAP_APP_H_
+#define MODULES_SKOTIN_A_HASHMAP_INCLUDE_HASHMAP_APP_H_
+
+#include <iostream>
+#include <string>
+#include "include/hashmap.h"
+
+class Application {
+public:
+    void run(int argc, char** argv);
+
+private:
+    HashMap<std::string, std::string> hashMap;
+    std::string processCommand(const std::string& command, const std::string& key, const std::string& value = "");
+};
+
+#endif  //MODULES_SKOTIN_A_HASHMAP_INCLUDE_HASHMAP_APP_H_
