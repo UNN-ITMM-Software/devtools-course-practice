dc481b0abfa0af7c99bec188cc041366d71c4fea
nik <lesnikowka@mail.ru>
2024-04-27 18:48:43 +0300

    ctest

--- modules/soloninko_a_huffman_code/include/huffman_app.h
+++ modules/soloninko_a_huffman_code/include/huffman_app.h
@@ -16,8 +16,11 @@ class HuffmanApp {
 		 if (validateStr) {
 			 return validateStr;
 		 }
-		 
-		 if (argc == 2) {
+
+		 if (argc == 1) {
+			 return _help(argv[0]);
+		 }
+		 else if (argc == 2) {
 			 return encode(argc, argv);
 		 }
 
@@ -26,6 +29,7 @@ class HuffmanApp {
 
 	 std::string encode(int argc, char** argv) {
 		 std::string data = argv[1];
+		 std::stringstream message;
 
 		 Huffman huffman;
 
@@ -33,26 +37,22 @@ class HuffmanApp {
 		 std::string encoded = huffman.encode(data);
 		 std::unordered_map<char, std::string> codes = huffman.getHaffmanCodesCharStr();
 
-		 _message << encoded << std::endl;
+		 message << encoded << " ";
 
 		 for (const auto& i : codes) {
-			 _message << i.first << " " << i.second << std::endl;
+			 message << i.first << " " << i.second << " ";
 		 }
 
-		 return _message.str();
+		 return message.str();
 	 }
 
 	 std::string decode(int argc, char** argv) {
 		 Huffman huffman;
 
-		 _message << huffman.decode(argv[1], _getCodes(argc, argv)) << std::endl;
-
-		 return _message.str();
+		 return huffman.decode(argv[1], _getCodes(argc, argv));
 	 }
 
  private:
-	 
-
 	 std::unordered_map<std::string, char> _getCodes(int argc, char** argv) {
 		 std::unordered_map<std::string, char> codes;
 
@@ -79,7 +79,7 @@ class HuffmanApp {
 	 }
 
 	 bool _validateNumArgs(int num) {
-		 return num % 2 == 0;
+		 return num == 1 || num % 2 == 0;
 	 }
 	 bool _validateEncoded(const char* str) {
 		 for (int i = 0; i < strlen(str); i++) {
@@ -105,18 +105,20 @@ class HuffmanApp {
 		 return true;
 	 }
 
-	 void _help(const std::string& appName, const std::string* message)
+	 std::string _help(const char* appName)
 	 {
-		 _message << "	This is an application that allows you to \
-			find the optimal Huffman code for a given sequence \
-			of characters. This operation allows you to compress data.\n \
-			For encoding, you must enter the data to be compressed in \
-			text format. The output will be a compressed string and \
-			a list of new character codes.\n \
-			To decode, you must enter a string and a list of \
-			character codes in the format:\
-			a 10101 b 110 ....";
+		 std::stringstream message;
+		 message << "This is an application that allows you to \
+find the optimal Huffman code for a given sequence \
+of characters. This operation allows you to compress data.\n \
+For encoding, you must enter the data to be compressed in \
+text format.\n";
+		 message << appName << " abc\n";
+		 message << "The output will be a compressed string and \
+a list of new character codes.\n \
+To decode, you must enter a string and a list of \
+character codes in the format:\n";
+		message << appName << " 10101001 a 10101 b 110 ....";
+		return message.str();
 	 }
-
-	 std::stringstream _message;
 };
