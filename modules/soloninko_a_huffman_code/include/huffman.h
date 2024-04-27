de405e09cc5baea8c97751b4f92423a8348a409e
nik <lesnikowka@mail.ru>
2024-04-27 21:47:11 +0300

    cpp

--- modules/soloninko_a_huffman_code/include/huffman.h
+++ modules/soloninko_a_huffman_code/include/huffman.h
@@ -17,154 +17,24 @@ struct Node {
 
 class Huffman {
  public:
-    void buildHuffmanTree(const std::string& text) {
-        _clear();
-
-        std::unordered_map<char, int> freq = _collectFrequency(text);
-
-        _pq = std::priority_queue<Node*, std::vector<Node*>,
-            bool (*)(Node*, Node*)>(
-            [](Node* l, Node* r) { return l->freq > r->freq; });
-
-        _buildHuffmanTree(freq);
-
-        _collectCodes(_pq.top(), "");
-    }
-
-    std::string encode(const std::string& text) {
-        std::string str = "";
-        for (const char& ch : text) {
-            str += _huffmanCode[ch];
-        }
-
-        return str;
-    }
-
-    std::string decode(const std::string& str) {
-        std::string dec_str;
-
-        int index = -1;
-        while (index < static_cast<int>(str.size()) - 1) {
-            dec_str = _decode(_pq.top(), index, str, dec_str);
-        }
-
-        return dec_str;
-    }
-
+    void buildHuffmanTree(const std::string& text);
+    std::string encode(const std::string& text);
+    std::string decode(const std::string& str);
     std::string decode(const std::string& str,
-        const std::unordered_map<std::string, char>& codes) {
-        int prev = 0;
-
-        std::string decoded;
-
-        for (int i = 0; i < static_cast<int>(str.size()); i++) {
-            std::string cur = str.substr(prev, i - prev + 1);
-
-            auto it = codes.find(cur);
-            if (it != codes.end()) {
-                decoded += (*it).second;
-                prev = i + 1;
-            }
-        }
-
-        return decoded;
-    }
-
-    std::unordered_map<std::string, char> getHaffmanCodesStrChar() {
-        std::unordered_map<std::string, char> huffmanCode;
-
-        for (const auto& i : _huffmanCode) {
-            huffmanCode[i.second] = i.first;
-        }
-
-        return huffmanCode;
-    }
-
-    std::unordered_map<char, std::string> getHaffmanCodesCharStr() {
-        return _huffmanCode;
-    }
+        const std::unordered_map<std::string, char>& codes);
+    std::unordered_map<std::string, char> getHaffmanCodesStrChar();
+    std::unordered_map<char, std::string> getHaffmanCodesCharStr();
 
  private:
     void _clearTree(std::priority_queue<Node*,
-        std::vector<Node*>, bool (*)(Node*, Node*)>& pq) {
-        while (!pq.empty()) {
-            Node* cur = pq.top();
-            pq.pop();
-            delete cur;
-        }
-    }
-
-    Node* _getNode(char ch, int freq, Node* left, Node* right) {
-        Node* node = new Node();
-
-        node->ch = ch;
-        node->freq = freq;
-        node->left = left;
-        node->right = right;
-
-        return node;
-    }
-
-    void _collectCodes(Node* root, std::string str) {
-        if (root == nullptr)
-            return;
-
-        if (!root->left && !root->right) {
-            _huffmanCode[root->ch] = str;
-        }
-
-        _collectCodes(root->left, str + "0");
-        _collectCodes(root->right, str + "1");
-    }
-
+        std::vector<Node*>, bool (*)(Node*, Node*)>& pq);
+    Node* _getNode(char ch, int freq, Node* left, Node* right);
+    void _collectCodes(Node* root, std::string str);
     std::string _decode(Node* root, int& index, std::string str
-        , std::string& dec_str) {
-        if (root == nullptr)
-            return dec_str;
-
-        if (!root->left && !root->right) {
-            dec_str += root->ch;
-            return dec_str;
-        }
-
-        index++;
-
-        if (str[index] == '0')
-            _decode(root->left, index, str, dec_str);
-        else
-            _decode(root->right, index, str, dec_str);
-
-        return dec_str;
-    }
-
-    std::unordered_map<char, int> _collectFrequency(const std::string& text) {
-        std::unordered_map<char, int> freq;
-
-        for (const char& ch : text)
-            freq[ch]++;
-
-        return freq;
-    }
-
-    void _buildHuffmanTree(const std::unordered_map<char, int>& freq) {
-        for (const auto& pair : freq)
-            _pq.push(_getNode(pair.first, pair.second,
-                nullptr, nullptr));
-
-        while (_pq.size() != 1) {
-            Node* left = _pq.top();
-            _pq.pop();
-            Node* right = _pq.top();
-            _pq.pop();
-            int sum = left->freq + right->freq;
-            _pq.push(_getNode('\0', sum, left, right));
-        }
-    }
-
-    void _clear() {
-        _clearTree(_pq);
-        _huffmanCode.clear();
-    }
+        , std::string& dec_str);
+    std::unordered_map<char, int> _collectFrequency(const std::string& text);
+    void _buildHuffmanTree(const std::unordered_map<char, int>& freq);
+    void _clear();
 
     std::priority_queue<Node*, std::vector<Node*>,
         bool (*)(Node*, Node*)> _pq;
--- modules/soloninko_a_huffman_code/include/huffman_app.h
+++ modules/soloninko_a_huffman_code/include/huffman_app.h
@@ -14,108 +14,16 @@
 
 class HuffmanApp {
  public:
-    std::string operator()(int argc, char** argv) {
-        std::string validateStr = _validate(argc, argv);
-        if (validateStr.size() > 0) {
-            return validateStr;
-        }
-        if (argc == 1) {
-            return _help(argv[0]);
-        } else if (argc == 2) {
-            return encode(argc, argv);
-        }
-        return decode(argc, argv);
-    }
-
-    std::string encode(int argc, char** argv) {
-        std::string data = argv[1];
-        std::stringstream message;
-
-        Huffman huffman;
-
-        huffman.buildHuffmanTree(data);
-        std::string encoded = huffman.encode(data);
-        std::unordered_map<char, std::string> codes =
-            huffman.getHaffmanCodesCharStr();
-
-        message << encoded << " ";
-
-        for (const auto& i : codes) {
-            message << i.first << " " << i.second << " ";
-        }
-
-        return message.str();
-    }
-
-    std::string decode(int argc, char** argv) {
-        Huffman huffman;
-
-        return huffman.decode(argv[1], _getCodes(argc, argv));
-    }
+    std::string operator()(int argc, char** argv);
+    std::string encode(int argc, char** argv);
+    std::string decode(int argc, char** argv);
 
  private:
     std::unordered_map<std::string, char> _getCodes(int argc,
-        char** argv) {
-        std::unordered_map<std::string, char> codes;
-
-        for (int i = 2; i < argc - 1; i += 2) {
-            codes[argv[i + 1]] = argv[i][0];
-        }
-
-        return codes;
-    }
-
-    std::string _validate(int argc, char** argv) {
-        if (!_validateNumArgs(argc)) {
-            return "Incorrect number of arguments!";
-        }
-        if (argc > 2 && !_validateEncoded(argv[1])) {
-            return "Invalid string for decoding!";
-        }
-        if (!_validateCodes(argc, argv)) {
-            return "Invalid codes!";
-        }
-        return "";
-    }
-
-    bool _validateNumArgs(int num) {
-        return num == 1 || num % 2 == 0;
-    }
-    bool _validateEncoded(const char* str) {
-        for (int i = 0; i < static_cast<int>(strlen(str)); i++) {
-            if (str[i] != '1' && str[i] != '0') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool _validateCodes(int argc, char** argv) {
-        for (int i = 2; i < argc - 1; i += 2) {
-            if (strlen(argv[i]) > 1) {
-                return false;
-            }
-            if (!_validateEncoded(argv[i+1])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    std::string _help(const char* appName) {
-        std::stringstream message;
-        message << "This is an application that allows you to ";
-        message << "find the optimal Huffman code for a given sequence ";
-        message << "of characters.This operation ";
-        message << "allows you to compress data.\n ";
-        message << "For encoding, you must enter the data to be compressed in ";
-        message << "text format.\n";
-        message << appName << " abc\n";
-        message << "The output will be a compressed string and ";
-        message << "a list of new character codes.\n ";
-        message << "To decode, you must enter a string and a list of ";
-        message << "character codes in the format:\n";
-        message << appName << " 10101001 a 10101 b 110 ....";
-        return message.str();
-    }
+         char** argv);
+    std::string _validate(int argc, char** argv);
+    bool _validateNumArgs(int num);
+    bool _validateEncoded(const char* str);
+    bool _validateCodes(int argc, char** argv);
+    std::string _help(const char* appName);
 };
