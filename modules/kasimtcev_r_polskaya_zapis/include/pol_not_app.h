8ef62942c329ae790a18670e6a79483b8cb32cd4
SokolovaDaria <105080743+SokolovaDaria@users.noreply.github.com>
2024-04-29 00:17:52 +0300

    Соколова Дарья. Лабораторная работа №3 (#313)
    * lab3
    
    * fix1
    
    * fix2
    
    * fix3
    
    * fix4
    
    * Added test
    
    * fix4
    
    * fix5
    
    * fix6
    
    * fix7

---
+++ modules/kasimtcev_r_polskaya_zapis/include/pol_not_app.h
@@ -0,0 +1,22 @@
+// Copyright 2024 Sokolova Daria
+
+#pragma once
+
+#include <string>
+
+#include "include/pol_not.h"
+
+class PolNotApp {
+ public:
+    PolNotApp() = default;
+
+    std::string operator()(int argc, char* argv[]);
+
+ private:
+    std::string helpMessage{};
+    std::string input{};
+
+    bool validate(int argc, char* argv[]);
+
+    void help(const char* appName, const char* errorMessage = nullptr);
+};
