c1848a9d537b3217f51907f9ce146789a1013b46
Stepa123de <stepa.kashin.2018@gmail.com>
2024-04-28 12:38:40 +0300

    Update console_handler.cpp

--- modules/yurin_a_prime_numbers_in_range/src/console_handler.cpp
+++ modules/yurin_a_prime_numbers_in_range/src/console_handler.cpp
@@ -6,18 +6,14 @@
 
 std::string ConsoleHandler::vectorToString(const std::vector<uint32_t> &vec) {
     std::stringstream ss;
-
     ss << "{";
-
     for (size_t i = 0; i < vec.size(); ++i) {
         ss << vec[i];
         if (i != vec.size() - 1) {
             ss << ", ";
         }
     }
-
     ss << "}";
-
     return ss.str();
 }
 
@@ -25,10 +21,8 @@ std::string ConsoleHandler::ConsoleInput(int argc, char **argv) {
   if (argc < 3) {
     return NO_ARGUMENTS;
   }
-
   uint32_t left = argv[1];
   uint32_t right = argv[2];
-
   return ConsoleHandler::vectorToString(
     primeNumbers.getPrimeNumbersInRange(left, right));
 }
