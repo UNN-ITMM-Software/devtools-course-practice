b9d0c2d522fcb93a34daec2e1667c1dbb69bb77e
Stepa123de <stepa.kashin.2018@gmail.com>
2024-04-28 12:32:14 +0300

    +++

--- modules/yurin_a_prime_numbers_in_range/src/console_handler.cpp
+++ modules/yurin_a_prime_numbers_in_range/src/console_handler.cpp
@@ -1,27 +1,27 @@
 // Copyright 2024 Kashin Stepan
 
-#include "include/getting_prime_numbers.h"
-
 #include <sstream>
+
+#include "include/getting_prime_numbers.h"
 
 std::string ConsoleHandler::vectorToString(const std::vector<uint32_t> &vec) {
     std::stringstream ss;
-    
+
     ss << "{";
-    
+
     for (size_t i = 0; i < vec.size(); ++i) {
         ss << vec[i];
         if (i != vec.size() - 1) {
             ss << ", ";
         }
     }
-    
+
     ss << "}";
-    
+
     return ss.str();
 }
 
-std::string ConsoleHandler::ConsoleInput(int argc,char **argv) {
+std::string ConsoleHandler::ConsoleInput(int argc, char **argv) {
   if (argc < 3) {
     return NO_ARGUMENTS;
   }
@@ -29,5 +29,7 @@ std::string ConsoleHandler::ConsoleInput(int argc,char **argv) {
   uint32_t left = argv[1];
   uint32_t right = argv[2];
 
-  return ConsoleHandler::vectorToString(primeNumbers.getPrimeNumbersInRange(left,right))
+  return ConsoleHandler::vectorToString(
+    primeNumbers.getPrimeNumbersInRange(left, right)
+    );
 }
