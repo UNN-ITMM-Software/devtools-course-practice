715a0e72bedd647260017c86db5cd5d7b84d1fd2
BlackAngel2108 <luba.sobol.nn@mail.ru>
2024-05-02 00:02:18 +0300

    last

--- modules/borisov_s_stack/src/stack_app.cpp
+++ modules/borisov_s_stack/src/stack_app.cpp
@@ -12,23 +12,21 @@ bool StackApp::validate(int argc, char* argv[]) {
     } else if (argc == 2 && std::strcmp(argv[1], "--help") == 0) {
         help(argv[0]);
         return false;
-    }
-    int count = 0;
-    while (count < argc)
-    if (
+    } else if (
         std::strcmp(argv[1] + count, "pop") != 0
         && std::strcmp(argv[1] + count, "show_top") != 0
         && std::strcmp(argv[1] + count, "isFull") != 0
         && std::strcmp(argv[1] + count, "isEmpty") != 0
         && std::strcmp(argv[1] + count, "current_size") != 0
-        && std::strcmp(argv[1] + count, "clear") != 0
-        && std::strcmp(argv[1] + count, "push") != 0) {
+        && std::strcmp(argv[1] + count, "clear") != 0) {
         help(argv[0], "Incorrect second argument");
         return false;
-    } else if (std::strcmp(argv[1] + count, "push") == 0 && count + 1 < argc) {
-        count += 2;
-    } else {
-        count++;
+    } else if (argc == 3 && std::strcmp(argv[1], "push") != 0) {
+        help(argv[0]);
+        return false;
+    } else if (argc > 3) {
+        help(argv[0], "Incorrect second argument");
+        return false;
     }
     return true;
 }
@@ -49,43 +47,33 @@ const char* message) {
 
 std::string StackApp::operator()(int argc, char* argv[]) {
     if (validate(argc, argv)) {
-        int count = 0;
         std::ostringstream stream;
-        while (count < argc) {
-            std::string operation = std::string(argv[1] + count);
+            std::string operation = std::string(argv[1]);
             if (operation == "pop") {
                     Mystack.pop();
                     stream << "Pop completed\n";
-                    count +1;
             } else if (operation == "show_top") {
                     T temp = Mystack.show_top();
                     stream << "Top: " << temp << "\n";
-                    count + 1;
             } else if (operation == "isFull") {
                     bool temp = Mystack.isFull();
                     stream << "isFull: " << temp << "\n";
-                    count + 1;
             } else if (operation == "isEmpty") {
                 bool temp = Mystack.isEmpty();
                 stream << "isEmpty: " << temp << "\n";
-                count + 1;
             } else if (operation == "current_size") {
                     bool temp = Mystack.current_size();
                     stream << "current_size: " << temp << "\n";
-                    count + 1;
             } else if (operation == "clear") {
                     Mystack.clear();
                     stream << "clear completed \n";
-                    count + 1;
             } else if (operation == "push") {
                 int input_string = std::stoi(argv[2] + count);
                 Mystack.push(input_string);
                 stream << "push completed \n";
-                count + 2;
             }
         }
         message_ = stream.str();
-    }
     return message_;
 }
 
