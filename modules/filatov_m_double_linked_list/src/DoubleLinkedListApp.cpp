fd7403492a88414c43c9d35d36ae3cf48d4cc332
aaldebarann <amivanchenko@yandex.ru>
2024-05-01 15:43:41 +0300

    minor changes

--- modules/filatov_m_double_linked_list/src/DoubleLinkedListApp.cpp
+++ modules/filatov_m_double_linked_list/src/DoubleLinkedListApp.cpp
@@ -2,12 +2,8 @@
 
 #include "include/DoubleLinkedListApp.h"
 
-#define HELP "Usage: ./target [options]\nOptinos:\n    --print\n    --reverse\n    --push_back\n    --push_front\n    --help\nNotion: you can use the same option several times"
-#define UNRECOGNIZED_OPTIONS_ERROR "Error: unrecognized command-line option. Run --help for all supported options"
-#define PUSH_BACK_NO_ARGUMENTS_ERROR "Error: --push_back option must have one argument. Run --help for all supported options"
-#define PUSH_FRONT_NO_ARGUMENTS_ERROR "Error: --push_front option must have one argument. Run --help for all supported options"
-
-std::vector<std::string> DoubleLinkedListApp::runApp(int argc, const char** argv) {
+std::vector<std::string> DoubleLinkedListApp::runApp(int argc,
+ const char** argv) {
     std::vector<std::string> output;
     auto args = parse(argc, argv);
     for (size_t i = 0; i < args.size(); i++) {
@@ -18,28 +14,35 @@ std::vector<std::string> DoubleLinkedListApp::runApp(int argc, const char** argv
         } else if (args[i] == "--print") {
             print(output);
         } else if (args[i] == "--push_back") {
-            if(i + 1 < args.size()) {
+            if (i + 1 < args.size()) {
                 push_back(output, args[i + 1]);
                 i++;
             } else {
-                output.push_back(PUSH_BACK_NO_ARGUMENTS_ERROR);
+                output.push_back("Error:");
+                output.push_back("--push_back option must have one argument.");
+                output.push_back("Run --help for all supported options");
             }
         } else if (args[i] == "--push_front") {
-            if(i + 1 < args.size()) {
+            if (i + 1 < args.size()) {
                 push_front(output, args[i + 1]);
                 i++;
             } else {
-                output.push_back(PUSH_FRONT_NO_ARGUMENTS_ERROR);
-            }
+                output.push_back("Error:");
+                output.push_back("--push_front option must have one argument.");
+                output.push_back("Run --help for all supported options");
+           }
         } else {
-            output.push_back(UNRECOGNIZED_OPTIONS_ERROR);
+            output.push_back("Error:");
+            output.push_back("unrecognized command-line option.");
+            output.push_back("Run --help for all supported options");
             break;
         }
     }
     return output;
-} 
+}
 
-std::vector<std::string> DoubleLinkedListApp::parse(int argc, const char** argv) {
+std::vector<std::string> DoubleLinkedListApp::parse(int argc,
+ const char** argv) {
     std::vector<std::string> input;
     for (int i = 1; i < argc; i++) {
         const char* ptr = argv[i];
@@ -50,7 +53,14 @@ std::vector<std::string> DoubleLinkedListApp::parse(int argc, const char** argv)
 }
 
 void DoubleLinkedListApp::help(std::vector<std::string>& output) {
-    output.push_back(HELP);
+    output.push_back("Usage: ./target [options]");
+    output.push_back("Optinos:");
+    output.push_back("    --print");
+    output.push_back("    --reverse");
+    output.push_back("    --push_back");
+    output.push_back("    --push_front");
+    output.push_back("    --help"); 
+    output.push_back("Notion: you can use the same option several times");
 }
 void DoubleLinkedListApp::reverse(std::vector<std::string>& output) {
     list.reverse();
@@ -61,9 +71,11 @@ void DoubleLinkedListApp::print(std::vector<std::string>& output) {
         output.push_back(list[i]->get_data());
     }
 }
-void DoubleLinkedListApp::push_back(std::vector<std::string>& output, std::string elem) {
+void DoubleLinkedListApp::push_back(std::vector<std::string>& output,
+ std::string elem) {
     list.push_back(elem);
 }
-void DoubleLinkedListApp::push_front(std::vector<std::string>& output, std::string elem) {
+void DoubleLinkedListApp::push_front(std::vector<std::string>& output,
+ std::string elem) {
     list.push_front(elem);
 }
