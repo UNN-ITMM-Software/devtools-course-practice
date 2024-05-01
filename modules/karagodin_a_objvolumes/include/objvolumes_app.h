676630d1899b1ac293005fd4110d35b375ff2c31
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2024-05-01 19:58:55 +0300

    codecheck fix 2

--- modules/karagodin_a_objvolumes/include/objvolumes_app.h
+++ modules/karagodin_a_objvolumes/include/objvolumes_app.h
@@ -8,20 +8,19 @@
 
 class ObjVolumeApp {
  public:
-	 ObjVolumeApp();
+     ObjVolumeApp();
 
-	 std::string operator()(int argc, const char** argv);
+     std::string operator()(int argc, const char** argv);
 
  private:
-	 void help(const char* appname, const char* message = "");
-	 bool validateNumberOfArguments(int argc, const char** argv);
-	 std::string message_;
-	 typedef struct {
-		 std::string objType;
-		 std::vector<double> arg;
-	 } Arguments;
+     void help(const char* appname, const char* message = "");
+     bool validateNumberOfArguments(int argc, const char** argv);
+     std::string message_;
+     typedef struct {
+          std::string objType;
+          std::vector<double> arg;
+     } Arguments;
 
 };
-
 
 #endif // MODULES_KARAGODIN_A_OBJVOLUMES_INCLUDE_OBJVOLUMES_APP_H_
--- modules/karagodin_a_objvolumes/src/objvolumes_app.cpp
+++ modules/karagodin_a_objvolumes/src/objvolumes_app.cpp
@@ -7,119 +7,119 @@
 ObjVolumeApp::ObjVolumeApp() : message_("") {}
 
 std::vector<double> parseVector(const char* input) {
-	std::vector<double> result;
-	std::stringstream ss(input);
-	std::string token;
+    std::vector<double> result;
+    std::stringstream ss(input);
+    std::string token;
 
-	while (std::getline(ss, token, ',')) {
-		try {
-			double number = std::stod(token);
-			result.push_back(number);
-		}
-		catch (const std::exception& e) {
-			throw std::logic_error("Wrong vector format!");
-			continue;
-		}
-	}
+    while (std::getline(ss, token, ',')) {
+        try {
+            double number = std::stod(token);
+            result.push_back(number);
+        }
+        catch (const std::exception& e) {
+            throw std::logic_error("Wrong vector format!");
+            continue;
+        }
+    }
 
-	return result;
+    return result;
 }
 
 std::string parseObjType(const char* arg) {
-	std::string ot;
-	if (std::strcmp(arg, "Sphere") == 0) {
-		ot = "Sphere";
-	}
-	else if (strcmp(arg, "Cylinder") == 0) {
-		ot = "Cylinder";
-	}
-	else if (strcmp(arg, "Cube") == 0) {
-		ot = "Cube";
-	}
-	else {
-		throw std::logic_error("Wrong object format!");
-	}
-	return ot;
+    std::string ot;
+    if (std::strcmp(arg, "Sphere") == 0) {
+        ot = "Sphere";
+    }
+    else if (strcmp(arg, "Cylinder") == 0) {
+        ot = "Cylinder";
+    }
+    else if (strcmp(arg, "Cube") == 0) {
+        ot = "Cube";
+    }
+    else {
+        throw std::logic_error("Wrong object format!");
+    }
+    return ot;
 }
 
 std::string ObjVolumeApp::operator()(int argc, const char** argv) {
-	Arguments input;
+    Arguments input;
 
-	if (!validateNumberOfArguments(argc, argv)) {
-		return message_;
-	}
-	try {
-		input.objType = parseObjType(argv[1]);
-		input.arg = parseVector(argv[2]);
-	}
-	catch (const std::exception& e) {
-		return e.what();
-	}
+    if (!validateNumberOfArguments(argc, argv)) {
+        return message_;
+    }
+    try {
+        input.objType = parseObjType(argv[1]);
+        input.arg = parseVector(argv[2]);
+    }
+    catch (const std::exception& e) {
+        return e.what();
+    }
 
-	std::ostringstream stream;
-	if (input.objType == "Sphere") {
-		if (input.arg.size() != 1) {
-			return message_;
-		}
-		try {
-			Sphere sphere(input.arg[0]);
-			stream << "Volume = " << sphere.volume();
-		}
-		catch (const std::invalid_argument& e) {
-			throw std::logic_error("Wrong arguement format!");
-		}
-	}
-	else if (input.objType == "Cylinder") {
-		if (input.arg.size() != 2) {
-			return message_;
-		}
-		try {
-			Cylinder cylinder(input.arg[0], input.arg[1]);
-			stream << "Volume = " << cylinder.volume();
-		}
-		catch (const std::invalid_argument& e) {
-			throw std::logic_error("Wrong arguement format!");
-		}
-	}
-	else if (input.objType == "Cube") {
-		if (input.arg.size() != 1) {
-			return message_;
-		}
-		try {
-			Cube cube(input.arg[0]);
-			stream << "Volume = " << cube.volume();
-		}
-		catch (const std::invalid_argument& e) {
-			throw std::logic_error("Wrong arguement format!");
-		}
-	}
+    std::ostringstream stream;
+    if (input.objType == "Sphere") {
+        if (input.arg.size() != 1) {
+            return message_;
+        }
+        try {
+            Sphere sphere(input.arg[0]);
+            stream << "Volume = " << sphere.volume();
+        }
+        catch (const std::invalid_argument& e) {
+            throw std::logic_error("Wrong arguement format!");
+        }
+    }
+    else if (input.objType == "Cylinder") {
+        if (input.arg.size() != 2) {
+            return message_;
+        }
+        try {
+            Cylinder cylinder(input.arg[0], input.arg[1]);
+            stream << "Volume = " << cylinder.volume();
+        }
+        catch (const std::invalid_argument& e) {
+            throw std::logic_error("Wrong arguement format!");
+        }
+    }
+    else if (input.objType == "Cube") {
+        if (input.arg.size() != 1) {
+            return message_;
+        }
+        try {
+            Cube cube(input.arg[0]);
+            stream << "Volume = " << cube.volume();
+        }
+        catch (const std::invalid_argument& e) {
+            throw std::logic_error("Wrong arguement format!");
+        }
+    }
 
-	message_ = stream.str();
-	return message_;
+    message_ = stream.str();
+    return message_;
 }
 
 void ObjVolumeApp::help(const char* appname, const char* message) {
-	message_ =
-		std::string(message) +
-		"This is an object volume calculator application.\n\n" +
-		"Please provide arguments in the following format:\n\n" +
+    message_ =
+        std::string(message) +
+        "This is an object volume calculator application.\n\n" +
+        "Please provide arguments in the following format:\n\n" +
 
-		"  $ " + appname + " <objType> <arg> \n\n" +
+        "  $ " + appname + " <objType> <arg> \n\n" +
 
-		"Where objType is a string with type of object (Sphere, Cube, Cylinder), " +
-		"and <arg> is a vector with double-precision numbers \n" +
-		"In the format of 1,2,3 or 1.0,2.0,3.0 \n";
+        "Where objType is a string with type of object (Sphere, Cube, Cylinder), " +
+        "and <arg> is a vector with double-precision numbers \n" +
+        "In the format of 1,2,3 or 1.0,2.0,3.0 \n";
 }
 
 bool ObjVolumeApp::validateNumberOfArguments(int argc, const char** argv) {
-	if (argc == 1) {
-		help(argv[0]);
-		return false;
-	}
-	else if (argc != 3) {
-		help(argv[0], "ERROR: Should be 2 arguments.\n\n");
-		return false;
-	}
-	return true;
+    if (argc == 1) {
+        help(argv[0]);
+        return false;
+    }
+    else if (argc != 3) {
+        help(argv[0], "ERROR: Should be 2 arguments.\n\n");
+        return false;
+    }
+    return true;
 }
 
