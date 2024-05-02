23a3e5332e26b5df15b55572ccb4d14ce1d60fa3
Trolozor <mplololowka@mail.ru>
2024-05-02 04:02:44 +0300

    first

---
+++ modules/kochetov_n_djarvis/application/main_app.cpp
@@ -0,0 +1,19 @@
+// Copyright 2024 Dostavalov Semyon
+
+#include "include/djarvis_app.h"
+#include "include/djarvis.h"
+
+int main(int argc, char* argv[]) {
+    std::vector<std::string> args;
+
+    for (int i = 1; i < argc; ++i) {
+        args.push_back(argv[i]);
+    }
+
+    Application app;
+
+    std::string output = app.run(args);
+    std::cout << output << std::endl;
+
+    return 0;
+}
