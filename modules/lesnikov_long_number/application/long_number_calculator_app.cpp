1018ea181301550659046102a68b8a2a80fd6cdc
Dmitry Vanushkin <56794726+dmitry-vnn@users.noreply.github.com>
2024-05-02 16:25:30 +0300

    Update modules/lesnikov_long_number/application/long_number_calculator_app.cpp
    Co-authored-by: Kuznetsov-Artyom <110616662+Kuznetsov-Artyom@users.noreply.github.com>

--- modules/lesnikov_long_number/application/long_number_calculator_app.cpp
+++ modules/lesnikov_long_number/application/long_number_calculator_app.cpp
@@ -3,7 +3,7 @@
 #include <cstdio>
 #include <string>
 
-#include "../include/long_number_calculator.h"
+#include "include/long_number_calculator.h"
 
 int main(int argc, const char** argv) {
     LongNumberCalculator app;
