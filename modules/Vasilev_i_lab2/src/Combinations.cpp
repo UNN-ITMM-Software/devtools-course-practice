8620dd17d436e7e7bd7e3b097b7f708a10f86440
karrin0chka <karinka25032003@gmail.com>
2024-05-02 21:14:32 +0300

    add lab

--- modules/Vasilev_i_lab2/src/Combinations.cpp
+++ modules/Vasilev_i_lab2/src/Combinations.cpp
@@ -1,2 +1,28 @@
-// Copyright 2024 Vasilev Ivan
+// Copyright 2024 Tushentsova Karina
 
+#include "include/Combinations.h"
+#include <vector>
+
+void combinationsUtil(const std::vector<int>& arr, size_t n, int k, int index,
+    std::vector<int>& data, int i,
+    std::vector<std::vector<int>>& result) {
+    if (index == k) {
+        result.push_back(data);
+        return;
+    }
+    if (i >= n) {
+        return;
+    }
+    data[index] = arr[i];
+    combinationsUtil(arr, n, k, index + 1, data, i + 1, result);
+    combinationsUtil(arr, n, k, index, data, i + 1, result);
+}
+
+std::vector<std::vector<int>> generateCombinations
+(const std::vector<int>& arr, int k) {
+    std::vector<std::vector<int>> result;
+    size_t n = arr.size();
+    std::vector<int> data(k);
+    combinationsUtil(arr, n, k, 0, data, 0, result);
+    return result;
+}
