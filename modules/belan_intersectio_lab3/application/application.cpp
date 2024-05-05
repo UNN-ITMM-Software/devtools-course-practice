6dc660ce10cdf2e8d18a360d8114dc6b81f7eea8
vadimbelan <92176898+vadimbelan@users.noreply.github.com>
2024-05-05 09:58:49 +0300

    Delete modules/belan_intersectio_lab3 directory

--- modules/belan_intersectio_lab3/application/application.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 Belan Vadim
-
-#include "include/console_application.h"
-
-int main(int argc, char* argv[]) {
-  Application app;
-  return app.main(argc, std::vector<std::string>(argv, argv + argc));
-}
