fccb33a63324f257d51f89378dd09df05b4ffb2b
AleksandrKutarin <fames5369@gmail.com>
2024-05-06 21:35:39 +0300

    fix 3.2

--- modules/novostroev_i_linear_single_list/include/SinglLinkedList.h
+++ modules/novostroev_i_linear_single_list/include/SinglLinkedList.h
@@ -15,7 +15,6 @@ class SinglLinkedList {
     void print(std::vector<std::string>& output);
     void push_back(std::vector<std::string>& output, std::string elem);
     void push_front(std::vector<std::string>& output, std::string elem);
-    void test_operator(std::vector<std::string>& output);
  public:
     SinglLinkedList() {}
     std::vector<std::string> runApp(int argc, const char** argv);
