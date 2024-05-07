54f1ae178e078d82373203e8ffa85e28d6843d27
FirewallGR <90064202+FirewallGR@users.noreply.github.com>
2024-05-07 22:20:42 +0300

    Revert "Филатов Максим - Лабораторная работа 3 (#352)"
    This reverts commit 360781b3aec9a0a61db17a95ef3ada83fc5cb7bb.

--- modules/kutarin_a_lab2/test/test_kutarin_a_deque.cpp
+++ modules/kutarin_a_lab2/test/test_kutarin_a_deque.cpp
@@ -116,7 +116,7 @@ TEST(DequeTest, LargeAmount) {
     EXPECT_TRUE(dq.empty());
 }
 
-TEST(DequeTest, DoubleFront) {
+TEST(DequeTest, Double) {
     // Arrange
     Deque<double> dq;
     double value = 1.5;
@@ -130,35 +130,7 @@ TEST(DequeTest, DoubleFront) {
     EXPECT_TRUE(dq.empty());
 }
 
-TEST(DequeTest, DoubleBack) {
-    // Arrange
-    Deque<double> dq;
-    double value = 1.5;
-
-    // Act
-    dq.push_back(value);
-    double pop_result = dq.pop_back();
-
-    // Assert
-    EXPECT_DOUBLE_EQ(pop_result, value);
-    EXPECT_TRUE(dq.empty());
-}
-
-TEST(DequeTest, StringFront) {
-    // Arrange
-    Deque<std::string> dq;
-    std::string value = "test";
-
-    // Act
-    dq.push_front(value);
-    std::string pop_result = dq.pop_front();
-
-    // Assert
-    EXPECT_EQ(pop_result, value);
-    EXPECT_TRUE(dq.empty());
-}
-
-TEST(DequeTest, StringBack) {
+TEST(DequeTest, String) {
     // Arrange
     Deque<std::string> dq;
     std::string value = "test";
@@ -232,104 +204,4 @@ TEST(DequeTest, MoveAssignment) {
     EXPECT_EQ(dq2.pop_front(), 1);
     EXPECT_EQ(dq2.pop_front(), 2);
     EXPECT_TRUE(dq2.empty());
-}
-
-TEST(DequeTest, PushFront) {
-    Deque<int> deque;
-    for (int i = 0; i < 5; ++i) {
-        deque.push_front(i);
-    }
-
-    for (int i = 0; i < 5; ++i) {
-        ASSERT_EQ(deque.pop_back(), i);
-    }
-}
-
-TEST(DequeTest, PushBack) {
-    Deque<int> deque;
-    for (int i = 0; i < 5; ++i) {
-        deque.push_back(i);
-    }
-
-    for (int i = 0; i < 5; ++i) {
-        ASSERT_EQ(deque.pop_front(), i);
-    }
-}
-
-TEST(DequeTest, Reverse) {
-    Deque<int> deque;
-    for (int i = 0; i < 5; ++i) {
-        deque.push_back(i);
-    }
-
-    deque.reverse();
-
-    for (int i = 0; i < 5; ++i) {
-        ASSERT_EQ(deque.pop_front(), 4 - i);
-    }
-}
-
-TEST(DequeTest, OperatorSquareBrackets) {
-    Deque<int> deque;
-    for (int i = 0; i < 5; ++i) {
-        deque.push_back(i);
-    }
-
-    for (int i = 0; i < 5; ++i) {
-        ASSERT_EQ(deque[i], i);
-    }
-}
-
-TEST(DequeTest, OperatorSquareBracketsOutOfRange) {
-    Deque<int> deque;
-    for (int i = 0; i < 5; ++i) {
-        deque.push_back(i);
-    }
-
-    try {
-        deque[5];
-        FAIL() << "Expected std::out_of_range";
-    }
-    catch(const std::out_of_range& e) {
-        EXPECT_EQ(e.what(), std::string("Index out of range"));
-    }
-    catch(...) {
-        FAIL() << "Expected std::out_of_range";
-    }
-}
-
-TEST(DequeTest, PushFrontToEmptyDeque) {
-    Deque<int> deque;
-    deque.push_front(1);
-    ASSERT_EQ(deque[0], 1);
-}
-
-TEST(DequeTest, PushBackToEmptyDeque) {
-    Deque<int> deque;
-    deque.push_back(1);
-    ASSERT_EQ(deque[0], 1);
-}
-
-
-TEST(DequeTest, PushFrontToEmptyDequeNoThrow) {
-    Deque<int> deque;
-    ASSERT_NO_THROW(deque.push_front(1));
-}
-
-TEST(DequeTest, PushBackToEmptyDequeNoThrow) {
-    Deque<int> deque;
-    ASSERT_NO_THROW(deque.push_back(1));
-}
-
-TEST(DequeTest, SimpleOperatorSquareBracketsOutOfRange) {
-    Deque<int> deque;
-    ASSERT_THROW(deque[5], std::out_of_range);
-}
-
-TEST(DequeTest, SimplePushBack) {
-    Deque<int> deque;
-    ASSERT_NO_THROW(deque.push_back(1));
-    ASSERT_NO_THROW(deque.push_back(2));
-    ASSERT_NO_THROW(deque.push_back(3));
-    ASSERT_EQ(static_cast<int>(deque.size()), 3);
 }
