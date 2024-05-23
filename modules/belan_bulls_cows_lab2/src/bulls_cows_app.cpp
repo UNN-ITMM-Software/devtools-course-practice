eafa832fe4f8cd70f6ae5c22e4164f7ba0e6ab9d
Ivan Novostroev <novostroievv@mail.ru>
2024-05-23 16:20:43 +0300

    fix

--- modules/belan_bulls_cows_lab2/src/bulls_cows_app.cpp
+++ modules/belan_bulls_cows_lab2/src/bulls_cows_app.cpp
@@ -5,14 +5,16 @@
 #include <sstream>
 #include <string>
 
-BullsCowsApplication::BullsCowsApplication() : appName("Bulls and Cows"), errorCode(EC_OK) {}
+BullsCowsApplication::BullsCowsApplication() : appName("Bulls and Cows"),
+errorCode(EC_OK) {}
 
 std::string BullsCowsApplication::getHelpMessage(const char* appname) {
     std::ostringstream message;
     message << "This is a Bulls and Cows application.\n"
             << "Please provide arguments in the following format:\n\n"
             << "  $ " << appname << " <difficulty level>\n\n"
-            << "Where <difficulty level> is an integer value that determines the number of digits in the secret number.\n";
+            << "Where <difficulty level> is an integer value that determines
+            the number of digits in the secret number.\n";
     return message.str();
 }
 
@@ -25,13 +27,15 @@ std::string BullsCowsApplication::doWork(int argc, const char** argv) {
         return getHelpMessage(argv[0]);
     } else if (argc != 2) {
         errorCode = EC_INCORRECT_ARGUMENT_NUMBER;
-        return "ERROR: Incorrect number of arguments.\n" + getHelpMessage(argv[0]);
+        return "ERROR: Incorrect number of arguments.\n"
+        + getHelpMessage(argv[0]);
     }
 
     int difficulty = charToInt(argv[1]);
     if (difficulty <= 0) {
         errorCode = EC_INCORRECT_DIFFICULTY;
-        return "ERROR: Difficulty level must be a positive integer.\n" + getHelpMessage(argv[0]);
+        return "ERROR: Difficulty level must be a positive integer.\n"
+        + getHelpMessage(argv[0]);
     }
 
     game.setDifficulty(difficulty);
@@ -39,7 +43,8 @@ std::string BullsCowsApplication::doWork(int argc, const char** argv) {
 
     std::ostringstream message;
     message << "Welcome to Bulls and Cows game!\n"
-            << "I have generated a secret number with " << difficulty << " unique digits.\n"
+            << "I have generated a secret number with "
+            << difficulty << " unique digits.\n"
             << "You have 3 attempts to guess the number.\n";
 
     while (game.getAttemptsLeft() > 0) {
@@ -49,20 +54,25 @@ std::string BullsCowsApplication::doWork(int argc, const char** argv) {
         std::cin >> guess;
 
         if (!game.validateGuess(guess)) {
-            message << "Invalid guess. Make sure it has " << difficulty << " digits and contains only numbers.\n";
+            message << "Invalid guess. Make sure it has " << difficulty
+            << " digits and contains only numbers.\n";
             continue;
         }
 
         auto result = game.guessNumber(guess);
         if (result.first == difficulty) {
-            message << "Congratulations! You've guessed the number correctly.\n";
+            message << "Congratulations! You've guessed
+            the number correctly.\n";
             break;
         } else {
-            message << "Bulls: " << result.first << ", Cows: " << result.second << "\n";
+            message << "Bulls: " << result.first << ", Cows: "
+            << result.second << "\n";
             if (game.getAttemptsLeft() > 0) {
                 message << "Attempts left: " << game.getAttemptsLeft() << "\n";
             } else {
-                message << "Sorry, you've run out of attempts. The secret number was: " << game.generateSecretNumber() << "\n";
+                message << "Sorry, you've run out of attempts.
+                The secret number was: "
+                << game.generateSecretNumber() << "\n";
             }
         }
     }
