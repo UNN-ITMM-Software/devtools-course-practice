1e564e641f18c266337cade47930b695807ec66e
KirFedotov <k.fedotov.guit29@mail.ru>
2024-05-25 20:56:06 +0300

    lab3

---
+++ modules/bakhtiarov_a_priority_queue/include/priority_queue_app.h
@@ -0,0 +1,20 @@
+// Copyright 2024 Fedotov Kirill
+
+#pragma once
+
+#include <string>
+#include <utility>
+
+#include "include/priority_queue.h"
+
+class PriorityQueueApplication {
+ private:
+    std::string msg;
+    TQueue_insert<int> queue;
+    bool Validate(int argc, char* argv[]);
+    void Help(const char* application, const char* errMsg = nullptr);
+
+ public:
+    PriorityQueueApplication() = default;
+    std::string ProcessQueueOperations(int argc, char* argv[]);
+};
