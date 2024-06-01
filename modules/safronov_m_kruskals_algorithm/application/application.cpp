bb219b8af2a09f446ce8b7a1645fc2d6be4c01e4
yourd8ne <done101@mail.ru>
2024-06-01 12:02:00 +0300

    another one

--- modules/safronov_m_kruskals_algorithm/application/application.cpp
+++ modules/safronov_m_kruskals_algorithm/application/application.cpp
@@ -5,8 +5,7 @@
 
 #include "include/kruskalapp.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     KruskalApp app;
     std::cout << app.Parse(argc, argv) << std::endl;
     return 0;
