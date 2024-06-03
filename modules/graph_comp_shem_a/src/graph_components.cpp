ab3ca8a9177e9c38694e26e9a7de120f53db2571
Алеся Шемякина <alesya.shemyakina.03@mail.ru>
2024-06-03 22:37:10 +0300

    third commit

--- modules/graph_comp_shem_a/src/graph_components.cpp
+++ modules/graph_comp_shem_a/src/graph_components.cpp
@@ -12,21 +12,21 @@
 
 void DFS(int x, std::vector<std::vector<int>>& v, std::vector<int>& was) {
     was[x] = 1;
-    for(int y : v[x])
-        if(was[y] == 0)
-	        DFS(y, v, was);
+    for (int y : v[x])
+        if (was[y] == 0)
+            DFS(y, v, was);
 }
 
 int graph_components_numbers(int n, std::vector<std::vector<int>> v){
     std::vector<int> was(n);
-    for(int x = 0; x < n; x++){
+    for (int x = 0; x < n; x++){
         was[x] = 0;
     }
     int ans = 0;
-    for(int x = 0; x < n; x++){
-        if(was[x] == 0){
-	        DFS(x, v, was);
-	        ans++;
+    for (int x = 0; x < n; x++){
+        if (was[x] == 0){
+            DFS(x, v, was);
+            ans++;
         }
     }
     return ans;
