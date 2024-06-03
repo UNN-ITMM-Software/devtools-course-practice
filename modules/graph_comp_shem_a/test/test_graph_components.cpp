af731da71670a735bf25dd8d861eba589afc791c
Алеся Шемякина <alesya.shemyakina.03@mail.ru>
2024-06-03 23:20:31 +0300

    tenth commit

--- modules/graph_comp_shem_a/test/test_graph_components.cpp
+++ modules/graph_comp_shem_a/test/test_graph_components.cpp
@@ -7,14 +7,12 @@
 #include "include/graph_components.h"
 
 TEST(Shemiakina_Alesia_GraphNumbersTest, returns_1_when_given_2_super) {
-    int was[2];
     std::vector<std::vector<int>> v{{1}, {0}};
     int count = graph_components_numbers(2, v);
     EXPECT_EQ(1, count);
 }
 
 TEST(Shemiakina_Alesia_GraphNumbersTest, returns_2_when_given_5) {
-    int was[5];
     std::vector<std::vector<int>> v{{1}, {0, 2}, {1}, {4}, {3}};
     int count = graph_components_numbers(5, v);
 
@@ -22,7 +20,6 @@ TEST(Shemiakina_Alesia_GraphNumbersTest, returns_2_when_given_5) {
 }
 
 TEST(Shemiakina_Alesia_GraphNumbersTest, returns_1_when_given_1) {
-    int was[1];
     std::vector<std::vector<int>> v{{ }};
     int count = graph_components_numbers(1, v);
 
@@ -30,7 +27,6 @@ TEST(Shemiakina_Alesia_GraphNumbersTest, returns_1_when_given_1) {
 }
 
 TEST(Shemiakina_Alesia_GraphNumbersTest, returns_3_when_given_8) {
-    int was[8];
     std::vector<std::vector<int>> v{{1, 7}, {0, 6}, {3}, {2, 4},
      {3}, { }, {1, 7}, {0, 6}};
     int count = graph_components_numbers(8, v);
@@ -39,7 +35,6 @@ TEST(Shemiakina_Alesia_GraphNumbersTest, returns_3_when_given_8) {
 }
 
 TEST(Shemiakina_Alesia_GraphNumbersTest, returns_1_when_given_10) {
-    int was[10];
     std::vector<std::vector<int>> v{{1}, {0, 2}, {3, 1}, {4, 2}, {5, 3},
      {6, 4}, {7, 5}, {8, 6}, {9, 7}, {8}};
     int count = graph_components_numbers(10, v);
@@ -48,9 +43,15 @@ TEST(Shemiakina_Alesia_GraphNumbersTest, returns_1_when_given_10) {
 }
 
 TEST(Shemiakina_Alesia_GraphNumbersTest, returns_0_when_given_0) {
-    int was[1];
     std::vector<std::vector<int>> v{ };
     int count = graph_components_numbers(0, v);
 
     EXPECT_EQ(count, 0);
+}
+
+TEST(Shemiakina_Alesia_GraphNumbersTest, one_sided_list) {
+    std::vector<std::vector<int>> v{{1}, {2}, {0}};
+    int count = graph_components_numbers(3, v);
+
+    EXPECT_EQ(count, 1);
 }
