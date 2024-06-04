c0e371d363656bbc8c73d4d00b6101344538ee39
igkonov <igkonov@yandex.ru>
2024-06-04 21:53:02 +0300

    init

---
+++ modules/graph_comp_shem_a/include/graph_components_app.h
@@ -0,0 +1,25 @@
+// Copyright 2024 Konovalov Igor
+
+#pragma once
+
+#include <cmath>
+#include <stdexcept>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <string>
+
+#include "include/graph_components.h"
+
+class GraphComponentsApplication {
+ public:
+  GraphComponentsApplication() = default;
+  std::string operator()(int argc, char* argv[]);
+
+ private:
+  int n{};
+  int result{};
+  std::string message_{};
+  bool validate(int argc, char* argv[]);
+  void help(const char* appName, const char* msg = nullptr);
+};
