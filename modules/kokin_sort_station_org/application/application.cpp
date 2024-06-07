cd6f400c437f53afef09706ebbb8e0c0ba9aafc1
ThreeToads <ivan.kokin.465@gmail.com>
2024-06-07 23:25:01 +0300

    feat(lab3): add app files

---
+++ modules/kokin_sort_station_org/application/application.cpp
@@ -0,0 +1,11 @@
+// Copyright 2024 Kokin Ivan
+
+#include <iostream>
+
+#include "../include/Console_app.h"
+
+int main(int argc, char** argv) {
+  ConsoleApp app;
+  app.run(argc, argv);
+  return 0;
+}
---
+++ modules/kokin_sort_station_org/include/sort_station.h
@@ -0,0 +1,28 @@
+// Copyright 2024 Kokin Ivan
+
+#ifdef MODULES_KOKIN_SORT_STATION_ORG_INCLUDE_SORT_STATION_H_
+#define MODULES_KOKIN_SORT_STATION_ORG_INCLUDE_SORT_STATION_H_
+
+#include <string>
+#include <stack>
+#include <unordered_map>
+#include <stdexcept>
+
+class SortStation {
+ public:
+    virtual double evaluateFunction(const std::string& expression) = 0;
+ protected:
+    std::unordered_map<char, int> precedence = {
+            {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}, {'^', 3}
+    };
+};
+class SortStationDerived : public SortStation {
+ public:
+     double evaluateFunction(const std::string& expression) override;
+     double evaluatePostfixExpression(const std::string& postfix);
+ private:
+     double applyOperator(double a, double b, char op);
+     std::string convertToPostfix(const std::string& expression);
+};
+
+#endif // MODULES_KOKIN_SORT_STATION_ORG_INCLUDE_SORT_STATION_H_
