838d1fbed4b5e0cabe491c2ace4f18f2b0a869fe
yourd8ne <done101@mail.ru>
2024-06-08 08:53:49 +0300

    asgqwr

--- modules/safronov_m_kruskals_algorithm/src/kruskal.cpp
+++ modules/safronov_m_kruskals_algorithm/src/kruskal.cpp
@@ -85,7 +85,8 @@ std::string KruskalApp::Parse(int argc, char* argv[]) {
     std::ostringstream stream;
     stream << "Edges in the minimum spanning tree:\n";
     for (const auto& edge : mst) {
-        stream << edge.src << " - " << edge.dest << " : " << edge.weight << "\n";
+        stream << edge.src << " - " << edge.dest << " : " << edge.weight 
+               << "\n";
     }
     return stream.str();
 }
