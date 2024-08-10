7738a2afca7d0c893c2f6925926b8493ee3c9da5
Hugelka <105743453+Hugelka@users.noreply.github.com>
2024-08-10 10:50:42 +0300

    lab2

---
+++ modules/mironov_i_stats/include/stats.h
@@ -0,0 +1,12 @@
+// Copyright 2024 Mironov Ilya
+
+#pragma once
+
+#include <vector>
+
+namespace stats {
+float moment(const std::vector<float>& data, std::size_t k, float mu);
+float mean(const std::vector<float>& data);
+float var_base(const std::vector<float>& data);
+float var(const std::vector<float>& data);
+}  // namespace stats
---
+++ modules/mironov_i_stats/src/stats.cpp
@@ -0,0 +1,33 @@
+// Copyright 2024 Mironov Ilya
+
+#include "include/stats.h"
+
+#include <cmath>
+#include <numeric>
+
+namespace stats {
+
+float moment(const std::vector<float>& data, std::size_t k, float mu) {
+    float result = 0.0f;
+    for (auto& x : data) {
+        result += std::pow(x - mu, k);
+    }
+    return result / data.size();
+}
+float mean(const std::vector<float>& data) {
+    return moment(data, 1, 0.0f);
+}
+float var_base(const std::vector<float>& data) {
+    return moment(data, 2, mean(data));
+}
+float var(const std::vector<float>& data) {
+    float result1 = 0.0f;
+    float result2 = 0.0f;
+    const auto size = data.size();
+    for (auto& x : data) {
+        result1 += x * x;
+        result2 += x;
+    }
+    return result1 / size - (result2 / size) * (result2 / size);
+}
+}  // namespace stats
