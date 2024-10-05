e9bf0b6d4ceeb3eb5bb21aa849e72aa1c537e89a
STELLSAN <stellsandio@gmail.com>
2024-10-05 19:21:19 +0300

    cpplint_fix_2

--- modules/nedelin_d_book_discount_lab2/src/HarryPotterBooks.cpp
+++ modules/nedelin_d_book_discount_lab2/src/HarryPotterBooks.cpp
@@ -6,7 +6,8 @@ double HarryPotterBooks::calculateTotalPrice(std::vector<int> books) {
     double total = 0.0;
 
     while (true) {
-        int uniqueBooks = std::count_if(books.begin(), books.end(), [](int x) { return x > 0; });
+        int uniqueBooks = std::count_if(books.begin(),
+            books.end(), [](int x) { return x > 0; });
 
         if (uniqueBooks == 0) {
             break;
