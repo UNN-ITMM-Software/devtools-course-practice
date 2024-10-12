80d3d06be41d263bd999daa91c3e9c0bb5d40936
Hugelka <105743453+Hugelka@users.noreply.github.com>
2024-10-12 10:59:09 +0300

    Update modules/mironov_i_stats/include/stats.h
    Co-authored-by: Kuznetsov-Artyom <110616662+Kuznetsov-Artyom@users.noreply.github.com>

--- modules/mironov_i_stats/include/stats.h
+++ modules/mironov_i_stats/include/stats.h
@@ -5,7 +5,7 @@
 #include <vector>
 
 namespace stats {
-float moment(const std::vector<float>& data, std::size_t k, float mu);
+float moment(const std::vector<float>& data, size_t k, float mu);
 float mean(const std::vector<float>& data);
 float var_base(const std::vector<float>& data);
 float var(const std::vector<float>& data);
