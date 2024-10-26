69496352ad47ac24d14e7f85b6ee824583ac87b4
PavelLSmirnov <pavel1201s@yandex.ru>
2024-10-26 08:37:32 +0300

    fix lint

--- modules/nedelin_d_book_discount_lab2/application/application.cpp
+++ modules/nedelin_d_book_discount_lab2/application/application.cpp
@@ -4,8 +4,7 @@
 
 #include "include/HarryPotterBooksApp.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   HarryPotterBooksApp app;
   auto output = app.run(argc, argv);
 
