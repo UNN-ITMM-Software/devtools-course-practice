d4c61f44bd14505e648f4627c8b52d202911e30c
STELLSAN <stellsandio@gmail.com>
2024-12-01 17:38:55 +0300

    cpplint_fix

--- modules/nedelin_d_number_to_words_lab3/include/number_to_wordsApp.h
+++ modules/nedelin_d_number_to_words_lab3/include/number_to_wordsApp.h
@@ -1,17 +1,17 @@
 // Copyright 2024 Nedelin Dmitry
 
-#ifndef MODULES_NEDELIN_DMITRY_NUMBER_TO_WORDS_INCLUDE_APPLICATION_H_
-#define MODULES_NEDELIN_DMITRY_NUMBER_TO_WORDS_INCLUDE_APPLICATION_H_
+#ifndef MODULES_NEDELIN_D_NUMBER_TO_WORDS_LAB3_INCLUDE_NUMBER_TO_WORDSAPP_H_
+#define MODULES_NEDELIN_D_NUMBER_TO_WORDS_LAB3_INCLUDE_NUMBER_TO_WORDSAPP_H_
 
 #include <string>
 #include <vector>
 
 class Application{
-public:
+ public:
     std::vector<std::string> runApp(int argc, const char** argv);
 
-private:
+ private:
     std::string processNumber(const std::string& input);
 };
 
-#endif  // MODULES_NEDELIN_DMITRY_NUMBER_TO_WORDS_INCLUDE_APPLICATION_H_
+#endif  // MODULES_NEDELIN_D_NUMBER_TO_WORDS_LAB3_INCLUDE_NUMBER_TO_WORDSAPP_H_
